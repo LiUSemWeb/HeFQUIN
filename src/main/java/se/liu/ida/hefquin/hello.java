c0cb16b32f066796f8bbe1530956deccf96b6968
Olaf Hartig <olaf.hartig@liu.se>
2021-03-04 17:08:27 +0100

    initial commit

---
+++ src/main/java/se/liu/ida/hefquin/hello.java
@@ -0,0 +1,15 @@
+package se.liu.ida.hefquin;
+
+import org.apache.jena.query.ARQ;
+
+/**
+ * 
+ * 
+ */
+public class hello
+{
+    public static void main( String... argv )
+    {
+        System.out.println("hello");
+    }
+}
