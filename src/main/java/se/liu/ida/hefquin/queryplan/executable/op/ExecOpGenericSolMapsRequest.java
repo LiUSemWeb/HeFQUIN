c68e6fd88f22929c9ce482797e8292a13c2ee8b8
Olaf Hartig <olaf.hartig@liu.se>
2021-03-13 17:37:42 +0100

    adds ExecOpGenericSolMapsRequest

---
+++ src/main/java/se/liu/ida/hefquin/queryplan/executable/op/ExecOpGenericSolMapsRequest.java
@@ -0,0 +1,39 @@
+package se.liu.ida.hefquin.queryplan.executable.op;
+
+import java.util.Iterator;
+
+import se.liu.ida.hefquin.federation.FederationAccessManager;
+import se.liu.ida.hefquin.federation.FederationMember;
+import se.liu.ida.hefquin.federation.access.DataRetrievalRequest;
+import se.liu.ida.hefquin.federation.access.SolMapsResponse;
+import se.liu.ida.hefquin.query.SolutionMapping;
+import se.liu.ida.hefquin.queryplan.executable.IntermediateResultElementSink;
+import se.liu.ida.hefquin.queryplan.executable.NullaryExecutableOp;
+import se.liu.ida.hefquin.queryproc.ExecutionContext;
+
+public abstract class ExecOpGenericSolMapsRequest<ReqType extends DataRetrievalRequest>
+                implements NullaryExecutableOp<SolutionMapping>
+{
+	protected final ReqType req;
+	protected final FederationMember fm;
+
+	public ExecOpGenericSolMapsRequest( final ReqType req, final FederationMember fm ) {
+		assert req != null;
+		assert fm != null;
+
+		this.req = req;
+		this.fm = fm;
+	}
+
+	public void execute( final IntermediateResultElementSink<SolutionMapping> sink,
+	                     final ExecutionContext execCxt )
+	{
+		final SolMapsResponse response = performRequest( execCxt.getFederationAccessMgr() );
+		final Iterator<SolutionMapping> it = response.getIterator();
+		while ( it.hasNext() ) {
+			sink.send( it.next() );
+		}
+	}
+
+	abstract protected SolMapsResponse performRequest( final FederationAccessManager fedAccessMgr );
+}
--- src/main/java/se/liu/ida/hefquin/queryplan/executable/op/ExecOpSPARQLRequest.java
+++ src/main/java/se/liu/ida/hefquin/queryplan/executable/op/ExecOpSPARQLRequest.java
@@ -1,36 +1,18 @@
 package se.liu.ida.hefquin.queryplan.executable.op;
 
-import java.util.Iterator;
-
+import se.liu.ida.hefquin.federation.FederationAccessManager;
 import se.liu.ida.hefquin.federation.SPARQLEndpoint;
 import se.liu.ida.hefquin.federation.access.SPARQLRequest;
 import se.liu.ida.hefquin.federation.access.SolMapsResponse;
-import se.liu.ida.hefquin.query.SolutionMapping;
-import se.liu.ida.hefquin.queryplan.executable.IntermediateResultElementSink;
-import se.liu.ida.hefquin.queryplan.executable.NullaryExecutableOp;
-import se.liu.ida.hefquin.queryproc.ExecutionContext;
 
-public class ExecOpSPARQLRequest implements NullaryExecutableOp<SolutionMapping>
+public class ExecOpSPARQLRequest extends ExecOpGenericSolMapsRequest<SPARQLRequest>
 {
-	protected final SPARQLRequest req;
-	protected final SPARQLEndpoint fm;
-
 	public ExecOpSPARQLRequest( final SPARQLRequest req, final SPARQLEndpoint fm ) {
-		assert req != null;
-		assert fm != null;
-
-		this.req = req;
-		this.fm = fm;
+		super( req, fm );
 	}
 
-	public void execute( final IntermediateResultElementSink<SolutionMapping> sink,
-	                     final ExecutionContext execCxt )
-	{
-		final SolMapsResponse response = execCxt.getFederationAccessMgr().performRequest(req, fm);
-		final Iterator<SolutionMapping> it = response.getIterator();
-		while ( it.hasNext() ) {
-			sink.send( it.next() );
-		}
+	protected SolMapsResponse performRequest( final FederationAccessManager fedAccessMgr ) {
+		return fedAccessMgr.performRequest( req, (SPARQLEndpoint) fm );
 	}
 
 }
