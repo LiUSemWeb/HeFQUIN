ce55e9530a559fcdce57dbb2d5bf413c7c918a48
Olaf Hartig <olaf.hartig@liu.se>
2021-03-16 09:29:27 +0100

    adds 'numberOfChildren' and 'getChild' to PhysicalOperator

--- src/main/java/se/liu/ida/hefquin/queryplan/PhysicalOperator.java
+++ src/main/java/se/liu/ida/hefquin/queryplan/PhysicalOperator.java
@@ -1,5 +1,7 @@
 package se.liu.ida.hefquin.queryplan;
 
+import java.util.NoSuchElementException;
+
 public interface PhysicalOperator
 {
 	/**
@@ -7,4 +9,18 @@ public interface PhysicalOperator
 	 * {@link ExecutableOperator} to be used for this physical operator.
 	 */
 	ExecutableOperatorCreator<?> getExecOpCreator();
+
+	/**
+	 * Returns the number of children that this operator has.
+	 */
+	int numberOfChildren();
+
+	/**
+	 * Returns the i-th child of this operator, where i starts at
+	 * index 0 (zero).
+	 *
+	 * If the operator has fewer children (or no children at all),
+	 * then a {@link NoSuchElementException} will be thrown.
+	 */
+	PhysicalOperator getChild( int i ) throws NoSuchElementException;
 }
