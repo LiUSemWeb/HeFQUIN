6e9f90c2f9335b164aa862676aa50794c3a5888c
Olaf Hartig <olaf.hartig@liu.se>
2021-03-19 16:34:30 +0100

    remove ExecutableOperatorCreator, it is not needed anymore because PhysicalOperator now has createExecOp()

--- src/main/java/se/liu/ida/hefquin/queryplan/ExecutableOperator.java
+++ src/main/java/se/liu/ida/hefquin/queryplan/ExecutableOperator.java
@@ -2,5 +2,5 @@ package se.liu.ida.hefquin.queryplan;
 
 public interface ExecutableOperator
 {
-	
+	// TODO: this interface should have a method that returns the preferred block size of input blocks for this executable op.
 }
