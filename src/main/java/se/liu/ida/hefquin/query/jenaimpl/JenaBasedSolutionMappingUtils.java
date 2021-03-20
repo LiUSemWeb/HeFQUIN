62c22f62d6d24531b2119c8496c7a89ab38cdf14
Olaf Hartig <olaf.hartig@liu.se>
2021-03-20 09:50:43 +0100

    adds creator methods to JenaBasedSolutionMappingUtils

--- src/main/java/se/liu/ida/hefquin/query/jenaimpl/JenaBasedSolutionMappingUtils.java
+++ src/main/java/se/liu/ida/hefquin/query/jenaimpl/JenaBasedSolutionMappingUtils.java
@@ -2,12 +2,68 @@ package se.liu.ida.hefquin.query.jenaimpl;
 
 import java.util.Iterator;
 
+import org.apache.jena.graph.Node;
 import org.apache.jena.sparql.core.Var;
 import org.apache.jena.sparql.engine.binding.Binding;
+import org.apache.jena.sparql.engine.binding.BindingFactory;
+import org.apache.jena.sparql.engine.binding.BindingMap;
 import org.apache.jena.sparql.engine.binding.BindingUtils;
 
 public class JenaBasedSolutionMappingUtils
 {
+	/**
+	 * Creates an empty solution mapping.
+	 */
+	public static JenaBasedSolutionMapping createJenaBasedSolutionMapping()
+	{
+		final Binding b = BindingFactory.binding();
+		return new JenaBasedSolutionMapping(b);
+	}
+
+	/**
+	 * Creates a solution mapping in which the given variable
+	 * is mapped to the given node.
+	 */
+	public static JenaBasedSolutionMapping createJenaBasedSolutionMapping(
+			final Var var, final Node node )
+	{
+		final Binding b = BindingFactory.binding(var, node);
+		return new JenaBasedSolutionMapping(b);
+	}
+
+	/**
+	 * Creates a solution mapping in which the given variables
+	 * are mapped to the given nodes, respectively.
+	 */
+	public static JenaBasedSolutionMapping createJenaBasedSolutionMapping(
+			final Var var1, final Node node1,
+			final Var var2, final Node node2 )
+	{
+		final BindingMap b = BindingFactory.create();
+		b.add(var1, node1);
+		b.add(var2, node2);
+		return new JenaBasedSolutionMapping(b);
+	}
+
+	/**
+	 * Creates a solution mapping in which the given variables
+	 * are mapped to the given nodes, respectively.
+	 */
+	public static JenaBasedSolutionMapping createJenaBasedSolutionMapping(
+			final Var var1, final Node node1,
+			final Var var2, final Node node2,
+			final Var var3, final Node node3 )
+	{
+		final BindingMap b = BindingFactory.create();
+		b.add(var1, node1);
+		b.add(var2, node2);
+		b.add(var3, node3);
+		return new JenaBasedSolutionMapping(b);
+	}
+
+	/**
+	 * Returns true if the given solution mappings are compatible.
+	 */
 	public static boolean compatible( final JenaBasedSolutionMapping m1, final JenaBasedSolutionMapping m2 ) {
 		final Binding b1 = m1.asJenaBinding();
 		final Binding b2 = m2.asJenaBinding();
@@ -22,6 +78,10 @@ public class JenaBasedSolutionMappingUtils
 		return true;		
 	}
 
+	/**
+	 * Merges the given solution mappings into one, assuming
+	 * that the given solution mappings are compatible.
+	 */
 	public static JenaBasedSolutionMapping merge( final JenaBasedSolutionMapping m1, final JenaBasedSolutionMapping m2 ) {
 		final Binding b1 = m1.asJenaBinding();
 		final Binding b2 = m2.asJenaBinding();
