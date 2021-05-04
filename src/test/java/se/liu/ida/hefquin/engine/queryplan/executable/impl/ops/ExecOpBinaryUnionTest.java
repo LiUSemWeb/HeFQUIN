1fd03aca2a966c6d9d58922e3d24999abfaec552
scferrada <scferradaa@gmail.com>
2021-05-04 16:35:10 +0200

    First simple test for binary union

--- src/test/java/se/liu/ida/hefquin/engine/queryplan/executable/impl/ops/ExecOpBinaryUnionTest.java
+++ src/test/java/se/liu/ida/hefquin/engine/queryplan/executable/impl/ops/ExecOpBinaryUnionTest.java
@@ -1,5 +1,14 @@
 package se.liu.ida.hefquin.engine.queryplan.executable.impl.ops;
 
-public class ExecOpBinaryUnionTest {
+import org.junit.Test;
 
+import se.liu.ida.hefquin.engine.federation.SPARQLEndpoint;
+
+public class ExecOpBinaryUnionTest extends TestsForUnionAlgorithms<SPARQLEndpoint>{
+	
+	@Test
+	public void testSimpleUnion(){
+		_testSimpleUnion();	
+	}
+	
 }
