b632d4c7d3ac7d02a18b59324af0743a10bef0c9
Sebastián Ferrada <scferradaa@gmail.com>
2021-05-05 08:31:33 +0200

    Update src/main/java/se/liu/ida/hefquin/engine/queryplan/physical/impl/PhysicalOpBinaryUnion.java
    Co-authored-by: Olaf Hartig <olaf.hartig@liu.se>

--- src/main/java/se/liu/ida/hefquin/engine/queryplan/physical/impl/PhysicalOpBinaryUnion.java
+++ src/main/java/se/liu/ida/hefquin/engine/queryplan/physical/impl/PhysicalOpBinaryUnion.java
@@ -43,7 +43,8 @@ public class PhysicalOpBinaryUnion implements BinaryPhysicalOpForLogicalOp{
 	}
 
 	@Override
-	public BinaryExecutableOp createExecOp(ExpectedVariables... inputVars) {
+	public BinaryExecutableOp createExecOp( final ExpectedVariables... inputVars ) {
+
 		return new ExecOpBinaryUnion();
 	}
 
