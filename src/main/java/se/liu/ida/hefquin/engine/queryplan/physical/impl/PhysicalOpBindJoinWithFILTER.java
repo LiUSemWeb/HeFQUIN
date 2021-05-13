cba247aba92181df74ab6f3f68d57d690378073e
scferrada <scferradaa@gmail.com>
2021-05-13 15:59:11 +0200

    first filter implementation

---
+++ src/main/java/se/liu/ida/hefquin/engine/queryplan/physical/impl/PhysicalOpBindJoinWithFILTER.java
@@ -0,0 +1,32 @@
+package se.liu.ida.hefquin.engine.queryplan.physical.impl;
+
+import se.liu.ida.hefquin.engine.federation.FederationMember;
+import se.liu.ida.hefquin.engine.federation.SPARQLEndpoint;
+import se.liu.ida.hefquin.engine.queryplan.ExpectedVariables;
+import se.liu.ida.hefquin.engine.queryplan.executable.impl.ops.ExecOpBindJoinSPARQLwiithFILTER;
+import se.liu.ida.hefquin.engine.queryplan.executable.impl.ops.UnaryExecutableOp;
+import se.liu.ida.hefquin.engine.queryplan.logical.UnaryLogicalOp;
+import se.liu.ida.hefquin.engine.queryplan.logical.impl.LogicalOpTPAdd;
+
+public class PhysicalOpBindJoinWithFILTER extends BasePhysicalOpSingleInputJoin {
+
+	protected PhysicalOpBindJoinWithFILTER(UnaryLogicalOp lop) {
+		super(lop);
+	}
+
+	@Override
+	public UnaryExecutableOp createExecOp(ExpectedVariables... inputVars) {
+		if ( lop instanceof LogicalOpTPAdd ) {
+			final LogicalOpTPAdd tpAdd = (LogicalOpTPAdd) lop;
+			final FederationMember fm = tpAdd.getFederationMember();
+
+			if ( fm instanceof SPARQLEndpoint )
+				return new ExecOpBindJoinSPARQLwiithFILTER( tpAdd.getTP(), (SPARQLEndpoint) fm );
+			else
+				throw new IllegalArgumentException("Unsupported type of federation member: " + fm.getClass().getName() );
+		}
+		else
+			throw new IllegalArgumentException("Unsupported type of operator: " + lop.getClass().getName() );
+	}
+
+}
