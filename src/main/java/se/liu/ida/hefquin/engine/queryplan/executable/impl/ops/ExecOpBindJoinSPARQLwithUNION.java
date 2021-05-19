8712b62a01c439758b1016f2074aa8a3a6ad7e05
scferrada <scferradaa@gmail.com>
2021-05-19 10:41:11 +0200

    implemented union-based bind join. all tests pass

--- src/main/java/se/liu/ida/hefquin/engine/queryplan/executable/impl/ops/ExecOpBindJoinSPARQLwithUNION.java
+++ src/main/java/se/liu/ida/hefquin/engine/queryplan/executable/impl/ops/ExecOpBindJoinSPARQLwithUNION.java
@@ -1,11 +1,23 @@
 package se.liu.ida.hefquin.engine.queryplan.executable.impl.ops;
 
+import java.util.Iterator;
 import java.util.Set;
 
+import org.apache.jena.graph.Node;
 import org.apache.jena.sparql.algebra.Op;
+import org.apache.jena.sparql.algebra.op.OpFilter;
+import org.apache.jena.sparql.algebra.op.OpTriple;
+import org.apache.jena.sparql.algebra.op.OpUnion;
 import org.apache.jena.sparql.core.Var;
+import org.apache.jena.sparql.engine.binding.Binding;
+import org.apache.jena.sparql.expr.E_Equals;
+import org.apache.jena.sparql.expr.E_LogicalAnd;
+import org.apache.jena.sparql.expr.Expr;
+import org.apache.jena.sparql.expr.ExprVar;
+import org.apache.jena.sparql.expr.nodevalue.NodeValueNode;
 
 import se.liu.ida.hefquin.engine.data.SolutionMapping;
+import se.liu.ida.hefquin.engine.data.impl.SolutionMappingUtils;
 import se.liu.ida.hefquin.engine.federation.SPARQLEndpoint;
 import se.liu.ida.hefquin.engine.federation.access.SPARQLRequest;
 import se.liu.ida.hefquin.engine.federation.access.SolMapsResponse;
@@ -36,7 +48,33 @@ public class ExecOpBindJoinSPARQLwithUNION extends ExecOpGenericBindJoin<TripleP
 	}
 
 	protected Op createUnion(final Iterable<SolutionMapping> solMaps) {
-		return null;
+		Op tp = new OpTriple(query.asJenaTriple());
+		if (varsInTP.isEmpty()) return tp;
+		Op union = null;
+		for ( final SolutionMapping s : solMaps) {
+			final Binding b = SolutionMappingUtils.restrict(s.asJenaBinding(), varsInTP);
+			if (b.size() == 0) continue;
+			Expr conjunction = null;
+			final Iterator<Var> vars = b.vars(); 
+			while (vars.hasNext()) {
+				final Var v = vars.next();
+				final Node uri = b.get(v);
+				final Expr expr = new E_Equals(new ExprVar(v), new NodeValueNode(uri));
+				if (conjunction == null) {
+					conjunction = expr;
+				} else {
+					conjunction = new E_LogicalAnd(conjunction, expr);
+				}
+			}
+			final Op filter = OpFilter.filter(conjunction, tp);
+			if (union == null) {
+				union = filter;
+			} else {
+				union = new OpUnion(union, filter);
+			}
+		}
+		if (union == null) return tp;
+		return union;
 	}
 
 }
