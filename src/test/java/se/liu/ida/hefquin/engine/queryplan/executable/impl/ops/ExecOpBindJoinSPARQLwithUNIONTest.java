ff71875bdb986c8b5558a1b3a047ceb0eb09ae9f
scferrada <scferradaa@gmail.com>
2021-05-19 10:34:05 +0200

    create required classes

---
+++ src/test/java/se/liu/ida/hefquin/engine/queryplan/executable/impl/ops/ExecOpBindJoinSPARQLwithUNIONTest.java
@@ -0,0 +1,51 @@
+package se.liu.ida.hefquin.engine.queryplan.executable.impl.ops;
+
+import org.apache.jena.graph.Graph;
+import org.junit.Test;
+
+import se.liu.ida.hefquin.engine.federation.SPARQLEndpoint;
+import se.liu.ida.hefquin.engine.query.TriplePattern;
+
+public class ExecOpBindJoinSPARQLwithUNIONTest extends TestsForTPAddAlgorithms<SPARQLEndpoint> {
+	
+	@Test
+	public void tpWithJoinOnObject() {
+		_tpWithJoinOnObject();
+	}
+
+	@Test
+	public void tpWithJoinOnSubjectAndObject() {
+		_tpWithJoinOnSubjectAndObject();
+	}
+
+	@Test
+	public void tpWithoutJoinVariable() {
+		_tpWithoutJoinVariable();
+	}
+
+	@Test
+	public void tpWithEmptyInput() {
+		_tpWithEmptyInput();
+	}
+
+	@Test
+	public void tpWithEmptySolutionMappingAsInput() {
+		_tpWithEmptySolutionMappingAsInput();
+	}
+
+	@Test
+	public void tpWithEmptyResponses() {
+		_tpWithEmptyResponses();
+	}
+
+	@Override
+	protected SPARQLEndpoint createFedMemberForTest( final Graph dataForMember) {
+		return new SPARQLEndpointForTest(dataForMember);
+	}
+
+	@Override
+	protected UnaryExecutableOp createExecOpForTest( final TriplePattern tp, final SPARQLEndpoint fm) {
+		return new ExecOpBindJoinSPARQLwithUNION(tp, fm);
+	}
+
+}
