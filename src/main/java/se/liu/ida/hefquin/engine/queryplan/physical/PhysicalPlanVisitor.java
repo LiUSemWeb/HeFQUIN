2cc1d8d433b74ff9d65a0531b51147cbe27e717a
scferrada <scferradaa@gmail.com>
2021-05-25 15:21:41 +0200

    fix of previous merge

--- src/main/java/se/liu/ida/hefquin/engine/queryplan/physical/PhysicalPlanVisitor.java
+++ src/main/java/se/liu/ida/hefquin/engine/queryplan/physical/PhysicalPlanVisitor.java
@@ -8,17 +8,11 @@ public interface PhysicalPlanVisitor {
     void visit(final PhysicalOpBindJoin op);
     void visit(final PhysicalOpBindJoinWithVALUES op);
     void visit(final PhysicalOpBindJoinWithFILTER op);
-<<<<<<< HEAD
-    void visit(final PhysicalOpBindJoinWithUNION physicalOpBindJoinWithUNION);
+    void visit(final PhysicalOpBindJoinWithUNION op);
     void visit(final PhysicalOpNaiveNestedLoopsJoin op);
     void visit(final PhysicalOpIndexNestedLoopsJoin op);
 
-    void visit(final PhysicalOpHashJoin physicalOpHashJoin);
-=======
-    void visit(final PhysicalOpNaiveNestedLoopsJoin op);
-    void visit(final PhysicalOpIndexNestedLoopsJoin op);
-
->>>>>>> 6e96eb9d74b6839c61a31117a25ad65797e00edd
+    void visit(final PhysicalOpHashJoin op);
     void visit(final PhysicalOpSymmetricHashJoin op);
 
     void visit(final PhysicalOpBinaryUnion op);
