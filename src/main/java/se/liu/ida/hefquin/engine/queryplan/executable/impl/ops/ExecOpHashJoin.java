49cf4ff6edcc625494a52d90832799efb47e165f
Sijin <sijin.cheng@liu.se>
2021-06-08 00:32:50 +0200

    update two operators: ExecOpHashJoin and ExecOpSymmetricHashJoin, use different SolutionMappingsIndex in different cases.

--- src/main/java/se/liu/ida/hefquin/engine/queryplan/executable/impl/ops/ExecOpHashJoin.java
+++ src/main/java/se/liu/ida/hefquin/engine/queryplan/executable/impl/ops/ExecOpHashJoin.java
@@ -4,9 +4,7 @@ import org.apache.jena.sparql.core.Var;
 import se.liu.ida.hefquin.engine.data.SolutionMapping;
 import se.liu.ida.hefquin.engine.data.impl.SolutionMappingUtils;
 import se.liu.ida.hefquin.engine.datastructures.SolutionMappingsIndex;
-import se.liu.ida.hefquin.engine.datastructures.impl.SolutionMappingsHashTable;
-import se.liu.ida.hefquin.engine.datastructures.impl.SolutionMappingsHashTableBasedOnOneVar;
-import se.liu.ida.hefquin.engine.datastructures.impl.SolutionMappingsHashTableBasedOnTwoVars;
+import se.liu.ida.hefquin.engine.datastructures.impl.*;
 import se.liu.ida.hefquin.engine.queryplan.ExpectedVariables;
 import se.liu.ida.hefquin.engine.queryplan.executable.IntermediateResultBlock;
 import se.liu.ida.hefquin.engine.queryplan.executable.IntermediateResultElementSink;
@@ -17,27 +15,36 @@ import java.util.Iterator;
 import java.util.List;
 
 public class ExecOpHashJoin implements BinaryExecutableOp {
-    protected final SolutionMappingsIndex solMHashTableL;
+    protected final SolutionMappingsIndex solMWithMatching;
     protected boolean child1InputComplete = false;
 
     public ExecOpHashJoin(final ExpectedVariables inputVars1, final ExpectedVariables inputVars2) {
         final List<Var> joinVars = new ArrayList<>( inputVars1.getCertainVariables() );
         joinVars.retainAll( inputVars2.getCertainVariables() );
 
+        SolutionMappingsIndex solMHashTable;
         if (joinVars.size() == 1 ){
             final Var joinVar = joinVars.iterator().next();
-            this.solMHashTableL = new SolutionMappingsHashTableBasedOnOneVar(joinVar);
+            solMHashTable = new SolutionMappingsHashTableBasedOnOneVar(joinVar);
         }
         else if (joinVars.size() == 2){
             final Iterator<Var> liVar = joinVars.iterator();
             final Var joinVar1 = liVar.next();
             final Var joinVar2 = liVar.next();
-
-            this.solMHashTableL = new SolutionMappingsHashTableBasedOnTwoVars(joinVar1, joinVar2);
+            solMHashTable = new SolutionMappingsHashTableBasedOnTwoVars(joinVar1, joinVar2);
         }
         else{
-            this.solMHashTableL = new SolutionMappingsHashTable(joinVars);
+            solMHashTable = new SolutionMappingsHashTable(joinVars);
         }
+
+        final List<Var> vars1 = new ArrayList<>( inputVars1.getCertainVariables() );
+        vars1.addAll(inputVars1.getPossibleVariables());
+        final List<Var> vars2 = new ArrayList<>( inputVars2.getCertainVariables() );
+        vars2.addAll(inputVars2.getPossibleVariables());
+        if (inputVars2.getPossibleVariables().removeAll(vars1)){
+            solMHashTable = new SolutionMappingsIndexWithPostMatching(solMHashTable);
+        }
+        this.solMWithMatching = solMHashTable;
     }
 
     @Override
@@ -53,7 +60,7 @@ public class ExecOpHashJoin implements BinaryExecutableOp {
     @Override
     public void processBlockFromChild1( final IntermediateResultBlock input, final IntermediateResultElementSink sink, final ExecutionContext execCxt ) {
         for ( final SolutionMapping smL : input.getSolutionMappings() ) {
-            solMHashTableL.add(smL);
+            solMWithMatching.add(smL);
         }
     }
 
@@ -68,7 +75,7 @@ public class ExecOpHashJoin implements BinaryExecutableOp {
             throw new IllegalStateException();
         }
         for ( final SolutionMapping smR : input.getSolutionMappings() ) {
-            final Iterable<SolutionMapping> matchSolMapL = solMHashTableL.getJoinPartners(smR);
+            final Iterable<SolutionMapping> matchSolMapL = solMWithMatching.getJoinPartners(smR);
             for ( final SolutionMapping smL : matchSolMapL ){
                 sink.send(SolutionMappingUtils.merge(smL, smR));
             }
--- src/main/java/se/liu/ida/hefquin/engine/queryplan/executable/impl/ops/ExecOpSymmetricHashJoin.java
+++ src/main/java/se/liu/ida/hefquin/engine/queryplan/executable/impl/ops/ExecOpSymmetricHashJoin.java
@@ -4,9 +4,7 @@ import org.apache.jena.sparql.core.Var;
 import se.liu.ida.hefquin.engine.data.SolutionMapping;
 import se.liu.ida.hefquin.engine.data.impl.SolutionMappingUtils;
 import se.liu.ida.hefquin.engine.datastructures.SolutionMappingsIndex;
-import se.liu.ida.hefquin.engine.datastructures.impl.SolutionMappingsHashTable;
-import se.liu.ida.hefquin.engine.datastructures.impl.SolutionMappingsHashTableBasedOnOneVar;
-import se.liu.ida.hefquin.engine.datastructures.impl.SolutionMappingsHashTableBasedOnTwoVars;
+import se.liu.ida.hefquin.engine.datastructures.impl.*;
 import se.liu.ida.hefquin.engine.queryplan.ExpectedVariables;
 import se.liu.ida.hefquin.engine.queryplan.executable.IntermediateResultBlock;
 import se.liu.ida.hefquin.engine.queryplan.executable.IntermediateResultElementSink;
@@ -16,30 +14,44 @@ import java.util.*;
 
 public class ExecOpSymmetricHashJoin implements BinaryExecutableOp{
 
-    protected final SolutionMappingsIndex solMHashTableL;
-    protected final SolutionMappingsIndex solMHashTableR;
+    protected final SolutionMappingsIndex solMWithMatchingL;
+    protected final SolutionMappingsIndex solMWithMatchingR;
 
     public ExecOpSymmetricHashJoin( final ExpectedVariables inputVars1, final ExpectedVariables inputVars2 ){
         final List<Var> joinVars = new ArrayList<>( inputVars1.getCertainVariables() );
         joinVars.retainAll( inputVars2.getCertainVariables() );
 
+        SolutionMappingsIndex solMHashTableL;
+        SolutionMappingsIndex solMHashTableR;
         if (joinVars.size() == 1 ){
             final Var joinVar = joinVars.iterator().next();
-            this.solMHashTableL = new SolutionMappingsHashTableBasedOnOneVar(joinVar);
-            this.solMHashTableR = new SolutionMappingsHashTableBasedOnOneVar(joinVar);
+            solMHashTableL = new SolutionMappingsHashTableBasedOnOneVar(joinVar);
+            solMHashTableR = new SolutionMappingsHashTableBasedOnOneVar(joinVar);
         }
         else if (joinVars.size() == 2){
             final Iterator<Var> liVar = joinVars.iterator();
             final Var joinVar1 = liVar.next();
             final Var joinVar2 = liVar.next();
 
-            this.solMHashTableL = new SolutionMappingsHashTableBasedOnTwoVars(joinVar1, joinVar2);
-            this.solMHashTableR = new SolutionMappingsHashTableBasedOnTwoVars(joinVar1, joinVar2);
+            solMHashTableL = new SolutionMappingsHashTableBasedOnTwoVars(joinVar1, joinVar2);
+            solMHashTableR = new SolutionMappingsHashTableBasedOnTwoVars(joinVar1, joinVar2);
         }
         else{
-            this.solMHashTableL = new SolutionMappingsHashTable(joinVars);
-            this.solMHashTableR = new SolutionMappingsHashTable(joinVars);
+            solMHashTableL = new SolutionMappingsHashTable(joinVars);
+            solMHashTableR = new SolutionMappingsHashTable(joinVars);
         }
+
+        final List<Var> vars1 = new ArrayList<>( inputVars1.getCertainVariables() );
+        vars1.addAll(inputVars1.getPossibleVariables());
+        final List<Var> vars2 = new ArrayList<>( inputVars2.getCertainVariables() );
+        vars2.addAll(inputVars2.getPossibleVariables());
+
+        if(inputVars1.getPossibleVariables().removeAll(vars2))
+            solMHashTableR = new SolutionMappingsIndexWithPostMatching(solMHashTableR);
+        if (inputVars2.getPossibleVariables().removeAll(vars1))
+            solMHashTableL = new SolutionMappingsIndexWithPostMatching(solMHashTableL);
+        this.solMWithMatchingL = new SolutionMappingsIndexForMixedUsage(solMHashTableL);
+        this.solMWithMatchingR = new SolutionMappingsIndexForMixedUsage(solMHashTableR);
     }
 
     @Override
@@ -55,9 +67,9 @@ public class ExecOpSymmetricHashJoin implements BinaryExecutableOp{
     @Override
     public void processBlockFromChild1( final IntermediateResultBlock input, final IntermediateResultElementSink sink, final ExecutionContext execCxt) {
         for ( final SolutionMapping smL : input.getSolutionMappings() ) {
-            solMHashTableL.add(smL);
+            solMWithMatchingL.add(smL);
 
-            final Iterable<SolutionMapping> matchSolMapR = solMHashTableR.getJoinPartners(smL);
+            final Iterable<SolutionMapping> matchSolMapR = solMWithMatchingR.getJoinPartners(smL);
             for ( final SolutionMapping smR : matchSolMapR ){
                 sink.send(SolutionMappingUtils.merge(smL, smR));
             }
@@ -72,9 +84,9 @@ public class ExecOpSymmetricHashJoin implements BinaryExecutableOp{
     @Override
     public void processBlockFromChild2(IntermediateResultBlock input, IntermediateResultElementSink sink, ExecutionContext execCxt) {
         for ( final SolutionMapping smR : input.getSolutionMappings() ) {
-            solMHashTableR.add(smR);
+            solMWithMatchingR.add(smR);
 
-            final Iterable<SolutionMapping> matchSolMapL = solMHashTableL.getJoinPartners(smR);
+            final Iterable<SolutionMapping> matchSolMapL = solMWithMatchingL.getJoinPartners(smR);
             for ( final SolutionMapping smL : matchSolMapL ){
                 sink.send(SolutionMappingUtils.merge(smL, smR));
             }
