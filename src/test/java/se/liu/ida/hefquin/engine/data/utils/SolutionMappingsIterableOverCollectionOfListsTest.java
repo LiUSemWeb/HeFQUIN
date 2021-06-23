b8b2e346803409d9678298e33b307b35d0c9c7a6
Olaf Hartig <olaf.hartig@liu.se>
2021-06-23 09:44:18 +0200

    additional unit tests in SolutionMappingsIterableOverCollectionOfListsTest

--- src/test/java/se/liu/ida/hefquin/engine/data/utils/SolutionMappingsIterableOverCollectionOfListsTest.java
+++ src/test/java/se/liu/ida/hefquin/engine/data/utils/SolutionMappingsIterableOverCollectionOfListsTest.java
@@ -1,42 +1,110 @@
 package se.liu.ida.hefquin.engine.data.utils;
 
-import org.apache.jena.sparql.engine.binding.Binding;
 import org.junit.Test;
 import se.liu.ida.hefquin.engine.data.SolutionMapping;
 
 import java.util.*;
 
-import static org.junit.Assert.*;
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 public class SolutionMappingsIterableOverCollectionOfListsTest extends TestsForSolutionMappingsIterableWithFilter
 {
     @Test
-    public void solMappingsIterableOverCollectionOfLists() {
+    public void oneList() {
         final Collection<List<SolutionMapping>> solMapCollection = new ArrayList<>();
         solMapCollection.add( getSolMapListWithTwoVar() );
 
         // Iterate over all solution mappings contained in a collection
         final Iterator<SolutionMapping> it = new SolutionMappingsIterableOverCollectionOfLists(solMapCollection).iterator();
 
-        assertTrue( it.hasNext() );
-        final Binding b1 = it.next().asJenaBinding();
-        assertEquals( 2, b1.size() );
-        assertEquals( "http://example.org/y1", b1.get(var2).getURI() );
-        assertEquals( "http://example.org/z1", b1.get(var3).getURI() );
+        assertHasNext( it, "http://example.org/y1", var2, "http://example.org/z1", var3 );
+        assertHasNext( it, "http://example.org/y1", var2, "http://example.org/z2", var3 );
+        assertHasNext( it, "http://example.org/y2", var2, "http://example.org/z3", var3 );
+        assertFalse( it.hasNext() );
+    }
 
-        assertTrue( it.hasNext() );
-        final Binding b2 = it.next().asJenaBinding();
-        assertEquals( 2, b2.size() );
-        assertEquals("http://example.org/y1", b2.get(var2).getURI());
-        assertEquals("http://example.org/z2", b2.get(var3).getURI());
+    @Test
+    public void twoListsSame() {
+        final Collection<List<SolutionMapping>> solMapCollection = new ArrayList<>();
+        solMapCollection.add( getSolMapListWithTwoVar() );
+        solMapCollection.add( getSolMapListWithTwoVar() );
 
-        assertTrue( it.hasNext() );
-        final Binding b3 = it.next().asJenaBinding();
-        assertEquals( 2, b3.size() );
-        assertEquals("http://example.org/y2", b3.get(var2).getURI());
-        assertEquals("http://example.org/z3", b3.get(var3).getURI());
+        // Iterate over all solution mappings contained in a collection
+        final Iterator<SolutionMapping> it = new SolutionMappingsIterableOverCollectionOfLists(solMapCollection).iterator();
+
+        assertHasNext( it, "http://example.org/y1", var2, "http://example.org/z1", var3 );
+        assertHasNext( it, "http://example.org/y1", var2, "http://example.org/z2", var3 );
+        assertHasNext( it, "http://example.org/y2", var2, "http://example.org/z3", var3 );
+
+        assertHasNext( it, "http://example.org/y1", var2, "http://example.org/z1", var3 );
+        assertHasNext( it, "http://example.org/y1", var2, "http://example.org/z2", var3 );
+        assertHasNext( it, "http://example.org/y2", var2, "http://example.org/z3", var3 );
 
         assertFalse( it.hasNext() );
     }
+
+    @Test
+    public void twoListsDifferent() {
+        final Collection<List<SolutionMapping>> solMapCollection = new ArrayList<>();
+        solMapCollection.add( getSolMapListWithTwoVar() );
+        solMapCollection.add( getSolMapListWithThreeVar() );
+
+        // Iterate over all solution mappings contained in a collection
+        final Iterator<SolutionMapping> it = new SolutionMappingsIterableOverCollectionOfLists(solMapCollection).iterator();
+
+        assertHasNext( it, "http://example.org/y1", var2, "http://example.org/z1", var3 );
+        assertHasNext( it, "http://example.org/y1", var2, "http://example.org/z2", var3 );
+        assertHasNext( it, "http://example.org/y2", var2, "http://example.org/z3", var3 );
+
+        assertHasNext( it, "http://example.org/y1", var2, "http://example.org/z1", var3, "http://example.org/x1", var1 );
+        assertHasNext( it, "http://example.org/y1", var2, "http://example.org/z2", var3, "http://example.org/x1", var1 );
+        assertHasNext( it, "http://example.org/y2", var2, "http://example.org/z2", var3, "http://example.org/x2", var1 );
+
+        assertFalse( it.hasNext() );
+    }
+
+    @Test
+    public void twoListsOneEmpty1() {
+        final Collection<List<SolutionMapping>> solMapCollection = new ArrayList<>();
+        solMapCollection.add( getSolMapListWithTwoVar() );
+        solMapCollection.add( new ArrayList<>() );  // <--- empty list second
+
+        // Iterate over all solution mappings contained in a collection
+        final Iterator<SolutionMapping> it = new SolutionMappingsIterableOverCollectionOfLists(solMapCollection).iterator();
+
+        assertHasNext( it, "http://example.org/y1", var2, "http://example.org/z1", var3 );
+        assertHasNext( it, "http://example.org/y1", var2, "http://example.org/z2", var3 );
+        assertHasNext( it, "http://example.org/y2", var2, "http://example.org/z3", var3 );
+
+        assertFalse( it.hasNext() );
+    }
+
+    @Test
+    public void twoListsOneEmpty2() {
+        final Collection<List<SolutionMapping>> solMapCollection = new ArrayList<>();
+        solMapCollection.add( new ArrayList<>() );  // <--- empty list first
+        solMapCollection.add( getSolMapListWithTwoVar() );
+
+        // Iterate over all solution mappings contained in a collection
+        final Iterator<SolutionMapping> it = new SolutionMappingsIterableOverCollectionOfLists(solMapCollection).iterator();
+
+        assertHasNext( it, "http://example.org/y1", var2, "http://example.org/z1", var3 );
+        assertHasNext( it, "http://example.org/y1", var2, "http://example.org/z2", var3 );
+        assertHasNext( it, "http://example.org/y2", var2, "http://example.org/z3", var3 );
+
+        assertFalse( it.hasNext() );
+    }
+
+    @Test
+    public void twoEmptyLists() {
+        final Collection<List<SolutionMapping>> solMapCollection = new ArrayList<>();
+        solMapCollection.add( new ArrayList<>() );
+        solMapCollection.add( new ArrayList<>() );
+
+        // Iterate over all solution mappings contained in a collection
+        final Iterator<SolutionMapping> it = new SolutionMappingsIterableOverCollectionOfLists(solMapCollection).iterator();
+
+        assertFalse( it.hasNext() );
+    }
+
 }
