4e603f93cfd38924a8bb46c10b6061f6ae358841
Sebastián Ferrada <scferradaa@gmail.com>
2021-06-30 15:00:51 +0200

    Update src/main/java/se/liu/ida/hefquin/engine/federation/access/Neo4jConnectionFactory.java
    Co-authored-by: Olaf Hartig <olaf.hartig@liu.se>

--- src/main/java/se/liu/ida/hefquin/engine/federation/access/Neo4jConnectionFactory.java
+++ src/main/java/se/liu/ida/hefquin/engine/federation/access/Neo4jConnectionFactory.java
@@ -13,7 +13,7 @@ public class Neo4jConnectionFactory {
     }
 
     public static class Neo4jConnection {
-        private String URL;
+        protected final String URL;
 
         public Neo4jConnection( final String URL ) {
             assert URL != null;
