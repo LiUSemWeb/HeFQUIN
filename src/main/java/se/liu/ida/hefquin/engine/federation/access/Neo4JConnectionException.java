d1bbd04bc0c1faa28f5ab520a1dc3530710b1b4e
Sebastián Ferrada <scferradaa@gmail.com>
2021-06-30 15:00:32 +0200

    Update src/main/java/se/liu/ida/hefquin/engine/federation/access/Neo4JConnectionException.java
    Co-authored-by: Olaf Hartig <olaf.hartig@liu.se>

--- src/main/java/se/liu/ida/hefquin/engine/federation/access/Neo4JConnectionException.java
+++ src/main/java/se/liu/ida/hefquin/engine/federation/access/Neo4JConnectionException.java
@@ -1,6 +1,7 @@
 package se.liu.ida.hefquin.engine.federation.access;
 
 public class Neo4JConnectionException extends RuntimeException {
-    public Neo4JConnectionException(String s) {
+    public Neo4JConnectionException( final String msg ) {
+        super(msg);
     }
 }
