a34260fd940fd87db4fb5fc10c50cda1aa840bde
Olaf Hartig <olaf.hartig@liu.se>
2021-06-30 18:05:08 +0200

    resolves merge conflict

--- src/test/java/se/liu/ida/hefquin/engine/federation/access/impl/FederationAccessManagerBaseTest.java
+++ src/test/java/se/liu/ida/hefquin/engine/federation/access/impl/FederationAccessManagerBaseTest.java
@@ -10,17 +10,21 @@ import org.junit.Test;
 import se.liu.ida.hefquin.engine.EngineTestBase;
 import se.liu.ida.hefquin.engine.federation.BRTPFServer;
 import se.liu.ida.hefquin.engine.federation.FederationAccessManager;
+import se.liu.ida.hefquin.engine.federation.Neo4jServer;
 import se.liu.ida.hefquin.engine.federation.SPARQLEndpoint;
 import se.liu.ida.hefquin.engine.federation.TPFServer;
 import se.liu.ida.hefquin.engine.federation.access.BRTPFRequest;
 import se.liu.ida.hefquin.engine.federation.access.CardinalityResponse;
+import se.liu.ida.hefquin.engine.federation.access.Neo4jRequest;
 import se.liu.ida.hefquin.engine.federation.access.SPARQLRequest;
+import se.liu.ida.hefquin.engine.federation.access.StringRetrievalResponse;
 import se.liu.ida.hefquin.engine.federation.access.TPFRequest;
 import se.liu.ida.hefquin.engine.federation.access.TPFResponse;
 import se.liu.ida.hefquin.engine.federation.access.impl.req.BGPRequestImpl;
 import se.liu.ida.hefquin.engine.federation.access.impl.req.SPARQLRequestImpl;
 import se.liu.ida.hefquin.engine.federation.access.impl.req.TriplePatternRequestImpl;
 import se.liu.ida.hefquin.engine.federation.access.impl.reqproc.BRTPFRequestProcessor;
+import se.liu.ida.hefquin.engine.federation.access.impl.reqproc.Neo4jRequestProcessor;
 import se.liu.ida.hefquin.engine.federation.access.impl.reqproc.SPARQLRequestProcessor;
 import se.liu.ida.hefquin.engine.federation.access.impl.reqproc.SPARQLRequestProcessorImpl;
 import se.liu.ida.hefquin.engine.federation.access.impl.reqproc.TPFRequestProcessor;
@@ -81,7 +85,7 @@ public class FederationAccessManagerBaseTest extends EngineTestBase
 	{
 		final SPARQLEndpoint fm = new SPARQLEndpointForTest("http://dbpedia.org/sparql");
 
-		final SPARQLRequestProcessor recProc = new SPARQLRequestProcessorImpl();
+		final SPARQLRequestProcessor reqProc = new SPARQLRequestProcessorImpl();
 		final TPFRequestProcessor reqProcTPF = new TPFRequestProcessor() {
 			@Override public TPFResponse performRequest(TPFRequest req, BRTPFServer fm) { return null; }
 			@Override public TPFResponse performRequest(TPFRequest req, TPFServer fm) { return null; }
@@ -89,8 +93,11 @@ public class FederationAccessManagerBaseTest extends EngineTestBase
 		final BRTPFRequestProcessor reqProcBRTPF = new BRTPFRequestProcessor() {
 			@Override public TPFResponse performRequest(BRTPFRequest req, BRTPFServer fm) { return null; }
 		};
+		final Neo4jRequestProcessor reqProcNeo4j = new Neo4jRequestProcessor() {
+			@Override public StringRetrievalResponse performRequest(Neo4jRequest req, Neo4jServer fm) { return null; }
+		};
 
-		final FederationAccessManager mgr = new BlockingFederationAccessManagerImpl(recProc, reqProcTPF, reqProcBRTPF);
+		final FederationAccessManager mgr = new BlockingFederationAccessManagerImpl(reqProc, reqProcTPF, reqProcBRTPF, reqProcNeo4j);
 
 		// executing the tested method
 		final CardinalityResponse resp = mgr.performCardinalityRequest(req, fm);
