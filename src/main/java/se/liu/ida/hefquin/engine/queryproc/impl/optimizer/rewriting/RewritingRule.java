999dcc383f179beacc1cceb38b2d2d568dbdeda7
Sijin <sijin.cheng@liu.se>
2021-08-17 18:06:04 +0200

    rename the interface to RewritingRule

---
+++ src/main/java/se/liu/ida/hefquin/engine/queryproc/impl/optimizer/rewriting/RewritingRule.java
@@ -0,0 +1,13 @@
+package se.liu.ida.hefquin.engine.queryproc.impl.optimizer.rewriting;
+
+import se.liu.ida.hefquin.engine.queryplan.PhysicalPlan;
+
+public interface RewritingRule {
+
+    boolean canBeAppliedTo( final PhysicalPlan plan );
+
+    PhysicalPlan applyTo( final PhysicalPlan plan, final PhysicalPlan[] subPlans );
+
+    double getPriority();
+
+}
