2ee494f48f317510e0d8393ab2729b1dd7599ba8
sijin <sijin.cheng@liu.se>
2021-08-27 14:52:06 +0200

    add Group number for rule instances;

--- src/main/java/se/liu/ida/hefquin/engine/queryproc/impl/optimizer/rewriting/RuleInstances.java
+++ src/main/java/se/liu/ida/hefquin/engine/queryproc/impl/optimizer/rewriting/RuleInstances.java
@@ -9,7 +9,8 @@ public class RuleInstances {
     Set<RewritingRule> ruleApplications = new HashSet<>();
 
     public void addRuleInstances() {
-        // 1. Convert TPAdd to binary join (category: C)
+
+        // Group 1. Convert TPAdd to binary join (category: C)
         // Convert TPAdd to Hash Join
         ruleApplications.add( new RuleConvertTPAddIndexNLJToHashJoin(0.15) );
         ruleApplications.add( new RuleConvertTPAddBJFILTERToHashJoin(0.15) );
@@ -31,7 +32,7 @@ public class RuleInstances {
         ruleApplications.add( new RuleConvertTPAddBJVALUESToNaiveNLJ(0.15) );
         ruleApplications.add( new RuleConvertTPAddBindJoinToNaiveNLJ(0.15) );
 
-        // 2. Conversion of physical algorithms of TPAdd (category: C)
+        // Group 2. Conversion of physical algorithms of TPAdd (category: C)
         // For TPAdd, convert other types of physical algorithm to IndexNLJ
         ruleApplications.add( new RuleConvertTPAddBindJoinToIndexNLJ(0.2) );
         ruleApplications.add( new RuleConvertTPAddBJFILTERToIndexNLJ(0.2) );
@@ -45,7 +46,7 @@ public class RuleInstances {
         ruleApplications.add( new RuleConvertTPAddIndexNLJToBJVALUES(0.2) );
         // TODO: more rules to be added (conversion between different algorithms of bind join)
 
-        // 3. Order tweaking of two TPAdd (category: B),
+        // Group 3. Order tweaking of two TPAdd (category: B),
         // Equation (22)
         ruleApplications.add( new RuleChangeOrderOfTwoTPAddIndexNLJ(0.25) );
         ruleApplications.add( new RuleChangeOrderOfTwoTPAddBindJoin(0.25) );
@@ -63,19 +64,19 @@ public class RuleInstances {
         ruleApplications.add( new RuleChangeOrderOfTPAddIndexNLJAndTPAddBJUNION(0.25) );
         ruleApplications.add( new RuleChangeOrderOfTPAddIndexNLJAndTPAddBJVALUES(0.25) );
 
-        // 4. Order tweaking of TPAdd and BGPAdd (category: B)
+        // Group 4. Order tweaking of TPAdd and BGPAdd (category: B)
         // Equation (23)
         ruleApplications.add( new RuleChangeOrderOfTPAddIndexNLJAndBGPAddIndexNLJ(0.25) );
         // TODO: more rules to be added (different variations of physical algorithm)
 
-        // 5. Merge a TPAdd and a BGP request (with the same fm) into one request (category: A), B' = B U {tp}
+        // Group 5. Merge a TPAdd and a BGP request (with the same fm) into one request (category: A), B' = B U {tp}
         // Equation (13)
         ruleApplications.add( new RuleMergeTPAddIndexNLJAndBGPReqIntoOneRequest(0.3) );
         ruleApplications.add( new RuleMergeTPAddBJFILTERAndBGPReqIntoOneRequest(0.3) );
         ruleApplications.add( new RuleMergeTPAddBJUNIONAndBGPReqIntoOneRequest(0.3) );
         ruleApplications.add( new RuleMergeTPAddBJVALUESAndBGPReqIntoOneRequest(0.3) );
 
-        // 6. Merge a tpAdd and a bgpAdd (with the same fm) into one bgpAdd (category: A), B' = B U {tp}
+        // Group 6. Merge a tpAdd and a bgpAdd (with the same fm) into one bgpAdd (category: A), B' = B U {tp}
         // Equation (14)
         ruleApplications.add( new RuleMergeTPAddIndexNLJAndBGPAddIndexNLJIntoBGPAdd(0.3) );
         ruleApplications.add( new RuleMergeTPAddBJFILTERAndBGPAddIndexNLJIntoBGPAdd(0.3) );
@@ -83,7 +84,7 @@ public class RuleInstances {
         ruleApplications.add( new RuleMergeTPAddBJVALUESAndBGPAddIndexNLJIntoBGPAdd(0.3) );
         // TODO: more rules to be added (different physical algorithms for bgpAdd)
 
-        // 7. Merge a tpAdd and a graph pattern request (with the same fm: SPARQL endpoint) into one request (category: A),  (P AND tp)
+        // Group 7. Merge a tpAdd and a graph pattern request (with the same fm: SPARQL endpoint) into one request (category: A),  (P AND tp)
         // Equation (20)
         ruleApplications.add( new RuleMergeTPAddIndexNLJAndGraphPatternReqIntoOneRequest(0.3) );
         ruleApplications.add( new RuleMergeTPAddBJFILTERAndGraphPatternReqIntoOneRequest(0.3) );
