b741857081f5b3046b7d8438a4899c4b1a5b70b9
sijin <sijin.cheng@liu.se>
2021-09-10 15:48:42 +0200

    Rewriting rules for bgpAdd; Group 2.2: Conversion of physical algorithms of bgpAdd (category: C)

---
+++ src/main/java/se/liu/ida/hefquin/engine/queryproc/impl/optimizer/rewriting/rules/RuleConvertBGPAddToBJFILTER.java
@@ -0,0 +1,40 @@
+package se.liu.ida.hefquin.engine.queryproc.impl.optimizer.rewriting.rules;
+
+import se.liu.ida.hefquin.engine.federation.FederationMember;
+import se.liu.ida.hefquin.engine.federation.SPARQLEndpoint;
+import se.liu.ida.hefquin.engine.queryplan.PhysicalOperator;
+import se.liu.ida.hefquin.engine.queryplan.PhysicalPlan;
+import se.liu.ida.hefquin.engine.queryplan.logical.impl.LogicalOpBGPAdd;
+import se.liu.ida.hefquin.engine.queryplan.logical.impl.LogicalOpTPAdd;
+import se.liu.ida.hefquin.engine.queryplan.physical.PhysicalOperatorForLogicalOperator;
+import se.liu.ida.hefquin.engine.queryplan.utils.PhysicalPlanFactory;
+import se.liu.ida.hefquin.engine.queryproc.impl.optimizer.rewriting.RuleApplication;
+
+public class RuleConvertBGPAddToBJFILTER extends AbstractRewritingRuleImpl{
+
+    public RuleConvertBGPAddToBJFILTER( final double priority ) {
+        super(priority);
+    }
+
+    @Override
+    protected boolean canBeAppliedTo( final PhysicalPlan plan ) {
+        final PhysicalOperator rootOp = plan.getRootOperator();
+        return IdentifyPhysicalOpUsedForBGPAdd.isIndexNLJ(rootOp)
+                || IdentifyPhysicalOpUsedForBGPAdd.isBindJoinUNION(rootOp)
+                || IdentifyPhysicalOpUsedForBGPAdd.isBindJoinVALUES(rootOp);
+
+    }
+
+    @Override
+    protected RuleApplication createRuleApplication( final PhysicalPlan[] pathToTargetPlan ) {
+        return new AbstractRuleApplicationImpl(pathToTargetPlan, this) {
+            @Override
+            protected PhysicalPlan rewritePlan( final PhysicalPlan plan ) {
+                final PhysicalOperatorForLogicalOperator rootOp = (PhysicalOperatorForLogicalOperator) plan.getRootOperator();
+                final LogicalOpBGPAdd lop = (LogicalOpBGPAdd) rootOp.getLogicalOperator();
+                return PhysicalPlanFactory.createPlan( lop , plan.getSubPlan(0) );
+            }
+        };
+    }
+
+}
