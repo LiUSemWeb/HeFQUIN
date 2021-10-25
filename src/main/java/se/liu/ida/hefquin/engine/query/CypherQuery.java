101436901aaef7d59ff01f93defd7c16a1ecaf32
scferrada <scferradaa@gmail.com>
2021-10-25 17:06:54 +0200

    added javadoc for the cypher query interfaces

--- src/main/java/se/liu/ida/hefquin/engine/query/CypherQuery.java
+++ src/main/java/se/liu/ida/hefquin/engine/query/CypherQuery.java
@@ -4,7 +4,19 @@ import se.liu.ida.hefquin.engine.query.cypher.CypherVar;
 
 import java.util.Set;
 
+/**
+ * Represents a Query written in the Cypher Query Language
+ */
 public interface CypherQuery extends Query {
+    /**
+     * Creates the string representation of the modelled Cypher query
+     * @return a Cypher query string
+     */
     String toString();
+
+    /**
+     * Obtains the set of variables that are defined in the MATCH clauses of the query
+     * @return a Set of CypherVar objects
+     */
     Set<CypherVar> getMatchVars();
 }
