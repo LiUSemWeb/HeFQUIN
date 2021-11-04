762159e66999e4257134a3c261e355ca056de74b
scferrada <scferradaa@gmail.com>
2021-11-04 11:34:50 +0100

    added reverse map functions

--- src/main/java/se/liu/ida/hefquin/engine/wrappers/lpgwrapper/LPG2RDFConfiguration.java
+++ src/main/java/se/liu/ida/hefquin/engine/wrappers/lpgwrapper/LPG2RDFConfiguration.java
@@ -1,5 +1,7 @@
 package se.liu.ida.hefquin.engine.wrappers.lpgwrapper;
 
+import se.liu.ida.hefquin.engine.wrappers.lpgwrapper.data.impl.LPGNode;
+
 /**
  * Represents an LPG-to-RDF configuration as defined in Hartig, Olaf. "Foundations to Query Labeled Property
  * Graphs using SPARQL." SEM4TRA-AMAR@ SEMANTICS. 2019.
@@ -15,10 +17,16 @@ public interface LPG2RDFConfiguration {
 
     /**
      * Returns an IRI or BNode label for the given node identifier.
-     * @param nodeID the id of the node to be mapped
+     * @param node the id of the node to be mapped
      * @return a String with the IRI/BNode
      */
-    String mapNode(final String nodeID);
+    String mapNode(final LPGNode node);
+
+    /**
+     * Returns the corresponding LPGNode of a given IRI
+     * @param iri the IRI to be unmapped
+     */
+    LPGNode unmapNode(final String iri);
 
     /**
      * Returns an IRI for the given node label
@@ -28,6 +36,12 @@ public interface LPG2RDFConfiguration {
     String mapNodeLabel(final String label);
 
     /**
+     * Returns the original  node label for the given mapped label
+     * @param label the mapped IRI or literal of a node label
+     */
+    String unmapNodeLabel(final String label);
+
+    /**
      * Returns an IRI for a given edge label
      * @param label the edge label to map
      * @return a String with the IRI
@@ -35,11 +49,23 @@ public interface LPG2RDFConfiguration {
     String mapEdgeLabel(final String label);
 
     /**
+     * Returns the original edge label for the given mapped label
+     * @param label the mapped IRI or literal of an edge label
+     */
+    String unmapEdgeLabel(final String label);
+
+    /**
      * Returns an IRI for a given property
      * @param property the name of the property to map
      * @return a String with the IRI
      */
     String mapProperty(final String property);
+
+    /**
+     * Returns the original property name of the given mapped IRI
+     * @param iri the mapped IRI of a property
+     */
+    String unmapProperty(final String iri);
 
     /**
      * Returns the IRI of the property defined as u_label
--- src/main/java/se/liu/ida/hefquin/engine/wrappers/lpgwrapper/impl/DefaultConfiguration.java
+++ src/main/java/se/liu/ida/hefquin/engine/wrappers/lpgwrapper/impl/DefaultConfiguration.java
@@ -1,6 +1,7 @@
 package se.liu.ida.hefquin.engine.wrappers.lpgwrapper.impl;
 
 import se.liu.ida.hefquin.engine.wrappers.lpgwrapper.LPG2RDFConfiguration;
+import se.liu.ida.hefquin.engine.wrappers.lpgwrapper.data.impl.LPGNode;
 
 public class DefaultConfiguration implements LPG2RDFConfiguration {
 
@@ -12,8 +13,14 @@ public class DefaultConfiguration implements LPG2RDFConfiguration {
     protected final String LABEL = "http://www.w3.org/2000/01/rdf-schema#Label";
 
     @Override
-    public String mapNode(final String nodeID) {
-        return NS + NODE + nodeID;
+    public String mapNode(final LPGNode node) {
+        return NS + NODE + node.getId();
+    }
+
+    @Override
+    public LPGNode unmapNode(String iri) {
+        final String id = iri.replaceAll(NS + NODE, "");
+        return new LPGNode(id, "", null);
     }
 
     @Override
@@ -22,13 +29,28 @@ public class DefaultConfiguration implements LPG2RDFConfiguration {
     }
 
     @Override
+    public String unmapNodeLabel(String label) {
+        return label.replaceAll(NS + NODELABEL, "");
+    }
+
+    @Override
     public String mapEdgeLabel(final String label) {
         return NS + RELATIONSHIP + label;
     }
 
     @Override
+    public String unmapEdgeLabel(String label) {
+        return label.replaceAll(NS + RELATIONSHIP, "");
+    }
+
+    @Override
     public String mapProperty(final String property) {
         return NS + PROPERTY + property;
+    }
+
+    @Override
+    public String unmapProperty(String iri) {
+        return iri.replaceAll(NS + PROPERTY, "");
     }
 
     @Override
