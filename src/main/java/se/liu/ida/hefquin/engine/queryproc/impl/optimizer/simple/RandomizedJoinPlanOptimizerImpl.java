b7641ace66a021c45cc5bd15e7ecee58d5dbf010
= <=>
2021-11-19 15:45:19 +0100

    Instead of extending JoinPlanOptimizerBase, this now implements the JoinPlanOptimizer interface. A lot of fat has been trimmed away here and I've sought to minimize the amount of function calls and memory allocations so as to increase the performance of the algorithm.

--- src/main/java/se/liu/ida/hefquin/engine/queryproc/impl/optimizer/simple/RandomizedJoinPlanOptimizerImpl.java
+++ src/main/java/se/liu/ida/hefquin/engine/queryproc/impl/optimizer/simple/RandomizedJoinPlanOptimizerImpl.java
@@ -7,77 +7,44 @@ import se.liu.ida.hefquin.engine.queryplan.PhysicalPlan;
 import se.liu.ida.hefquin.engine.queryplan.utils.PhysicalPlanFactory;
 import se.liu.ida.hefquin.engine.queryproc.QueryOptimizationException;
 
-public class RandomizedJoinPlanOptimizerImpl extends JoinPlanOptimizerBase {
-	@Override // Implement initializeEnumerationAlgorithm from JoinPlanOptimizerBase.
-	public EnumerationAlgorithm initializeEnumerationAlgorithm( final List<PhysicalPlan> subplans ) {
-		return new RandomizedEnumerationAlgorithm(subplans);
+public class RandomizedJoinPlanOptimizerImpl implements JoinPlanOptimizer {
+	
+	/**
+	 * The class contains a random seed object.
+	 * This cannot be marked final (compilation error if so).
+	 */
+	protected Random random = new Random();
+	
+	/**
+	 * Have a list of subplans.
+	 */
+	protected final List<PhysicalPlan> subplans;
+	
+	/**
+	 *  When initializing the algorithm, start with the given list of subplans.
+	 *  @param subplans
+	 */
+	public RandomizedJoinPlanOptimizerImpl( final List<PhysicalPlan> subplans ) {
+		this.subplans = subplans;
+		this.random = new Random(); // Generates the random seed.
 	}
 	
-	protected class RandomizedEnumerationAlgorithm implements EnumerationAlgorithm {
-		
-		/**
-		 * The class contains a random seed object.
-		 */
-		protected final Random random = new Random();
-		
-		/**
-		 * Have a list of subplans.
-		 */
-		protected final List<PhysicalPlan> subplans;
 
-		/**
-		 *  When initializing the algorithm, start with the given list of subplans.
-		 *  @param subplans
-		 */
-		public RandomizedEnumerationAlgorithm( final List<PhysicalPlan> subplans ) {
-			this.subplans = subplans;
-			this.random = new Random(); // Generates the random seed.
+	@Override
+	public PhysicalPlan determineJoinPlan(List<PhysicalPlan> subplans) throws QueryOptimizationException {
+
+		int indexOfRandomPlan = random.nextInt(subplans.size()); // nextInt returns an int between 0 (inclusive) and size (exclusive).
+
+		PhysicalPlan currentPlan = subplans.get(indexOfRandomPlan); // Picks out one plan to be the first, at random.
+		subplans.remove(indexOfRandomPlan); // Then removes that from the subplans.
+
+		while ( subplans.size() > 0 ){ // After a first plan is picked, runs this loop until the subplans are exhausted.
+			indexOfRandomPlan = random.nextInt(subplans.size());  // Picks a plan at random.
+			currentPlan = PhysicalPlanFactory.createPlanWithJoin( currentPlan, subplans.get(indexOfRandomPlan)); // Joins it with the current plan.
+			subplans.remove(indexOfRandomPlan);	// Remove the joined plan.
 		}
-		
 
-		/**
-		 * Also taken from Greedy and left unchanged save for the different method being called within.
-		 */
-		@Override
-		public PhysicalPlan getResultingPlan() throws QueryOptimizationException {
-			PhysicalPlan currentPlan = chooseFirstSubplan();
-
-			while ( subplans.size() > 0 ){
-				currentPlan = addNextRandomJoin(currentPlan);
-			}
-
-			return currentPlan;
-		}
-		
-
-		/**
-		 * Returns one of the subplans which is picked at random.
-		 */
-		protected PhysicalPlan chooseFirstSubplan() throws QueryOptimizationException {
-			final int indexOfRandomPlan = random.nextInt(subplans.size()); // nextInt returns an int between 0 (inclusive) and size (exclusive).
-
-			final PhysicalPlan randomPlan = subplans.get(indexOfRandomPlan);
-			subplans.remove(indexOfRandomPlan);
-			return randomPlan;
-		}
-		
-
-		/**
-		 * Next random join.
-		 * @param currentPlan
-		 * @return
-		 * @throws QueryOptimizationException
-		 */
-		protected PhysicalPlan addNextRandomJoin( final PhysicalPlan currentPlan )
-				throws QueryOptimizationException
-		{
-			final int indexOfRandomPlan = random.nextInt(subplans.size());
-			final PhysicalPlan nextPlan = PhysicalPlanFactory.createPlanWithJoin( currentPlan, subplans.get(indexOfRandomPlan));
-			subplans.remove(indexOfRandomPlan);			
-			return nextPlan;
-		}
-		
+		// At this stage, everything is done, and our working plan is now the final plan.
+		return currentPlan;
 	}
-	
-	
 }
