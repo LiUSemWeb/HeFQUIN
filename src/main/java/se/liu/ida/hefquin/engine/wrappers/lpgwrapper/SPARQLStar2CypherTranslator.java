330e41f7f112d77393a2a9898b26e6847b5744f5
scferrada <scferradaa@gmail.com>
2021-11-25 11:18:12 +0100

    fixed translator interface JavaDoc

--- src/main/java/se/liu/ida/hefquin/engine/wrappers/lpgwrapper/SPARQLStar2CypherTranslator.java
+++ src/main/java/se/liu/ida/hefquin/engine/wrappers/lpgwrapper/SPARQLStar2CypherTranslator.java
@@ -7,7 +7,8 @@ public interface SPARQLStar2CypherTranslator {
 
     /**
      * Translates the given Triple Pattern into a Cypher query, using a given LPG2RDFConfiguration.
-     * If the Triple Pattern is incompatible with the configuration, this method returns null.
+     * If the Triple Pattern has a shape for which the configuration-specific RDF-star view of the
+     * LPG is guaranteed to obtain no matching triples, this method returns null.
      */
     CypherQuery translateTriplePattern(final TriplePattern tp, final LPG2RDFConfiguration conf);
 }
