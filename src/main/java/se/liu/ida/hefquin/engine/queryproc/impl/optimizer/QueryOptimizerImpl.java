3d8158f5793af0a964ed08bb25e27f17702bcd8a
sijin <sijin.cheng@liu.se>
2021-12-06 18:55:37 +0100

    Apply different optimization algorithms in QueryOptimizer: evolutionary algorithm, dynamic programming algorithm, greedy algorithm

--- src/main/java/se/liu/ida/hefquin/engine/queryproc/impl/optimizer/QueryOptimizerImpl.java
+++ src/main/java/se/liu/ida/hefquin/engine/queryproc/impl/optimizer/QueryOptimizerImpl.java
@@ -4,6 +4,13 @@ import se.liu.ida.hefquin.engine.queryplan.LogicalPlan;
 import se.liu.ida.hefquin.engine.queryplan.PhysicalPlan;
 import se.liu.ida.hefquin.engine.queryproc.QueryOptimizationException;
 import se.liu.ida.hefquin.engine.queryproc.QueryOptimizer;
+import se.liu.ida.hefquin.engine.queryproc.impl.optimizer.evolutionaryAlgorithm.EvolutionaryAlgorithmQueryOptimizer;
+import se.liu.ida.hefquin.engine.queryproc.impl.optimizer.evolutionaryAlgorithm.TerminatedByNumberOfGenerations;
+import se.liu.ida.hefquin.engine.queryproc.impl.optimizer.evolutionaryAlgorithm.TerminationCriterion;
+import se.liu.ida.hefquin.engine.queryproc.impl.optimizer.simple.DPBasedJoinPlanOptimizer;
+import se.liu.ida.hefquin.engine.queryproc.impl.optimizer.simple.GreedyJoinPlanOptimizerImpl;
+import se.liu.ida.hefquin.engine.queryproc.impl.optimizer.simple.JoinPlanOptimizer;
+import se.liu.ida.hefquin.engine.queryproc.impl.optimizer.simple.SimpleJoinOrderingQueryOptimizer;
 
 public class QueryOptimizerImpl implements QueryOptimizer
 {
@@ -18,11 +25,31 @@ public class QueryOptimizerImpl implements QueryOptimizer
 	public PhysicalPlan optimize( final LogicalPlan initialPlan )
 			throws QueryOptimizationException
 	{
+		// TODO query optimization: currently, different optimization algorithms can be applied by uncommenting the corresponding part of the code below.
+		// TODO adds an argument to the command line that allows us to choose which algorithm to use
+
 		final boolean keepMultiwayJoins = false;
 		final PhysicalPlan initialPhysicalPlan = ctxt.getLogicalToPhysicalPlanConverter().convert(initialPlan, keepMultiwayJoins);
 		return initialPhysicalPlan;
 
-		// TODO implement query optimization
+		// Uncomment the following part to apply an evolutionary algorithm for query optimization:
+/*
+		final TerminationCriterion termination = new TerminatedByNumberOfGenerations(20);
+		final QueryOptimizer queryOptimize = new EvolutionaryAlgorithmQueryOptimizer(ctxt, 8, 2, termination);
+		return queryOptimize.optimize( initialPlan );
+*/
+
+		// Uncomment the following part to apply a dynamic programming algorithm or greedy algorithm for query optimization:
+/*
+		// Greedy algorithm:
+		final JoinPlanOptimizer joinOpti = new GreedyJoinPlanOptimizerImpl( ctxt.getCostModel() );
+		// Dynamic programming algorithm:
+		// final JoinPlanOptimizer joinOpti = new DPBasedJoinPlanOptimizer( ctxt );
+
+		final QueryOptimizer queryOptimize = new SimpleJoinOrderingQueryOptimizer( joinOpti, ctxt );
+		return queryOptimize.optimize( initialPlan );
+ */
+
 	}
 
 }
