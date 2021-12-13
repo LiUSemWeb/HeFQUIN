eccbb1efd97f75556e49a63aa6a168dd4a489040
SHSenpai <Senpai@oniichan.eu>
2021-12-13 13:47:25 +0100

    Returns to only using initianPlan instead of the Randomizer and uses the PhysicalPlanWithCost.

--- src/main/java/se/liu/ida/hefquin/engine/queryproc/impl/optimizer/randomized/IterativeImprovementBasedQueryOptimizer.java
+++ src/main/java/se/liu/ida/hefquin/engine/queryproc/impl/optimizer/randomized/IterativeImprovementBasedQueryOptimizer.java
@@ -6,9 +6,9 @@ import se.liu.ida.hefquin.engine.queryplan.PhysicalPlan;
 import se.liu.ida.hefquin.engine.queryproc.QueryOptimizationException;
 import se.liu.ida.hefquin.engine.queryproc.impl.optimizer.QueryOptimizationContext;
 import se.liu.ida.hefquin.engine.queryproc.impl.optimizer.rewriting.RuleInstances;
-import se.liu.ida.hefquin.engine.queryproc.impl.optimizer.simple.RandomizedJoinPlanOptimizerImpl;
 import se.liu.ida.hefquin.engine.queryproc.impl.optimizer.utils.CostEstimationUtils;
 import se.liu.ida.hefquin.engine.queryproc.impl.optimizer.utils.PhysicalPlanWithCost;
+import se.liu.ida.hefquin.engine.queryproc.impl.optimizer.utils.PhysicalPlanWithCostUtils;
 
 public class IterativeImprovementBasedQueryOptimizer extends RandomizedQueryOptimizerBase
 {
@@ -25,60 +25,45 @@ public class IterativeImprovementBasedQueryOptimizer extends RandomizedQueryOpti
 
 	@Override
 	public PhysicalPlan optimize( final PhysicalPlan initialPlan ) throws QueryOptimizationException {
-		// The best plan we have found so far. As we have only found one plan, it is the best one so far.
-		PhysicalPlan bestPlan = initialPlan;
-
-		final Double initialCost = CostEstimationUtils.getEstimates( context.getCostModel(), initialPlan )[0];
+		// The best plan and cost we have found so far. As we have only found one plan, it is the best one so far.
+		PhysicalPlanWithCost bestPlan = new PhysicalPlanWithCost( initialPlan, CostEstimationUtils.getEstimates( context.getCostModel(), initialPlan )[0]);
 
 		// generation = number of times the outer loop has run. Has to be declared here since it will increment each outer loop.
 		int generation = 0;
 
-		// bestCost = best possible cost out of everything that the algorithm has found. Has to be declared here so that we have something to compare to in all the iterations of the outer loop without losing track of it between loops.
-		Double bestCost = initialCost;
-
 		while ( !condition.readyToStop(generation) ) { // Currently only handles generation number as a stopping condition!
 			// The randomized plan generator is to be used here. As a temporary measure, the initial plan is used.
-			/*
-			PhysicalPlan currentPlan = initialPlan; // This variable will hold the plan which is currently being worked on.
-			*/
-			final RandomizedJoinPlanOptimizerImpl planRandomizer = new RandomizedJoinPlanOptimizerImpl();
-			PhysicalPlan currentPlan = planRandomizer.determineJoinPlan(getNeighbours(initialPlan));
+			PhysicalPlanWithCost currentPlan = new PhysicalPlanWithCost(initialPlan, CostEstimationUtils.getEstimates( context.getCostModel(), initialPlan )[0]); // This variable will hold the plan which is currently being worked on.
 			
-			Double currentCost = CostEstimationUtils.getEstimates( context.getCostModel(), currentPlan )[0]; // The cost of the current plan. For it to be a local minimum, none of its neighbours can have a lower cost
 			boolean improvementFound = false;
 
 			do {
-				final List<PhysicalPlan> neighbours = getNeighbours(currentPlan);
-				final Double[] neighbourCosts = CostEstimationUtils.getEstimates( context.getCostModel(), neighbours );
+				final List<PhysicalPlanWithCost> neighbours = PhysicalPlanWithCostUtils.annotatePlansWithCost( context.getCostModel(), getNeighbours(currentPlan.getPlan()));
 				final List<PhysicalPlanWithCost> betterPlans = new ArrayList<>();
 				improvementFound = false;
 
 				// Using a for-loop in order to have the index for which neighbouring plan to pick.
-				for (int x = 0; x < neighbourCosts.length; x++) {
-					if(neighbourCosts[x] < currentCost) {
+				for (int x = 0; x < neighbours.size(); x++) {
+					if(neighbours.get(x).getWeight() < currentPlan.getWeight()) {
 						improvementFound = true;
-						final PhysicalPlanWithCost betterPlan = new PhysicalPlanWithCost(neighbours.get(x), neighbourCosts[x]);
-						betterPlans.add(betterPlan); // I haven't figured out how to do this properly for Java yet, but it will be solved by the PhysicalPlanWithCost anyway.
+						betterPlans.add(neighbours.get(x));
 					}
 				}
 
 				if(improvementFound) { // if we have found at least one possible improvement, we want to make one of them into our new current plan.
-					final PhysicalPlanWithCost newPlan = getRandom(betterPlans); // Get a random object.
-					currentPlan = newPlan.getPlan();
-					currentCost = newPlan.getWeight();
+					currentPlan = getRandom(betterPlans); // Get a random object.
 				}
 			}
 			while(improvementFound);
 
-			if(currentCost < bestCost) {
-				bestCost = currentCost;
+			if(currentPlan.getWeight() < bestPlan.getWeight()) {
 				bestPlan = currentPlan;
 			}
 
 			generation++;
 		}
 
-		return bestPlan;
+		return bestPlan.getPlan();
 	}
 
 }
