7926e8341d0366081d0670da003b9637d19d876a
Sebastián Ferrada <scferradaa@gmail.com>
2021-12-14 10:27:57 +0100

    Update src/main/java/se/liu/ida/hefquin/engine/wrappers/lpgwrapper/SPARQLStar2CypherTranslator.java
    Co-authored-by: Olaf Hartig <olaf.hartig@liu.se>

--- src/main/java/se/liu/ida/hefquin/engine/wrappers/lpgwrapper/SPARQLStar2CypherTranslator.java
+++ src/main/java/se/liu/ida/hefquin/engine/wrappers/lpgwrapper/SPARQLStar2CypherTranslator.java
@@ -12,7 +12,7 @@ public interface SPARQLStar2CypherTranslator {
 
     /**
      * Translates the given Triple Pattern into a Cypher query, using a given LPG2RDFConfiguration.
-     * This method returns a {@link CypherQuery object} and a SPARQL-to-Cypher variable mapping.
+     * This method returns a {@link CypherQuery} object and a SPARQL-to-Cypher variable mapping.
      * If the Triple Pattern has a shape for which the configuration-specific RDF-star view of the
      * LPG is guaranteed to obtain no matching triples, this method returns null.
      * @return
