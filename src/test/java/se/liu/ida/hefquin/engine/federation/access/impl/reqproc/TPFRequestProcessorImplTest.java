c6801787275ce0d93757a2984880a51db14763eb
Olaf Hartig <olaf.hartig@liu.se>
2022-01-01 12:32:18 +0100

    adds a few more unit tests for TPFRequestProcessorImpl

--- src/test/java/se/liu/ida/hefquin/engine/federation/access/impl/reqproc/TPFRequestProcessorImplTest.java
+++ src/test/java/se/liu/ida/hefquin/engine/federation/access/impl/reqproc/TPFRequestProcessorImplTest.java
@@ -3,6 +3,7 @@ package se.liu.ida.hefquin.engine.federation.access.impl.reqproc;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import org.apache.jena.datatypes.xsd.XSDDatatype;
 import org.apache.jena.graph.Node;
 import org.apache.jena.graph.NodeFactory;
 import org.junit.Test;
@@ -22,13 +23,79 @@ import se.liu.ida.hefquin.engine.query.impl.TriplePatternImpl;
 public class TPFRequestProcessorImplTest extends EngineTestBase
 {
 	@Test
-	public void performRequestWithPatternOnDBpedia() throws FederationAccessException {
+	public void performRequestOnDBpedia_onePage() throws FederationAccessException {
 		if ( skipLiveWebTests ) { return; }
 
 		// setting up
 		final Node s = NodeFactory.createURI("http://dbpedia.org/resource/Berlin");
 		final Node p = NodeFactory.createURI("http://dbpedia.org/property/name");
 		final Node o = NodeFactory.createVariable("o");
+
+		// performing the tested operation
+		final TPFResponse resp = performTestRequest(s, p, o);
+
+		// checking
+		// - the following tests assume that i) a card.estimate was returned
+		//   in the TPF response and ii) the number of matching triples is
+		//   smaller than the page size
+		assertTrue( resp.getPayloadSize() == resp.getCardinalityEstimate() );
+		assertEquals( true, resp.isLastPage() );
+	}
+
+	@Test
+	public void performRequestOnDBpedia_notLastPage() throws FederationAccessException {
+		if ( skipLiveWebTests ) { return; }
+
+		// setting up
+		final Node s = NodeFactory.createURI("http://dbpedia.org/resource/Berlin");
+		final Node p = NodeFactory.createVariable("p");
+		final Node o = NodeFactory.createVariable("o");
+
+		// performing the tested operation
+		final TPFResponse resp = performTestRequest(s, p, o);
+
+		// checking
+		// - the following tests assume that i) a card.estimate was returned
+		//   in the TPF response and ii) the number of matching triples is
+		//   greater than the page size
+		assertTrue( resp.getPayloadSize() < resp.getCardinalityEstimate() );
+		assertEquals( false, resp.isLastPage() );
+	}
+
+	@Test
+	public void performRequestOnDBpedia_literal1() throws FederationAccessException {
+		if ( skipLiveWebTests ) { return; }
+
+		// setting up
+		final Node s = NodeFactory.createURI("http://dbpedia.org/resource/Berlin");
+		final Node p = NodeFactory.createVariable("p");
+		final Node o = NodeFactory.createLiteral("Berlin", "en");
+
+		// performing the tested operation
+		final TPFResponse resp = performTestRequest(s, p, o);
+
+		assertTrue( resp.getPayloadSize() > 1 );
+	}
+
+	@Test
+	public void performRequestOnDBpedia_literal2() throws FederationAccessException {
+		if ( skipLiveWebTests ) { return; }
+
+		// setting up
+		final Node s = NodeFactory.createURI("http://dbpedia.org/resource/Berlin");
+		final Node p = NodeFactory.createVariable("p");
+		final Node o = NodeFactory.createLiteral("38.1", XSDDatatype.XSDdouble);
+
+		// performing the tested operation
+		final TPFResponse resp = performTestRequest(s, p, o);
+
+		assertTrue( resp.getPayloadSize() > 1 );
+	}
+
+
+	// ----------- helper methods ----------
+
+	protected TPFResponse performTestRequest( final Node s, final Node p, final Node o ) throws FederationAccessException {
 		final TriplePattern tp = new TriplePatternImpl(s,p,o);
 		final int pageNumber = 0;
 		final TPFRequest req = new TPFRequestImpl(tp, pageNumber);
@@ -47,6 +114,7 @@ public class TPFRequestProcessorImplTest extends EngineTestBase
 		// checking
 		assertEquals( req, resp.getRequest() );
 		assertEquals( tpfServer, resp.getFederationMember() );
+
 		for ( final Triple t : resp.getPayload() ) {
 			final org.apache.jena.graph.Triple tt = t.asJenaTriple();
 			if ( s.isConcrete() ) { assertTrue( tt.getSubject().matches(s) ); }
@@ -54,11 +122,7 @@ public class TPFRequestProcessorImplTest extends EngineTestBase
 			if ( o.isConcrete() ) { assertTrue( tt.getObject().matches(o) ); }
 		}
 
-		// - the following tests assume that i) a card.estimate was returned
-		//   in the TPF response and ii) the number of matching triples is
-		//   smaller than the page size
-		assertTrue( resp.getPayloadSize() == resp.getCardinalityEstimate() );
-		assertEquals( true, resp.isLastPage() );
+		return resp;
 	}
 
 }
