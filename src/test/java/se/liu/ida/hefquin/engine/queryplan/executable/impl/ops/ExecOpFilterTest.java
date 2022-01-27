c350ffae33e5c5747d50cb7b528b00af6bc1bc85
Olaf Hartig <olaf.hartig@liu.se>
2022-01-27 23:07:30 +0100

    adds stub for ExecOpFilterTest

---
+++ src/test/java/se/liu/ida/hefquin/engine/queryplan/executable/impl/ops/ExecOpFilterTest.java
@@ -0,0 +1,17 @@
+package se.liu.ida.hefquin.engine.queryplan.executable.impl.ops;
+
+import org.junit.Test;
+
+public class ExecOpFilterTest
+{
+	@Test
+	public void test1_RenameTheseTestMethodsToSomethingThatDescribesTheirPurpose() {
+		
+	}
+
+	@Test
+	public void test2_RenameTheseTestMethodsToSomethingThatDescribesTheirPurpose() {
+		
+	}
+
+}
