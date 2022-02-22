afecc9866b7144e1c89ce0b4f64c6c62b68b4a4a
JulietteVV <99813521+JulietteVV@users.noreply.github.com>
2022-02-22 13:07:42 +0100

    Update src/main/java/se/liu/ida/hefquin/engine/data/VocabularyMapping.java
    Co-authored-by: Olaf Hartig <olaf.hartig@liu.se>

--- src/main/java/se/liu/ida/hefquin/engine/data/VocabularyMapping.java
+++ src/main/java/se/liu/ida/hefquin/engine/data/VocabularyMapping.java
@@ -1,7 +1,5 @@
 package se.liu.ida.hefquin.engine.data;
 
-import java.util.Set;
-
 import se.liu.ida.hefquin.engine.query.SPARQLGraphPattern;
 import se.liu.ida.hefquin.engine.query.TriplePattern;
 
@@ -10,11 +8,13 @@ public interface VocabularyMapping {
 
 	/**
 	 * Applies this vocabulary mapping to the given triple pattern and
-	 * returns the resulting triple patterns. If this mapping is not relevant
-	 * for the given triple pattern (i.e., applying the mapping to the given
-	 * triple pattern does not have any effect), then the result of this
-	 * function is a singleton set that simply contains the given triple
-	 * pattern.
+	 * returns the resulting combination of triple patterns, which may be
+	 * a union of triple patterns (captured as a {@link SPARQLUnionPattern})
+	 * or just a single triple pattern (captured as a {@link TriplePattern}).
+	 * If this mapping is not relevant for the given triple pattern (i.e.,
+	 * applying the mapping to the given triple pattern does not have any
+	 * effect), then the result of this function is simply the given triple
+	 * pattern itself.
 	 */
-	Set<SPARQLGraphPattern> translateTriplePattern( TriplePattern tp );
+	SPARQLGraphPattern translateTriplePattern( TriplePattern tp );
 }
