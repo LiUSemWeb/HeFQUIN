ad8076016635856d6d7934b83d952f663f9b9150
Juliette Winneroth <j.winneroth@gmail.com>
2022-05-12 12:31:44 +0200

    ExecOpRequestSPARQLWithTranslation fixes and new PhysicalOpRequestWithTranslation

--- src/main/java/se/liu/ida/hefquin/engine/queryplan/executable/impl/ops/ExecOpRequestSPARQLWithTranslation.java
+++ src/main/java/se/liu/ida/hefquin/engine/queryplan/executable/impl/ops/ExecOpRequestSPARQLWithTranslation.java
@@ -5,6 +5,14 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import org.apache.jena.graph.Triple;
+import org.apache.jena.sparql.algebra.Op;
+import org.apache.jena.sparql.algebra.op.OpBGP;
+import org.apache.jena.sparql.algebra.op.OpFilter;
+import org.apache.jena.sparql.algebra.op.OpJoin;
+import org.apache.jena.sparql.algebra.op.OpSequence;
+import org.apache.jena.sparql.algebra.op.OpUnion;
+
 import se.liu.ida.hefquin.engine.data.SolutionMapping;
 import se.liu.ida.hefquin.engine.federation.SPARQLEndpoint;
 import se.liu.ida.hefquin.engine.federation.access.FederationAccessException;
@@ -20,8 +28,12 @@ import se.liu.ida.hefquin.engine.query.SPARQLGroupPattern;
 import se.liu.ida.hefquin.engine.query.SPARQLUnionPattern;
 import se.liu.ida.hefquin.engine.query.TriplePattern;
 import se.liu.ida.hefquin.engine.query.impl.BGPImpl;
+import se.liu.ida.hefquin.engine.query.impl.GenericSPARQLGraphPatternImpl1;
+import se.liu.ida.hefquin.engine.query.impl.GenericSPARQLGraphPatternImpl2;
 import se.liu.ida.hefquin.engine.query.impl.SPARQLGroupPatternImpl;
 import se.liu.ida.hefquin.engine.query.impl.SPARQLUnionPatternImpl;
+import se.liu.ida.hefquin.engine.query.impl.TriplePatternImpl;
+import se.liu.ida.hefquin.engine.queryplan.executable.IntermediateResultElementSink;
 
 public class ExecOpRequestSPARQLWithTranslation extends ExecOpGenericSolMapsRequest<SPARQLRequest, SPARQLEndpoint>
 {
@@ -29,6 +41,16 @@ public class ExecOpRequestSPARQLWithTranslation extends ExecOpGenericSolMapsRequ
 		super( req, fm );
 	}
 
+	@Override
+	protected void process( final SolMapsResponse response, final IntermediateResultElementSink sink )
+	{
+		for ( SolutionMapping sm : response.getSolutionMappings() ) {
+			for (SolutionMapping smTranslated : fm.getVocabularyMapping().translateSolutionMapping(sm)) {
+				sink.send( smTranslated );
+			}
+		}
+	}
+	
 	@Override
 	protected SolMapsResponse performRequest( final FederationAccessManager fedAccessMgr ) throws FederationAccessException {
 		final SPARQLGraphPattern query = req.getQueryPattern();
@@ -42,21 +64,22 @@ public class ExecOpRequestSPARQLWithTranslation extends ExecOpGenericSolMapsRequ
 			reqTranslation = handleGroup((SPARQLGroupPattern) query);
 		} else if (query instanceof SPARQLUnionPattern) {
 			reqTranslation = handleUnion((SPARQLUnionPattern) query);
+		} else if (query instanceof GenericSPARQLGraphPatternImpl1) {
+			Op gp = ((GenericSPARQLGraphPatternImpl1) query).asJenaOp();
+			reqTranslation = handleOp(gp);		
+		} else if (query instanceof GenericSPARQLGraphPatternImpl2) {
+			Op gp = ((GenericSPARQLGraphPatternImpl2) query).asJenaOp();
+			reqTranslation = handleOp(gp);	
 		}
 		else {
 			throw new FederationAccessException("Unsupported type of pattern: " + query.getClass().getName(), req, fm);
 		}
 		
 		final SPARQLRequest newReq = new SPARQLRequestImpl(reqTranslation);
-		final SolMapsResponse res = FederationAccessUtils.performRequest(fedAccessMgr, newReq, fm);	
-		
-		List<SolutionMapping> resList = new ArrayList<>();
-		for(final SolutionMapping i : res.getSolutionMappings()) {
-			resList.addAll(fm.getVocabularyMapping().translateSolutionMapping(i));
-		}
-		
-		return new SolMapsResponseImpl(resList, fm, req, null);
+		return FederationAccessUtils.performRequest(fedAccessMgr, newReq, fm);	
 	}
+	
+	
 	
 	protected SPARQLGraphPattern handleTriplePattern(TriplePattern tp) {
 		return fm.getVocabularyMapping().translateTriplePattern((TriplePattern) tp);
@@ -121,6 +144,79 @@ public class ExecOpRequestSPARQLWithTranslation extends ExecOpGenericSolMapsRequ
 			}
 		}
 		
+		return new SPARQLGroupPatternImpl(subPatterns);
+	}
+	
+	protected SPARQLGraphPattern handleOp(Op op) throws FederationAccessException {
+		if (op instanceof OpJoin) {
+			return handleJoin((OpJoin) op);
+		} else if (op instanceof OpUnion) {
+			return handleOpUnion((OpUnion) op);
+		} else if (op instanceof OpFilter) {
+			return handleOpFilter((OpFilter) op);
+		} else if (op instanceof OpBGP) {
+			return handleOpBGP((OpBGP) op);
+		} else if (op instanceof OpSequence) {
+			return handleOpSequence((OpSequence) op);
+		} else {
+			throw new FederationAccessException("Unsupported type of pattern: " + op.getClass().getName(), req, fm);
+		}
+	}
+	
+	protected SPARQLGraphPattern handleJoin(OpJoin oj) throws FederationAccessException {
+		List<SPARQLGraphPattern> subPatterns = new ArrayList<>();
+		Op left = oj.getLeft();
+		subPatterns.add(handleOp(left));
+		Op right = oj.getRight();
+		subPatterns.add(handleOp(right));
+		return new SPARQLGroupPatternImpl(subPatterns);
+	}
+	
+	protected SPARQLGraphPattern handleOpUnion(OpUnion ou) throws FederationAccessException {
+		final SPARQLUnionPatternImpl unionTranslation = new SPARQLUnionPatternImpl();
+		Op left = ou.getLeft();
+		unionTranslation.addSubPattern(handleOp(left));
+		Op right = ou.getRight();
+		unionTranslation.addSubPattern(handleOp(right));
+		return unionTranslation;
+	}
+	
+	//TODO: Handle OpFilter
+	protected SPARQLGraphPattern handleOpFilter(OpFilter of) throws FederationAccessException {
+		return null;
+	}
+	
+	protected SPARQLGraphPattern handleOpBGP(OpBGP obgp) throws FederationAccessException {	
+		final List<SPARQLGraphPattern> allSubPatterns = new ArrayList<>();
+		final Set<TriplePattern> tpSubPatterns = new HashSet<>();
+		boolean allSubPatternsAreTriplePatterns = true; // assume yes
+
+		for( final Triple i : obgp.getPattern().getList() ) {
+			TriplePattern itp = new TriplePatternImpl(i);
+			final SPARQLGraphPattern iTranslation = fm.getVocabularyMapping().translateTriplePattern(itp); 
+			allSubPatterns.add(iTranslation);
+
+			if ( allSubPatternsAreTriplePatterns && iTranslation instanceof TriplePattern ) {
+				tpSubPatterns.add( (TriplePattern) iTranslation );
+			}
+			else {
+				allSubPatternsAreTriplePatterns = false;
+			}
+		}
+
+		if ( allSubPatternsAreTriplePatterns ) {
+			return new BGPImpl(tpSubPatterns);
+		}
+		else {
+			return new SPARQLGroupPatternImpl(allSubPatterns);
+		}
+	}
+	
+	protected SPARQLGraphPattern handleOpSequence(OpSequence os) throws FederationAccessException {
+		List<SPARQLGraphPattern> subPatterns = new ArrayList<>();
+		for( final Op i : os.getElements()) {
+			subPatterns.add(handleOp(i));
+		}
 		return new SPARQLGroupPatternImpl(subPatterns);
 	}
 
---
+++ src/main/java/se/liu/ida/hefquin/engine/queryplan/physical/impl/PhysicalOpRequestWithTranslation.java
@@ -0,0 +1,80 @@
+package se.liu.ida.hefquin.engine.queryplan.physical.impl;
+
+import se.liu.ida.hefquin.engine.federation.FederationMember;
+import se.liu.ida.hefquin.engine.federation.SPARQLEndpoint;
+import se.liu.ida.hefquin.engine.federation.access.DataRetrievalRequest;
+import se.liu.ida.hefquin.engine.federation.access.SPARQLRequest;
+import se.liu.ida.hefquin.engine.queryplan.ExpectedVariables;
+import se.liu.ida.hefquin.engine.queryplan.executable.impl.ops.ExecOpRequestSPARQLWithTranslation;
+import se.liu.ida.hefquin.engine.queryplan.executable.impl.ops.NullaryExecutableOp;
+import se.liu.ida.hefquin.engine.queryplan.logical.impl.LogicalOpRequest;
+import se.liu.ida.hefquin.engine.queryplan.physical.NullaryPhysicalOpForLogicalOp;
+import se.liu.ida.hefquin.engine.queryplan.physical.PhysicalPlanVisitor;
+
+/**
+ * This class represents a (physical) request operator that rewrites its
+ * request based on the vocabulary mapping associated with the given
+ * federation member, and then it also rewrites the solutions mappings
+ * by applying the inverse of the vocabulary mapping.
+ */
+public class PhysicalOpRequestWithTranslation<ReqType extends DataRetrievalRequest, MemberType extends FederationMember>
+                       implements NullaryPhysicalOpForLogicalOp
+{
+	protected final LogicalOpRequest<ReqType,MemberType> lop;
+
+	public PhysicalOpRequestWithTranslation( final LogicalOpRequest<ReqType,MemberType> lop ) {
+		assert lop != null;
+		assert lop.getFederationMember().getVocabularyMapping() != null;
+
+		this.lop = lop;
+	}
+
+	@Override
+	public boolean equals( final Object o ) {
+		return o instanceof PhysicalOpRequestWithTranslation<?,?> && ((PhysicalOpRequestWithTranslation<?,?>) o).lop.equals(lop);
+	}
+
+	@Override
+	public LogicalOpRequest<ReqType,MemberType> getLogicalOperator() {
+		return lop;
+	}
+
+	@Override
+	public NullaryExecutableOp createExecOp( final ExpectedVariables ... inputVars ) {
+		final ReqType req = lop.getRequest();
+		final MemberType fm = lop.getFederationMember();
+		if ( fm instanceof SPARQLEndpoint && req instanceof SPARQLRequest ) {
+			return new ExecOpRequestSPARQLWithTranslation( (SPARQLRequest) req, (SPARQLEndpoint) fm );
+		}
+// TODO: we need executable operators for the following cases
+//		else if ( fm instanceof TPFServer && req instanceof TriplePatternRequest ) {
+//			return new ExecOpRequestTPFatTPFServerWithTranslation( (TriplePatternRequest) req, (TPFServer) fm );
+//		}
+//		else if ( fm instanceof BRTPFServer && req instanceof TriplePatternRequest ) {
+//			return new ExecOpRequestTPFatBRTPFServer( (TriplePatternRequest) req, (BRTPFServer) fm );
+//		}
+//		else if ( fm instanceof BRTPFServer && req instanceof TriplePatternRequest ) {
+//			return new ExecOpRequestBRTPF( (BindingsRestrictedTriplePatternRequest) req, (BRTPFServer) fm );
+//		}
+		else
+			throw new IllegalArgumentException("Unsupported combination of federation member (type: " + fm.getClass().getName() + ") and request type (" + req.getClass().getName() + ")");
+	}
+
+	@Override
+	public ExpectedVariables getExpectedVariables( final ExpectedVariables... inputVars ) {
+		assert inputVars.length == 0;
+
+		return lop.getRequest().getExpectedVariables();
+	}
+
+	@Override
+	public void visit(final PhysicalPlanVisitor visitor) {
+		visitor.visit(this);
+	}
+
+	@Override
+	public String toString() {
+		return lop.toString();
+	}
+
+}
