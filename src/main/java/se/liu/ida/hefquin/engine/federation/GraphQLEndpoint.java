d987b107eb4b39b4e2a07f97145294dfcd54edae
WiktorKarlsson <karlssonwiktor@gmail.com>
2022-05-20 03:05:58 +0200

    indentation and spacing fixes

--- src/main/java/se/liu/ida/hefquin/engine/federation/GraphQLEndpoint.java
+++ src/main/java/se/liu/ida/hefquin/engine/federation/GraphQLEndpoint.java
@@ -9,33 +9,32 @@ import se.liu.ida.hefquin.engine.wrappers.graphqlwrapper.data.GraphQLField;
 import se.liu.ida.hefquin.engine.wrappers.graphqlwrapper.data.impl.GraphQLEntrypointType;
 import se.liu.ida.hefquin.engine.wrappers.graphqlwrapper.data.impl.GraphQLFieldType;
 
-public interface GraphQLEndpoint extends FederationMember
-{
-	@Override
-	GraphQLInterface getInterface();
+public interface GraphQLEndpoint extends FederationMember {
+    @Override
+    GraphQLInterface getInterface();
 
-	/**
-     * Verifies that a GraphQL object type with the name @param objectTypeName exists 
+    /**
+     * Verifies that a GraphQL object type with the name @param objectTypeName exists
      * for the endpoint (as defined by its schema).
      */
     public boolean containsGraphQLObjectType(final String objectTypeName);
-	
-	/**
+
+    /**
      * Verifies that a field with the name @param fieldName exists for a GraphQL object type
      * with the name @param objectTypeName in the endpoint (as defined by its schema).
-     */    
+     */
     public boolean containsGraphQLField(final String objectTypeName, final String fieldName);
 
-	/**
+    /**
      * @return a GraphQLFieldType enum that describes whether the field with the name @param fieldName
      * of the GraphQL object type of the name @param objectTypeName is a scalar value or an object.
      * If the parameter names provided doesn't correspond to an object type or a field for the endpoint return null.
      */
     public GraphQLFieldType getGraphQLFieldType(final String objectTypeName, final String fieldName);
 
-	/**
-     * @return the value type (String,Int,... etc.) for the field with the name @param fieldName of the
-     * GraphQL object type with the name @param objectTypeName .
+    /**
+     * @return the value type (String,Int,... etc.) for the field with the name
+     * @param fieldName of the GraphQL object type with the name @param objectTypeName .
      * If the parameter names provided doesn't correspond to an object type or a field for the endpoint return null.
      */
     public String getGraphQLFieldValueType(final String objectTypeName, final String fieldName);
@@ -45,18 +44,18 @@ public interface GraphQLEndpoint extends FederationMember
      */
     public Set<String> getGraphQLObjectTypes();
 
-	/**
-	 * @return a map of field names for the GraphQL object type with the name @param objectTypeName mapped to their 
-     * respective GraphQLField objects containing information about the field. 
+    /**
+     * @return a map of field names for the GraphQL object type with the name @param objectTypeName mapped 
+     * to their respective GraphQLField objects containing information about the field.
      * If the parameter name provided doesn't correspond to an object type for the endpoint return null.
-	 */
-	public Map<String,GraphQLField> getGraphQLObjectFields(final String objectTypeName);
+     */
+    public Map<String, GraphQLField> getGraphQLObjectFields(final String objectTypeName);
 
-	/**
+    /**
      * @return a GraphQLEntrypoint object containing information about a specific field in the GraphQL "query" type.
      * @param obejectTypeName is the name of the object type the field returns and @param fieldType is an enum describing
      * whether the field returns a single object, a filtered list of objects or a list of all objects.
      * If the parameter names provided doesn't correspond to an object type or a GraphQLEntrypointType for the endpoint return null.
      */
-    public GraphQLEntrypoint getEntrypoint(final String objectTypeName, final GraphQLEntrypointType fieldType);  
+    public GraphQLEntrypoint getEntrypoint(final String objectTypeName, final GraphQLEntrypointType fieldType);
 }
