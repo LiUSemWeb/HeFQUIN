1f3088261185950ac5d76ba44a545353264e9cb2
Olaf Hartig <olaf.hartig@liu.se>
2022-07-07 16:10:52 +0200

    changes MaterializingIntermediateResultElementSink to not anymore implement ClosableIntermediateResultElementSink

--- src/main/java/se/liu/ida/hefquin/engine/queryplan/executable/impl/MaterializingIntermediateResultElementSink.java
+++ src/main/java/se/liu/ida/hefquin/engine/queryplan/executable/impl/MaterializingIntermediateResultElementSink.java
@@ -4,32 +4,31 @@ import java.util.ArrayList;
 import java.util.List;
 
 import se.liu.ida.hefquin.engine.data.SolutionMapping;
+import se.liu.ida.hefquin.engine.queryplan.executable.IntermediateResultElementSink;
 
+/**
+ * An implementation of {@link IntermediateResultElementSink}
+ * that collects all solution mappings that are sent to it. The
+ * collected solution mappings can then be accessed by calling
+ * {@link #getMaterializedIntermediateResult()}.
+ *
+ * Attention, this implementation is not thread safe.
+ */
 public class MaterializingIntermediateResultElementSink
-                             implements ClosableIntermediateResultElementSink
+                             implements IntermediateResultElementSink
 {
 	protected final List<SolutionMapping> l = new ArrayList<>();
-	private boolean closed = false;
 
 	@Override
-	public void send(SolutionMapping element) {
-		if ( ! closed ) {
-			l.add(element);
-		}
+	public void send( final SolutionMapping element ) {
+		l.add(element);
 	}
 
-	@Override
-	public void close() {
-		closed = true;
-	}
-
-	@Override
-	public boolean isClosed() {
-		return closed;
-	}
-
+	/**
+	 * Returns an iterable over the solution mappings that have been
+	 * collected in this sink since it was created.
+	 */
 	public Iterable<SolutionMapping> getMaterializedIntermediateResult() {
 		return l;
 	}
-
 }
