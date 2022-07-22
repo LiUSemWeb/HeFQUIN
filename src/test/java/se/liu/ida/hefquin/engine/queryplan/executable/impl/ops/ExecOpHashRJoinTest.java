ad99c457c6c25be2200638c30507f4283b9a5691
Olaf Hartig <olaf.hartig@liu.se>
2022-07-22 16:44:43 +0200

    adds ExecOpHashRJoin

---
+++ src/test/java/se/liu/ida/hefquin/engine/queryplan/executable/impl/ops/ExecOpHashRJoinTest.java
@@ -0,0 +1,33 @@
+package se.liu.ida.hefquin.engine.queryplan.executable.impl.ops;
+
+import org.junit.Test;
+
+import se.liu.ida.hefquin.engine.queryplan.ExpectedVariables;
+import se.liu.ida.hefquin.engine.queryplan.executable.BinaryExecutableOp;
+import se.liu.ida.hefquin.engine.queryproc.ExecutionException;
+
+public class ExecOpHashRJoinTest extends TestsForRightJoinAlgorithms
+{
+    @Test
+    public void joinWithOneJoinVariable() throws ExecutionException { _joinWithOneJoinVariable(); }
+
+    @Test
+    public void joinWithTwoJoinVariables() throws ExecutionException { _joinWithTwoJoinVariables(); }
+
+    @Test
+    public void joinWithTwoJoinVariables_noJoinPartner() throws ExecutionException { _joinWithTwoJoinVariables_noJoinPartner(); }
+
+    @Test
+    public void joinWithOneJoinVariable_withPossibleVars_noOverlap() throws ExecutionException { _joinWithOneJoinVariable_withPossibleVars_noOverlap(); }
+
+    @Test
+    public void joinWithOneJoinVariable_withPossibleVars_overlapped() throws ExecutionException { _joinWithOneJoinVariable_withPossibleVars_overlapped(); }
+
+    @Override
+    protected BinaryExecutableOp createExecOpForTest(final ExpectedVariables... inputVars) {
+        assert inputVars.length == 2;
+
+        return new ExecOpHashRJoin( inputVars[0], inputVars[1] );
+    }
+
+}
