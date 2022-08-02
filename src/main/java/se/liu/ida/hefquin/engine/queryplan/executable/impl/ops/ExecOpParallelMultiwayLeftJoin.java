b3d90c3eeb372f5278759805b26f84950abdcbb7
Olaf Hartig <olaf.hartig@liu.se>
2022-08-02 11:42:17 +0200

    initial commit of skeleton for ExecOpParallelMultiwayLeftJoin

---
+++ src/main/java/se/liu/ida/hefquin/engine/queryplan/executable/impl/ops/ExecOpParallelMultiwayLeftJoin.java
@@ -0,0 +1,37 @@
+package se.liu.ida.hefquin.engine.queryplan.executable.impl.ops;
+
+import java.util.List;
+
+import se.liu.ida.hefquin.engine.queryplan.executable.ExecOpExecutionException;
+import se.liu.ida.hefquin.engine.queryplan.executable.IntermediateResultBlock;
+import se.liu.ida.hefquin.engine.queryplan.executable.IntermediateResultElementSink;
+import se.liu.ida.hefquin.engine.queryplan.logical.impl.LogicalOpRequest;
+import se.liu.ida.hefquin.engine.queryproc.ExecutionContext;
+
+public class ExecOpParallelMultiwayLeftJoin extends UnaryExecutableOpBase
+{
+	public ExecOpParallelMultiwayLeftJoin( final List<LogicalOpRequest<?,?>> optionalParts ) {
+		// TODO ...
+		
+	}
+
+	@Override
+	public int preferredInputBlockSize() {
+		return 30;
+	}
+
+	@Override
+	protected void _process( final IntermediateResultBlock input,
+	                         final IntermediateResultElementSink sink,
+	                         final ExecutionContext execCxt ) throws ExecOpExecutionException {
+		// TODO ...
+		
+	}
+
+	@Override
+	protected void _concludeExecution( final IntermediateResultElementSink sink,
+	                                   final ExecutionContext execCxt ) throws ExecOpExecutionException {
+		// nothing to be done here
+	}
+
+}
