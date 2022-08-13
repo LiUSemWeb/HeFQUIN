322738eaa79c0682950fadfacaa31d67f6744f51
Olaf Hartig <olaf.hartig@liu.se>
2022-08-13 21:55:16 +0200

    renames LogicalQueryOptimizer to LogicalOptimizer

--- src/main/java/se/liu/ida/hefquin/engine/queryproc/LogicalQueryOptimizer.java
+++
@@ -1,8 +0,0 @@
-package se.liu.ida.hefquin.engine.queryproc;
-
-import se.liu.ida.hefquin.engine.queryplan.logical.LogicalPlan;
-
-public interface LogicalQueryOptimizer
-{
-	LogicalPlan optimize( LogicalPlan inputPlan, boolean keepNaryOperators ) throws LogicalQueryOptimizationException;
-}
