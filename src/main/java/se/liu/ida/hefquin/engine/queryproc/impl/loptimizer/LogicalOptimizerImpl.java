a83559fa1704f9376e8d901148fbdbff29e152b4
Olaf Hartig <olaf.hartig@liu.se>
2022-08-31 18:17:31 +0200

    adds 'HeuristicForLogicalOptimization' interface and 'UnionPullUp' as a first heuristic

--- src/main/java/se/liu/ida/hefquin/engine/queryproc/impl/loptimizer/LogicalOptimizerImpl.java
+++ src/main/java/se/liu/ida/hefquin/engine/queryproc/impl/loptimizer/LogicalOptimizerImpl.java
@@ -1,14 +1,29 @@
 package se.liu.ida.hefquin.engine.queryproc.impl.loptimizer;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import se.liu.ida.hefquin.engine.queryplan.logical.LogicalPlan;
 import se.liu.ida.hefquin.engine.queryproc.LogicalOptimizationException;
 import se.liu.ida.hefquin.engine.queryproc.LogicalOptimizer;
+import se.liu.ida.hefquin.engine.queryproc.impl.loptimizer.heuristics.UnionPullUp;
 
 public class LogicalOptimizerImpl implements LogicalOptimizer
 {
+	protected final List<HeuristicForLogicalOptimization> heuristics = new ArrayList<>();
+
+	public LogicalOptimizerImpl() {
+		heuristics.add( new UnionPullUp() );
+	}
+
 	@Override
 	public LogicalPlan optimize( final LogicalPlan inputPlan, final boolean keepNaryOperators ) throws LogicalOptimizationException {
-		return inputPlan;
+		LogicalPlan resultPlan = inputPlan;
+		for ( final HeuristicForLogicalOptimization h : heuristics ) {
+			resultPlan = h.apply(resultPlan);
+		}
+
+		return resultPlan;
 	}
 
 }
