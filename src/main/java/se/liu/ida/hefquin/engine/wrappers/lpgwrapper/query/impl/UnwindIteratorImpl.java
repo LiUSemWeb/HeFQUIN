9b519c66b0bb28b55b9cc9b894c90d1426ba9ff9
Sebastián Ferrada <scferradaa@gmail.com>
2022-09-06 10:24:49 +0200

    documentation for UnwindIterator interface

--- src/main/java/se/liu/ida/hefquin/engine/wrappers/lpgwrapper/query/impl/UnwindIteratorImpl.java
+++ src/main/java/se/liu/ida/hefquin/engine/wrappers/lpgwrapper/query/impl/UnwindIteratorImpl.java
@@ -9,7 +9,7 @@ import java.util.List;
 public class UnwindIteratorImpl implements UnwindIterator {
 
     protected final CypherVar innerVar;
-    //TODO: this should be a CypherExpression
+    //TODO: this below should be a CypherExpression
     protected final String listExpression;
     protected final List<WhereCondition> filters;
     protected final List<String> returnExpressions;
@@ -18,6 +18,10 @@ public class UnwindIteratorImpl implements UnwindIterator {
     public UnwindIteratorImpl(final CypherVar innerVar, final String listExpression,
                               final List<WhereCondition> filters, final List<String> returnExpressions,
                               final CypherVar alias) {
+        assert innerVar != null;
+        assert listExpression != null;
+        assert alias != null;
+
         this.innerVar = innerVar;
         this.listExpression = listExpression;
         this.filters = filters;
@@ -25,19 +29,30 @@ public class UnwindIteratorImpl implements UnwindIterator {
         this.alias = alias;
     }
 
+    @Override
     public CypherVar getInnerVar() {
         return innerVar;
     }
 
+    @Override
     public String getListExpression() {
         return listExpression;
     }
 
+    @Override
     public List<WhereCondition> getFilters() {
         return filters;
     }
 
+    @Override
     public List<String> getReturnExpressions() {
         return returnExpressions;
     }
+
+    @Override
+    public CypherVar getAlias() {
+        return alias;
+    }
+
+
 }
