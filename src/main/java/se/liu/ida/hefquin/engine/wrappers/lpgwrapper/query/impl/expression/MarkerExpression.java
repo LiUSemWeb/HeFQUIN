69ca40dd368031c52de4280e54f5d1735b39b221
Sebastián Ferrada <scferradaa@gmail.com>
2022-09-28 10:08:22 +0200

    adds javadoc to MarkerExpression

--- src/main/java/se/liu/ida/hefquin/engine/wrappers/lpgwrapper/query/impl/expression/MarkerExpression.java
+++ src/main/java/se/liu/ida/hefquin/engine/wrappers/lpgwrapper/query/impl/expression/MarkerExpression.java
@@ -1,7 +1,15 @@
 package se.liu.ida.hefquin.engine.wrappers.lpgwrapper.query.impl.expression;
 
-import se.liu.ida.hefquin.engine.wrappers.lpgwrapper.query.CypherExpression;
+import se.liu.ida.hefquin.engine.wrappers.lpgwrapper.Record2SolutionMappingTranslator;
+import se.liu.ida.hefquin.engine.wrappers.lpgwrapper.query.CypherUnionQuery;
 
+/**
+ * This class represents a special type of {@link AliasedExpression}. An object of this
+ * class represents a marker that helps the Record to Solution Mapping translator
+ * {@link Record2SolutionMappingTranslator} to identify from which sub-query any given
+ * record comes from. The identification is done through the index field in a way that for
+ * a given {@link CypherUnionQuery}, each of its sub-queries is enumerated consecutively.
+ */
 public class MarkerExpression extends AliasedExpression {
 
     private final int index;
