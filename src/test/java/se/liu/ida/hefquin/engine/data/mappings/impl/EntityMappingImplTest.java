bee9a5e88e7624b3786e2803c45a487342f359a2
= <Senpai@oniichan.eu>
2022-09-30 19:54:15 +0200

    ParseMappingDescriptionTest fully implemented, runs correctly.

--- src/test/java/se/liu/ida/hefquin/engine/data/mappings/impl/EntityMappingImplTest.java
+++ src/test/java/se/liu/ida/hefquin/engine/data/mappings/impl/EntityMappingImplTest.java
@@ -3,15 +3,20 @@ package se.liu.ida.hefquin.engine.data.mappings.impl;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
+import org.apache.commons.io.IOUtils;
 import org.apache.jena.graph.Graph;
 import org.apache.jena.graph.Node;
 import org.apache.jena.graph.NodeFactory;
+import org.apache.jena.riot.Lang;
+import org.apache.jena.riot.RDFDataMgr;
 import org.apache.jena.sparql.core.Var;
+import org.apache.jena.sparql.graph.GraphFactory;
 import org.junit.Test;
 
 import se.liu.ida.hefquin.engine.data.SolutionMapping;
@@ -43,8 +48,50 @@ public class EntityMappingImplTest {
 	}
 	
 	@Test
-	public void parseMappingDescriptionTest() {
+	public void parseMappingDescriptionTest() throws IOException {
+		final String mappingAsTurtle =
+				  "@prefix ex:   <http://example.org/> .  \n"
+				+ "@prefix owl:  <http://www.w3.org/2002/07/owl#> . \n"
+				+ "ex:Bob owl:sameAs ex:Robert . "
+				+ "ex:Bob owl:sameAs ex:Bobby . "
+				+ "ex:Ali owl:sameAs ex:Alibaba . ";
+		final Graph mapping = GraphFactory.createDefaultGraph();
+		RDFDataMgr.read(mapping, IOUtils.toInputStream(mappingAsTurtle, "UTF-8"), Lang.TURTLE);
 		
+		final EntityMappingImplForTests entityMapping = new EntityMappingImplForTests(mapping);
+		
+		// Create global nodes.
+		final Node bob = NodeFactory.createURI("http://example.org/Bob");
+		final Node ali = NodeFactory.createURI("http://example.org/Ali");
+		
+		final Set<Node> globalAli = new HashSet<>();
+		globalAli.add(ali);
+		final Set<Node> globalBob = new HashSet<>();
+		globalBob.add(bob);
+		
+		
+		// Create local nodes.
+		final Node robert = NodeFactory.createURI("http://example.org/Robert");
+		final Node bobby = NodeFactory.createURI("http://example.org/Bobby");
+		final Node alibaba = NodeFactory.createURI("http://example.org/Alibaba");
+		
+		final Set<Node> localAli = new HashSet<>();
+		localAli.add(alibaba);
+		final Set<Node> localBob = new HashSet<>();
+		localBob.add(bobby);
+		localBob.add(robert);
+		
+		// Reference maps.
+		final Map<Node, Set<Node>> g2lMap = new HashMap<>();
+		g2lMap.put(ali, localAli);
+		g2lMap.put(bob, localBob);
+		final Map<Node, Set<Node>> l2gMap = new HashMap<>();
+		l2gMap.put(alibaba, globalAli);
+		l2gMap.put(bobby, globalBob);
+		l2gMap.put(robert, globalBob);
+		
+		assertEquals( g2lMap, entityMapping.getG2lMap() );
+		assertEquals( l2gMap, entityMapping.getL2gMap() );
 	}
 	
 	@Test
@@ -53,7 +100,7 @@ public class EntityMappingImplTest {
 		// Create solution mappings.
 		final Node bob = NodeFactory.createURI("ex:Bob");
 		final Node ali = NodeFactory.createURI("ex:Ali");
-		final Node charles = NodeFactory.createURI("ex:Chales");
+		final Node charles = NodeFactory.createURI("ex:Charles");
 		final Var x = Var.alloc("x");
 		final Var y = Var.alloc("y");
 		final Var z = Var.alloc("z");
