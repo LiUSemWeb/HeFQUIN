c85be1070608f88da879671eb2315ad51eb1443b
Sebastián Ferrada <scferradaa@gmail.com>
2022-10-20 16:21:00 +0200

    adds missing parenthesis to LabelsExpression

--- src/main/java/se/liu/ida/hefquin/engine/wrappers/lpgwrapper/query/impl/expression/LabelsExpression.java
+++ src/main/java/se/liu/ida/hefquin/engine/wrappers/lpgwrapper/query/impl/expression/LabelsExpression.java
@@ -34,6 +34,6 @@ public class LabelsExpression implements CypherExpression {
 
     @Override
     public String toString() {
-        return "HEAD(LABELS(" + var + ')';
+        return "HEAD(LABELS(" + var + "))";
     }
 }
