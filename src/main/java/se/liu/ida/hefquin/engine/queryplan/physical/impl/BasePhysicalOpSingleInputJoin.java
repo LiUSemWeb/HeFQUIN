0e2373d3cc2ad7377981555decefb870e6e4c0de
Olaf Hartig <olaf.hartig@liu.se>
2022-12-27 18:25:19 +0100

    changes the implementations of getExpectedVariables in the physical operators to use the newly added getExpectedVariables of LogicalOperator

--- src/main/java/se/liu/ida/hefquin/engine/queryplan/physical/impl/BasePhysicalOpSingleInputJoin.java
+++ src/main/java/se/liu/ida/hefquin/engine/queryplan/physical/impl/BasePhysicalOpSingleInputJoin.java
@@ -2,8 +2,6 @@ package se.liu.ida.hefquin.engine.queryplan.physical.impl;
 
 import java.util.Objects;
 
-import se.liu.ida.hefquin.engine.query.SPARQLGraphPattern;
-import se.liu.ida.hefquin.engine.query.impl.QueryPatternUtils;
 import se.liu.ida.hefquin.engine.queryplan.ExpectedVariables;
 import se.liu.ida.hefquin.engine.queryplan.logical.UnaryLogicalOp;
 import se.liu.ida.hefquin.engine.queryplan.logical.impl.LogicalOpBGPAdd;
@@ -49,32 +47,7 @@ public abstract class BasePhysicalOpSingleInputJoin implements UnaryPhysicalOpFo
 
 	@Override
 	public ExpectedVariables getExpectedVariables( final ExpectedVariables... inputVars ) {
-		assert inputVars.length == 1;
-
-		final SPARQLGraphPattern p;
-
-		if ( lop instanceof LogicalOpTPAdd ) {
-			p = ( (LogicalOpTPAdd) lop ).getTP();
-		}
-		else if ( lop instanceof LogicalOpTPOptAdd ) {
-			p = ( (LogicalOpTPOptAdd) lop ).getTP();
-		}
-		else if ( lop instanceof LogicalOpBGPAdd ) {
-			p = ( (LogicalOpBGPAdd) lop ).getBGP();
-		}
-		else if ( lop instanceof LogicalOpBGPOptAdd ) {
-			p = ( (LogicalOpBGPOptAdd) lop ).getBGP();
-		}
-		else if ( lop instanceof LogicalOpGPAdd ) {
-			p = ( (LogicalOpGPAdd) lop ).getPattern();
-		}
-		else if ( lop instanceof LogicalOpGPOptAdd ) {
-			p = ( (LogicalOpGPOptAdd) lop ).getPattern();
-		}
-		else
-			throw new IllegalArgumentException("Unsupported type of operator: " + lop.getClass().getName() );
-
-		return QueryPatternUtils.getExpectedVariablesInPattern(p);
+		return lop.getExpectedVariables(inputVars);
 	}
 
 }
