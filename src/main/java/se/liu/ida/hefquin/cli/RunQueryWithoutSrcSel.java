dc4838312cbf53c23528a10073b4777aea305d0c
Olaf Hartig <olaf.hartig@liu.se>
2023-01-25 17:01:01 +0100

    changes the name of the new command line argument from '--printQueryProcTime' to '--printQueryProcMeasurements'

--- src/main/java/se/liu/ida/hefquin/cli/RunQueryWithoutSrcSel.java
+++ src/main/java/se/liu/ida/hefquin/cli/RunQueryWithoutSrcSel.java
@@ -42,7 +42,7 @@ public class RunQueryWithoutSrcSel extends CmdARQ
 	protected final ArgDecl argPrintLogicalPlan   = new ArgDecl(ArgDecl.NoValue, "printLogicalPlan");
 	protected final ArgDecl argPrintPhysicalPlan  = new ArgDecl(ArgDecl.NoValue, "printPhysicalPlan");
 	protected final ArgDecl argQueryProcStats = new ArgDecl(ArgDecl.NoValue, "printQueryProcStats");
-	protected final ArgDecl argOnelineTimeStats = new ArgDecl(ArgDecl.NoValue, "printQueryProcTime");
+	protected final ArgDecl argOnelineTimeStats = new ArgDecl(ArgDecl.NoValue, "printQueryProcMeasurements");
 	protected final ArgDecl argFedAccessStats = new ArgDecl(ArgDecl.NoValue, "printFedAccessStats");
 
 	public static void main( final String... argv ) {
@@ -61,7 +61,7 @@ public class RunQueryWithoutSrcSel extends CmdARQ
 		add(argPrintLogicalPlan, "--printLogicalPlan", "Print out the logical plan produced by the logical query optimization");
 		add(argPrintPhysicalPlan, "--printPhysicalPlan", "Print out the physical plan produced by the physical query optimization and used for the query execution");
 		add(argQueryProcStats, "--printQueryProcStats", "Print out statistics about the query execution process");
-		add(argOnelineTimeStats, "--printQueryProcTime", "Print out statistics about the query processing time in one line");
+		add(argOnelineTimeStats, "--printQueryProcMeasurements", "Print out measurements about the query processing time in one line that can be used for a CSV file");
 		add(argFedAccessStats, "--printFedAccessStats", "Print out statistics of the federation access manager");
 
 		addModule(modQuery);
