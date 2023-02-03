a16697ac272afbe96ebbe27539b179cee454d81a
sijin <sijin.cheng@liu.se>
2023-02-03 11:48:25 +0100

    Simplify countDuplicates function; Update comments;

--- src/main/java/se/liu/ida/hefquin/engine/queryproc/impl/loptimizer/heuristics/utils/Join_Analyzer.java
+++ src/main/java/se/liu/ida/hefquin/engine/queryproc/impl/loptimizer/heuristics/utils/Join_Analyzer.java
@@ -2,25 +2,23 @@ package se.liu.ida.hefquin.engine.queryproc.impl.loptimizer.heuristics.utils;
 
 import org.apache.jena.graph.Node;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Join_Analyzer {
 
-    // Consider the subject-subject and object-object joins as star joins
+    // Count number of star joins ( i.e., subject-subject and object-object joins )
     public static double getNumOfStarJoins( final List<Node> vars_s, final List<Node> vars_o ){
         final int jsubs = countDuplicates(vars_s);
         final int jobjs = countDuplicates(vars_o);
         return ( jsubs + jobjs );
     }
 
-    // Consider the object-subject and subject-object joins as chain joins
+    // Count number of chain joins (i.e., object-subject and subject-object joins )
     public static double getNumOfChainJoins( final List<Node> vars_s, final List<Node> vars_o ) {
         return countDuplicates(vars_s,vars_o);
     }
 
-    // Consider subject-predicate, predicate-object, predicate-predicate joins as unusual joins
+    // Count number of unusual joins (i.e., subject-predicate, predicate-object, predicate-predicate joins )
     public static double getNumOfUnusualJoins( final List<Node> vars_s, final List<Node> vars_p, final List<Node> vars_o ) {
         return countDuplicates(vars_s,vars_p) + countDuplicates(vars_p,vars_o)+ countDuplicates(vars_p);
     }
@@ -30,28 +28,22 @@ public class Join_Analyzer {
      * e.g., if the input contains three ?s variable, the number of duplicates is 2
      */
     private static int countDuplicates( final List<Node> vars ) {
-        final Map<Node,Integer> map= new HashMap<>();
-
-        int count = 0;
-        for ( final Node s: vars ) {
-            if ( map.containsKey(s) ) {
-                count = map.get(s) + 1;
-            }
-            map.put( s, count );
-        }
+        final Set<Node> uniqueVars = new HashSet<>();
 
         int sum = 0;
-        for ( final Node s: map.keySet() ) {
-            if( map.get(s) > 0 ) {
-                sum += map.get(s);
+        for ( final Node s: vars ) {
+            if ( !uniqueVars.contains(s) ) {
+                uniqueVars.add(s);
             }
+            else
+                sum += 1;
         }
 
         return sum;
     }
 
     /**
-     * Count the number of matched pairs between two lists of variables
+     * Count the number of pairs that have the same variable between two lists of variables
      */
     private static int countDuplicates( final List<Node> vars_a, final List<Node> vars_b ) {
         final Map<Node,Integer> map= new HashMap<>();
