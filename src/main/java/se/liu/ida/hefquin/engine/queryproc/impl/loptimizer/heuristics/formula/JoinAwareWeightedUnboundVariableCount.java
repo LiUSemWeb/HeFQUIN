c9001c363909806d09d620d2f23251a97769858c
sijin <sijin.cheng@liu.se>
2023-02-08 12:02:56 +0100

    Remove updating 'boundVariable' when counting the number of unbound variables

--- src/main/java/se/liu/ida/hefquin/engine/queryproc/impl/loptimizer/heuristics/formula/JoinAwareWeightedUnboundVariableCount.java
+++ src/main/java/se/liu/ida/hefquin/engine/queryproc/impl/loptimizer/heuristics/formula/JoinAwareWeightedUnboundVariableCount.java
@@ -1,8 +1,8 @@
 package se.liu.ida.hefquin.engine.queryproc.impl.loptimizer.heuristics.formula;
 
 import org.apache.jena.graph.Node;
-import se.liu.ida.hefquin.engine.queryproc.impl.loptimizer.heuristics.utils.Join_Analyzer;
-import se.liu.ida.hefquin.engine.queryproc.impl.loptimizer.heuristics.utils.Query_Analyzer;
+import se.liu.ida.hefquin.engine.queryproc.impl.loptimizer.heuristics.utils.JoinAnalyzer;
+import se.liu.ida.hefquin.engine.queryproc.impl.loptimizer.heuristics.utils.QueryAnalyzer;
 
 import java.util.HashSet;
 import java.util.List;
@@ -29,13 +29,13 @@ public class JoinAwareWeightedUnboundVariableCount {
     protected static final double J_Ts = 0.5; // weight for star join
     protected static final double J_Tu = 1; // weight for unusual join
 
-    public static double estimate( final List<Query_Analyzer> selectedPlans, final Query_Analyzer subPlan ) {
+    public static double estimate( final List<QueryAnalyzer> selectedPlans, final QueryAnalyzer subPlan ) {
         // Add bound variables of all selected plans to a set
         final Set<Node> boundVariables = new HashSet<>();
 
         // This part can be optimized if all formulas need to consider bound variables:
         // adding to this set directly when adding a new subPlan to 'selectedPlans'
-        for ( final Query_Analyzer plan : selectedPlans ) {
+        for ( final QueryAnalyzer plan : selectedPlans ) {
             boundVariables.addAll( plan.getSubs() );
             boundVariables.addAll( plan.getPreds() );
             boundVariables.addAll( plan.getObjs() );
@@ -45,15 +45,17 @@ public class JoinAwareWeightedUnboundVariableCount {
     }
 
     // Formula (7) in paper "Heuristics-based Query Reordering for Federated Queries in SPARQL 1.1 and SPARQL-LD"
-    private static double calculateCost( final Set<Node> boundVariables, final Query_Analyzer subPlan ) {
+    private static double calculateCost( final Set<Node> boundVariables, final QueryAnalyzer subPlan ) {
         return weightedUnboundVarsCount(boundVariables, subPlan)
-                / weightedJoinsCount( subPlan );
+                / weightedJoinsCountInNextPlan( subPlan );
     }
 
     /**
-     * Calculate the weighed sum of subs, preds and objs
+     * Calculate the weighed sum of unbound variables based on different positions (subs, preds and objs)
+     * @param boundVariables All bound variables (i.e., variables that are bound through any sub-plan of the selectedPlans)
+     * @param plan The next candidate plan
      */
-    protected static double weightedUnboundVarsCount( final Set<Node> boundVariables, final Query_Analyzer plan ) {
+    protected static double weightedUnboundVarsCount( final Set<Node> boundVariables, final QueryAnalyzer plan ) {
         // Calculate the number of (unique) unbound subjects, predicates and objects
         final int totalSubs = countUnboundVars( plan.getSubs(), boundVariables);
         final int totalObjs = countUnboundVars( plan.getObjs(), boundVariables);
@@ -63,27 +65,23 @@ public class JoinAwareWeightedUnboundVariableCount {
     }
 
     /**
-     * For a given list of variables, count the number of unbound variables
-     * @param vars A list of variables, can be subs, preds or objects
-     * @param boundVariables All bound variables (including variables in selected plans and counted part of this subquery)
-     * @return The number of unbounded variables
+     * For a given list of variables, count the number of unique variables that are not in the set of 'boundVariables'
      */
-    private static int countUnboundVars( final List<Node> vars, final Set<Node> boundVariables ) {
+    protected static int countUnboundVars( final List<Node> vars, final Set<Node> boundVariables ) {
         final Set<Node> uniqueVars = new HashSet<>(vars);
         uniqueVars.removeAll(boundVariables);
 
-        boundVariables.addAll( uniqueVars );
         return uniqueVars.size();
     }
 
     /**
      * Calculate the weighed sum of different types of joins
      */
-    protected static double weightedJoinsCount( final Query_Analyzer plan ) {
+    protected static double weightedJoinsCountInNextPlan( final QueryAnalyzer plan ) {
         return 1
-                + Join_Analyzer.countNumOfStarJoins(plan) * J_Ts
-                + Join_Analyzer.countNumOfChainJoins(plan) * J_Tc
-                + Join_Analyzer.countNumOfUnusualJoins(plan) * J_Tu;
+                + ( JoinAnalyzer.countNumOfJoinsWithSameSub(plan) + JoinAnalyzer.countNumOfJoinsWithSameObj(plan)) * J_Ts
+                + JoinAnalyzer.countNumOfChainJoins(plan) * J_Tc
+                + JoinAnalyzer.countNumOfUnusualJoins(plan) * J_Tu;
     }
 
 }
