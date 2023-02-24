554b932393b480c42d0f08d9fef430afc1856275
sijin <sijin.cheng@liu.se>
2023-02-24 10:44:14 +0100

    Create an interface: Formula

---
+++ src/main/java/se/liu/ida/hefquin/engine/queryproc/impl/loptimizer/heuristics/formula/Formula.java
@@ -0,0 +1,11 @@
+package se.liu.ida.hefquin.engine.queryproc.impl.loptimizer.heuristics.formula;
+
+import se.liu.ida.hefquin.engine.queryproc.impl.loptimizer.heuristics.utils.QueryAnalyzer;
+
+import java.util.List;
+
+public interface Formula {
+
+    double estimate( final List<QueryAnalyzer> selectedPlans, final QueryAnalyzer subPlan );
+
+}
