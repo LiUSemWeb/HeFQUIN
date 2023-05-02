f6343589b41d24656f4364cea023963256371b6e
sijin <sijin.cheng@liu.se>
2023-05-02 17:08:44 +0200

    Extend the cost model to consider operators PhysicalOpLocalToGlobal and PhysicalOpGlobalToLocal

--- src/main/java/se/liu/ida/hefquin/engine/queryproc/impl/poptimizer/costmodel/CFRNumberOfVarsShippedInResponses.java
+++ src/main/java/se/liu/ida/hefquin/engine/queryproc/impl/poptimizer/costmodel/CFRNumberOfVarsShippedInResponses.java
@@ -84,7 +84,11 @@ public class CFRNumberOfVarsShippedInResponses extends CFRBase
 				throw createIllegalArgumentException(fm);
 			}
 		}
-		else if ( lop instanceof LogicalOpJoin || lop instanceof LogicalOpUnion || lop instanceof LogicalOpMultiwayUnion ) {
+		else if ( lop instanceof LogicalOpJoin
+				|| lop instanceof LogicalOpUnion
+				|| lop instanceof LogicalOpMultiwayUnion
+				|| lop instanceof LogicalOpLocalToGlobal
+				|| lop instanceof LogicalOpGlobalToLocal) {
 			return CompletableFuture.completedFuture(0);
 		}
 		else {
