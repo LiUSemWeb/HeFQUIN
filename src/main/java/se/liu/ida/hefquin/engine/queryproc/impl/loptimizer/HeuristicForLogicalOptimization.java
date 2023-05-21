48740c4f33984e6b93aaab18e4b9f62cdc1e7e94
Olaf Hartig <olaf.hartig@liu.se>
2023-05-21 18:26:41 +0200

    heuristics for logical optimization may throw a LogicalOptimizationException

--- src/main/java/se/liu/ida/hefquin/engine/queryproc/impl/loptimizer/HeuristicForLogicalOptimization.java
+++ src/main/java/se/liu/ida/hefquin/engine/queryproc/impl/loptimizer/HeuristicForLogicalOptimization.java
@@ -1,6 +1,7 @@
 package se.liu.ida.hefquin.engine.queryproc.impl.loptimizer;
 
 import se.liu.ida.hefquin.engine.queryplan.logical.LogicalPlan;
+import se.liu.ida.hefquin.engine.queryproc.LogicalOptimizationException;
 
 public interface HeuristicForLogicalOptimization
 {
@@ -8,6 +9,6 @@ public interface HeuristicForLogicalOptimization
 	 * Applies this heuristics to the given plan and returns
 	 * the resulting, potentially rewritten plan.
 	 */
-	LogicalPlan apply( LogicalPlan inputPlan );
+	LogicalPlan apply( LogicalPlan inputPlan ) throws LogicalOptimizationException;
 
 }
