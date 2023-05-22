6f76bb4d766f718fe7d67e088b35567245e79a38
sijin <sijin.cheng@liu.se>
2023-05-22 11:12:43 +0200

    Update src/main/java/se/liu/ida/hefquin/engine/data/mappings/impl/SchemaMappingImpl.java
    Co-authored-by: Olaf Hartig <olaf.hartig@liu.se>

--- src/main/java/se/liu/ida/hefquin/engine/data/mappings/impl/SchemaMappingImpl.java
+++ src/main/java/se/liu/ida/hefquin/engine/data/mappings/impl/SchemaMappingImpl.java
@@ -216,7 +216,7 @@ public class SchemaMappingImpl implements SchemaMapping
 		final Set<TermMapping> termMappings = vocMap.get(node);
 		final Set<Node> results = new HashSet<>();
 		if ( termMappings != null ) {
-			for (TermMapping mappingTypes : termMappings) {
+			for ( final TermMapping mappingTypes : termMappings ) {
 				if ( mappingTypes.getTypeOfRule().equals(OWL.equivalentClass.asNode()) || mappingTypes.getTypeOfRule().equals(OWL.equivalentProperty.asNode()) ) {
 					results.addAll(mappingTypes.getTranslatedTerms());
 				}
