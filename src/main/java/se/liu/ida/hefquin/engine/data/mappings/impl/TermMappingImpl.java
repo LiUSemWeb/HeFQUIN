340285b23cf781261c51c611028353dd2e5cdd73
sijin <sijin.cheng@liu.se>
2023-05-22 11:08:52 +0200

    Update src/main/java/se/liu/ida/hefquin/engine/data/mappings/impl/TermMappingImpl.java
    Co-authored-by: Olaf Hartig <olaf.hartig@liu.se>

--- src/main/java/se/liu/ida/hefquin/engine/data/mappings/impl/TermMappingImpl.java
+++ src/main/java/se/liu/ida/hefquin/engine/data/mappings/impl/TermMappingImpl.java
@@ -7,8 +7,8 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class TermMappingImpl implements TermMapping {
-    final Node type;
-    final Set<Node> translatedTerms = new HashSet<>();
+    protected final Node type;
+    protected final Set<Node> translatedTerms = new HashSet<>();
 
     public TermMappingImpl( final Node type ) {
         this.type = type;
