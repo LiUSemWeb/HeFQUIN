6715b2d4fa24c26abf94716a9edd5877c837b9c7
sijin <sijin.cheng@liu.se>
2023-05-22 09:30:50 +0200

    Add an 'TermMapping' interface and the corresponding implementation

---
+++ src/main/java/se/liu/ida/hefquin/engine/data/mappings/impl/TermMappingImpl.java
@@ -0,0 +1,35 @@
+package se.liu.ida.hefquin.engine.data.mappings.impl;
+
+import org.apache.jena.graph.Node;
+import se.liu.ida.hefquin.engine.data.mappings.TermMapping;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class TermMappingImpl implements TermMapping {
+    final Node type;
+    final Set<Node> translatedTerms = new HashSet<>();
+
+    public TermMappingImpl( final Node type ) {
+        this.type = type;
+    }
+
+    public void addTranslatedTerm( final Node term ) {
+        translatedTerms.add(term);
+    }
+
+    public void addTranslatedTerm( final Set<Node> term ) {
+        translatedTerms.addAll(term);
+    }
+
+    @Override
+    public Node getTypeOfRule() {
+        return type;
+    }
+
+    @Override
+    public Set<Node> getTranslatedTerms() {
+        return translatedTerms;
+    }
+
+}
