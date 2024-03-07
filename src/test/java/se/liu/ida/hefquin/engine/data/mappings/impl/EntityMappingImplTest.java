d3c667748943ac229bb95dfe3276aebf3cd8133b
Olaf Hartig <olaf.hartig@liu.se>
2024-03-07 12:52:46 +0100

    cleans up and fixes EntityMappingImpl, and adds unit tests for it

--- src/test/java/se/liu/ida/hefquin/engine/data/mappings/impl/EntityMappingImplTest.java
+++ src/test/java/se/liu/ida/hefquin/engine/data/mappings/impl/EntityMappingImplTest.java
@@ -1,11 +1,15 @@
 package se.liu.ida.hefquin.engine.data.mappings.impl;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
 
@@ -13,6 +17,7 @@ import org.apache.commons.io.IOUtils;
 import org.apache.jena.graph.Graph;
 import org.apache.jena.graph.Node;
 import org.apache.jena.graph.NodeFactory;
+import org.apache.jena.graph.Triple;
 import org.apache.jena.riot.Lang;
 import org.apache.jena.riot.RDFDataMgr;
 import org.apache.jena.sparql.core.Var;
@@ -20,45 +25,26 @@ import org.apache.jena.sparql.graph.GraphFactory;
 import org.junit.Test;
 
 import se.liu.ida.hefquin.engine.data.SolutionMapping;
+import se.liu.ida.hefquin.engine.data.impl.SolutionMappingImpl;
 import se.liu.ida.hefquin.engine.data.mappings.EntityMapping;
 import se.liu.ida.hefquin.engine.data.utils.SolutionMappingUtils;
+import se.liu.ida.hefquin.engine.query.TriplePattern;
+import se.liu.ida.hefquin.engine.query.impl.TriplePatternImpl;
 
-
-
-public class EntityMappingImplTest {
-
-	protected class EntityMappingImplForTests extends EntityMappingImpl {
-
-		public EntityMappingImplForTests(final Graph mappingDescription) {
-			super(mappingDescription);
-		}
-		
-		public EntityMappingImplForTests( final Map<Node, Set<Node>> g2l, final Map<Node, Set<Node>> l2g) {
-			super(g2l,l2g);
-		}
-		
-		public Map<Node, Set<Node>> getG2lMap() {
-			return g2lMap;
-		}
-		
-		public Map<Node, Set<Node>> getL2gMap() {
-			return l2gMap;
-		}
-		
-	}
-	
+public class EntityMappingImplTest
+{
 	@Test
 	public void parseMappingDescriptionTest() throws IOException {
 		final String mappingAsTurtle =
 				  "@prefix ex:   <http://example.org/> .  \n"
 				+ "@prefix owl:  <http://www.w3.org/2002/07/owl#> . \n"
-				+ "ex:Bob owl:sameAs ex:Robert . "
-				+ "ex:Bob owl:sameAs ex:Bobby . "
-				+ "ex:Ali owl:sameAs ex:Alibaba . ";
+				+ "ex:Robert owl:sameAs ex:Bob . "
+				+ "ex:Bobby owl:sameAs ex:Bob . "
+				+ "ex:Alibaba owl:sameAs ex:Ali . ";
 		final Graph mapping = GraphFactory.createDefaultGraph();
 		RDFDataMgr.read(mapping, IOUtils.toInputStream(mappingAsTurtle, "UTF-8"), Lang.TURTLE);
 		
-		final EntityMappingImplForTests entityMapping = new EntityMappingImplForTests(mapping);
+		final EntityMappingImpl entityMapping = new EntityMappingImpl(mapping);
 		
 		// Create global nodes.
 		final Node bob = NodeFactory.createURI("http://example.org/Bob");
@@ -90,66 +76,324 @@ public class EntityMappingImplTest {
 		l2gMap.put(bobby, globalBob);
 		l2gMap.put(robert, globalBob);
 		
-		assertEquals( g2lMap, entityMapping.getG2lMap() );
-		assertEquals( l2gMap, entityMapping.getL2gMap() );
+		assertEquals( g2lMap, entityMapping.g2lMap );
+		assertEquals( l2gMap, entityMapping.l2gMap );
 	}
-	
-	@Test
-	public void applyToSolutionMappingTest() {
-		
-		// Create solution mappings.
-		final Node bob = NodeFactory.createURI("ex:Bob");
-		final Node ali = NodeFactory.createURI("ex:Ali");
-		final Node charles = NodeFactory.createURI("ex:Charles");
-		final Var x = Var.alloc("x");
-		final Var y = Var.alloc("y");
-		final Var z = Var.alloc("z");
-		
-		final SolutionMapping solutionMapping = SolutionMappingUtils.createSolutionMapping(x, bob, y, ali, z, charles);
 
-		// Create local vocabulary.
-		final Node robert = NodeFactory.createURI("ex:Robert");
-		final Node bobby = NodeFactory.createURI("ex:Bobby");
-		final Node alibaba = NodeFactory.createURI("ex:Alibaba");
-		final Node charlie = NodeFactory.createURI("ex:Charlie");
-		final Node carolus = NodeFactory.createURI("ex:Carolus");
-		final Node karl = NodeFactory.createURI("ex:Karl");
-		
-		final Set<Node> localAli = new HashSet<>();
-		localAli.add(alibaba);
-		final Set<Node> localBob = new HashSet<>();
-		localBob.add(bobby);
-		localBob.add(robert);
-		final Set<Node> localCharles = new HashSet<>();
-		localCharles.add(charlie);
-		localCharles.add(carolus);
-		localCharles.add(karl);
-		
-		final Map<Node, Set<Node>> g2lMap = new HashMap<>();
-		g2lMap.put(ali, localAli);
-		g2lMap.put(bob, localBob);
-		g2lMap.put(charles, localCharles);
-		final Map<Node, Set<Node>> l2gMap = new HashMap<>();
-		
-		final EntityMapping entityMapping = new EntityMappingImpl(g2lMap, l2gMap);
-		
-		final Set<SolutionMapping> resultSet = entityMapping.applyToSolutionMapping(solutionMapping);
-		
-		// Create cartesian product
-		final SolutionMapping robertCharlie = SolutionMappingUtils.createSolutionMapping(x, robert, y, alibaba, z, charlie);
-		final SolutionMapping robertCarolus = SolutionMappingUtils.createSolutionMapping(x, robert, y, alibaba, z, carolus);
-		final SolutionMapping robertKarl = SolutionMappingUtils.createSolutionMapping(x, robert, y, alibaba, z, karl);
-		final SolutionMapping bobbyCharlie = SolutionMappingUtils.createSolutionMapping(x, bobby, y, alibaba, z, charlie);
-		final SolutionMapping bobbyCarolus = SolutionMappingUtils.createSolutionMapping(x, bobby, y, alibaba, z, carolus);
-		final SolutionMapping bobbyKarl = SolutionMappingUtils.createSolutionMapping(x, bobby, y, alibaba, z, karl);
-	
-		// See to it that the result set contains the cartesian product and only the cartesian product.
-		assertEquals( 6, resultSet.size() );
-		assertTrue(resultSet.contains(robertCharlie));
-		assertTrue(resultSet.contains(robertCarolus));
-		assertTrue(resultSet.contains(robertKarl));
-		assertTrue(resultSet.contains(bobbyCharlie));
-		assertTrue(resultSet.contains(bobbyCarolus));
-		assertTrue(resultSet.contains(bobbyKarl));
+	@Test
+	public void testApplyToTriplePattern_NoChange1() {
+		final Node gBob  = NodeFactory.createURI("http://example.org/global/Bob");
+		final Node lBob = NodeFactory.createURI("http://example.org/local/Bob");
+
+		final Set<Node> lBobSet = Collections.singleton(lBob);
+		final Map<Node, Set<Node>> g2lMap = Collections.singletonMap(gBob, lBobSet);
+
+		final Node s = NodeFactory.createURI("http://example.org/s");
+		final Node p = NodeFactory.createURI("http://example.org/p");
+		final Node o = NodeFactory.createURI("http://example.org/o");
+		final TriplePattern tp = new TriplePatternImpl(s, p, o);
+
+		final EntityMapping em = new EntityMappingImpl(g2lMap);
+
+		final Set<TriplePattern> tps = em.applyToTriplePattern(tp);
+		assertEquals( 1, tps.size() );
+		assertEquals( tp, tps.iterator().next() );
 	}
+
+	@Test
+	public void testApplyToTriplePattern_NoChange2() {
+		// Tests that URIs in the predicate position are ignored.
+		final Node gBob  = NodeFactory.createURI("http://example.org/global/Bob");
+		final Node lBob = NodeFactory.createURI("http://example.org/local/Bob");
+
+		final Set<Node> lBobSet = Collections.singleton(lBob);
+		final Map<Node, Set<Node>> g2lMap = Collections.singletonMap(gBob, lBobSet);
+
+		final Node s = NodeFactory.createURI("http://example.org/s");
+		final Node p = gBob;
+		final Node o = NodeFactory.createURI("http://example.org/o");
+		final TriplePattern tp = new TriplePatternImpl(s, p, o);
+
+		final EntityMapping em = new EntityMappingImpl(g2lMap);
+
+		final Set<TriplePattern> tps = em.applyToTriplePattern(tp);
+		assertEquals( 1, tps.size() );
+		assertEquals( tp, tps.iterator().next() );
+	}
+
+	@Test
+	public void testApplyToTriplePattern_ChangeSubject() {
+		final Node gBob  = NodeFactory.createURI("http://example.org/global/Bob");
+		final Node lBob = NodeFactory.createURI("http://example.org/local/Bob");
+
+		final Set<Node> lBobSet = Collections.singleton(lBob);
+		final Map<Node, Set<Node>> g2lMap = Collections.singletonMap(gBob, lBobSet);
+
+		final Node s = gBob;
+		final Node p = NodeFactory.createURI("http://example.org/p");
+		final Node o = NodeFactory.createURI("http://example.org/o");
+		final TriplePattern tp = new TriplePatternImpl(s, p, o);
+
+		final EntityMapping em = new EntityMappingImpl(g2lMap);
+
+		final Set<TriplePattern> tps = em.applyToTriplePattern(tp);
+		assertEquals( 1, tps.size() );
+
+		final Triple tpOut = tps.iterator().next().asJenaTriple();
+		assertEquals( lBob, tpOut.getSubject() );
+		assertEquals( p,    tpOut.getPredicate() );
+		assertEquals( o,    tpOut.getObject() );
+	}
+
+	@Test
+	public void testApplyToTriplePattern_ChangeSubjectAndObject1() {
+		final Node gBob  = NodeFactory.createURI("http://example.org/global/Bob");
+		final Node lBob = NodeFactory.createURI("http://example.org/local/Bob");
+
+		final Set<Node> lBobSet = Collections.singleton(lBob);
+		final Map<Node, Set<Node>> g2lMap = Collections.singletonMap(gBob, lBobSet);
+
+		final Node s = gBob;
+		final Node p = NodeFactory.createURI("http://example.org/p");
+		final Node o = gBob;
+		final TriplePattern tp = new TriplePatternImpl(s, p, o);
+
+		final EntityMapping em = new EntityMappingImpl(g2lMap);
+
+		final Set<TriplePattern> tps = em.applyToTriplePattern(tp);
+		assertEquals( 1, tps.size() );
+
+		final Triple tpOut = tps.iterator().next().asJenaTriple();
+		assertEquals( lBob, tpOut.getSubject() );
+		assertEquals( p,    tpOut.getPredicate() );
+		assertEquals( lBob, tpOut.getObject() );
+	}
+
+	@Test
+	public void testApplyToTriplePattern_ChangeSubjectAndObject2() {
+		final Node gBob  = NodeFactory.createURI("http://example.org/global/Bob");
+		final Node lBob1 = NodeFactory.createURI("http://example.org/local/Bob1");
+		final Node lBob2 = NodeFactory.createURI("http://example.org/local/Bob2");
+
+		final Set<Node> lBobSet = new HashSet<>();
+		lBobSet.add(lBob1);
+		lBobSet.add(lBob2);
+		final Map<Node, Set<Node>> g2lMap = Collections.singletonMap(gBob, lBobSet);
+
+		final Node s = gBob;
+		final Node p = NodeFactory.createURI("http://example.org/p");
+		final Node o = gBob;
+		final TriplePattern tp = new TriplePatternImpl(s, p, o);
+
+		final EntityMapping em = new EntityMappingImpl(g2lMap);
+
+		final Set<TriplePattern> tps = em.applyToTriplePattern(tp);
+		assertEquals( 4, tps.size() );
+
+		// check the subjects, each lBob must appear twice
+		int cntBob1 = 0;
+		int cntBob2 = 0;
+		for ( final TriplePattern tpOut : tps ) {
+			assertEquals( p, tpOut.asJenaTriple().getPredicate() );
+			if ( tpOut.asJenaTriple().getSubject().equals(lBob1) ) cntBob1++;
+			if ( tpOut.asJenaTriple().getSubject().equals(lBob2) ) cntBob2++;
+		}
+		assertEquals(2, cntBob1);
+		assertEquals(2, cntBob2);
+
+		// check the objects, each lBob must appear twice
+		cntBob1 = 0;
+		cntBob2 = 0;
+		for ( final TriplePattern tpOut : tps ) {
+			if ( tpOut.asJenaTriple().getObject().equals(lBob1) ) cntBob1++;
+			if ( tpOut.asJenaTriple().getObject().equals(lBob2) ) cntBob2++;
+		}
+		assertEquals(2, cntBob1);
+		assertEquals(2, cntBob2);
+	}
+
+	@Test
+	public void testApplyToTriplePattern_ChangeObject1() {
+		final Node gBob  = NodeFactory.createURI("http://example.org/global/Bob");
+		final Node lBob = NodeFactory.createURI("http://example.org/local/Bob");
+
+		final Set<Node> lBobSet = Collections.singleton(lBob);
+		final Map<Node, Set<Node>> g2lMap = Collections.singletonMap(gBob, lBobSet);
+
+		final Node s = NodeFactory.createURI("http://example.org/s");
+		final Node p = NodeFactory.createURI("http://example.org/p");
+		final Node o = gBob;
+		final TriplePattern tp = new TriplePatternImpl(s, p, o);
+
+		final EntityMapping em = new EntityMappingImpl(g2lMap);
+
+		final Set<TriplePattern> tps = em.applyToTriplePattern(tp);
+		assertEquals( 1, tps.size() );
+
+		final Triple tpOut = tps.iterator().next().asJenaTriple();
+		assertEquals( s,    tpOut.getSubject() );
+		assertEquals( p,    tpOut.getPredicate() );
+		assertEquals( lBob, tpOut.getObject() );
+	}
+
+	@Test
+	public void testApplyToTriplePattern_ChangeObject2() {
+		final Node gBob  = NodeFactory.createURI("http://example.org/global/Bob");
+		final Node lBob1 = NodeFactory.createURI("http://example.org/local/Bob1");
+		final Node lBob2 = NodeFactory.createURI("http://example.org/local/Bob2");
+
+		final Set<Node> lBobSet = new HashSet<>();
+		lBobSet.add(lBob1);
+		lBobSet.add(lBob2);
+		final Map<Node, Set<Node>> g2lMap = Collections.singletonMap(gBob, lBobSet);
+
+		final Node s = NodeFactory.createURI("http://example.org/s");
+		final Node p = NodeFactory.createURI("http://example.org/p");
+		final Node o = gBob;
+		final TriplePattern tp = new TriplePatternImpl(s, p, o);
+
+		final EntityMapping em = new EntityMappingImpl(g2lMap);
+
+		final Iterator<TriplePattern> it = em.applyToTriplePattern(tp).iterator();
+
+		assertTrue( it.hasNext() );
+		final Triple tpOut1 = it.next().asJenaTriple();
+
+		assertTrue( it.hasNext() );
+		final Triple tpOut2 = it.next().asJenaTriple();
+
+		assertFalse( it.hasNext() );
+
+		assertEquals( s, tpOut1.getSubject() );
+		assertEquals( s, tpOut2.getSubject() );
+		assertEquals( p, tpOut1.getPredicate() );
+		assertEquals( p, tpOut2.getPredicate() );
+
+		assertTrue( tpOut1.getObject().equals(lBob1) || tpOut2.getObject().equals(lBob1) );
+		assertTrue( tpOut1.getObject().equals(lBob2) || tpOut2.getObject().equals(lBob2) );
+	}
+
+	@Test
+	public void testApplyMapToSolutionMapping_EmptySolutionMapping() {
+		final Node xURI1 = NodeFactory.createURI("http://example.org/uri1");
+		final Node yURI1a = NodeFactory.createURI("http://example.org/uri1a");
+		final Node yURI1b = NodeFactory.createURI("http://example.org/uri1b");
+		final Map<Node, Set<Node>> g2lMap = createG2LMap(xURI1, yURI1a, yURI1b);
+
+		final SolutionMapping emptySolMap = new SolutionMappingImpl();
+
+		// Apply the mapping
+		final Set<SolutionMapping> r = EntityMappingImpl.applyMapToSolutionMapping(emptySolMap, g2lMap);
+
+		assertEquals( 1, r.size() );
+		assertTrue( r.contains(emptySolMap) );
+	}
+
+	@Test
+	public void testApplyMapToSolutionMapping_SolutionMappingWithUnmappedURI() {
+		final Node xURI1 = NodeFactory.createURI("http://example.org/uri1");
+		final Node yURI1a = NodeFactory.createURI("http://example.org/uri1a");
+		final Node yURI1b = NodeFactory.createURI("http://example.org/uri1b");
+		final Map<Node, Set<Node>> g2lMap = createG2LMap(xURI1, yURI1a, yURI1b);
+
+		final Var var1 = Var.alloc("var1");
+		final Node xURI2 = NodeFactory.createURI("http://example.org/uri2");
+		final SolutionMapping sm = SolutionMappingUtils.createSolutionMapping(var1, xURI2);
+
+		// Apply the mapping
+		final Set<SolutionMapping> r = EntityMappingImpl.applyMapToSolutionMapping(sm, g2lMap);
+
+		assertEquals( 1, r.size() );
+		assertTrue( r.contains(sm) );
+	}
+
+	@Test
+	public void testApplyMapToSolutionMapping_SolutionMappingWithMappedURI() {
+		final Node xURI1 = NodeFactory.createURI("http://example.org/uri1");
+		final Node yURI1a = NodeFactory.createURI("http://example.org/uri1a");
+		final Node yURI1b = NodeFactory.createURI("http://example.org/uri1b");
+		final Map<Node, Set<Node>> g2lMap = createG2LMap(xURI1, yURI1a, yURI1b);
+
+		final Var var1 = Var.alloc("var1");
+		final Var var2 = Var.alloc("var2");
+		final Node xURI2 = NodeFactory.createURI("http://example.org/uri2");
+		final SolutionMapping sm = SolutionMappingUtils.createSolutionMapping(var1, xURI1, var2, xURI2);
+
+		// Apply the mapping
+		final Set<SolutionMapping> r = EntityMappingImpl.applyMapToSolutionMapping(sm, g2lMap);
+
+		assertEquals( 2, r.size() );
+
+		final SolutionMapping expectSM1 = SolutionMappingUtils.createSolutionMapping(var1, yURI1a, var2, xURI2);
+		final SolutionMapping expectSM2 = SolutionMappingUtils.createSolutionMapping(var1, yURI1b, var2, xURI2);
+
+		boolean sm1Found = false;
+		boolean sm2Found = false;
+		for ( final SolutionMapping s : r ) {
+			if ( s.equals(expectSM1) ) sm1Found = true;
+			if ( s.equals(expectSM2) ) sm2Found = true;
+		}
+
+		assertTrue(sm1Found);
+		assertTrue(sm2Found);
+	}
+
+	@Test
+	public void testApplyMapToSolutionMapping_SolutionMappingWithTwoMappedURIs() {
+		final Node xURI1 = NodeFactory.createURI("http://example.org/uri1");
+		final Node yURI1a = NodeFactory.createURI("http://example.org/uri1a");
+		final Node yURI1b = NodeFactory.createURI("http://example.org/uri1b");
+		final Map<Node, Set<Node>> g2lMap = createG2LMap(xURI1, yURI1a, yURI1b);
+
+		final Node xURI2 = NodeFactory.createURI("http://example.org/uri2");
+		final Node yURI2a = NodeFactory.createURI("http://example.org/uri2a");
+		final Node yURI2b = NodeFactory.createURI("http://example.org/uri2b");
+		final Set<Node> locals2 = new HashSet<>();
+		locals2.add(yURI2a);
+		locals2.add(yURI2b);
+		g2lMap.put(xURI2, locals2);
+		
+		final Var var1 = Var.alloc("var1");
+		final Var var2 = Var.alloc("var2");
+		final SolutionMapping sm = SolutionMappingUtils.createSolutionMapping(var1, xURI1, var2, xURI2);
+
+		// Apply the mapping
+		final Set<SolutionMapping> r = EntityMappingImpl.applyMapToSolutionMapping(sm, g2lMap);
+
+		assertEquals( 4, r.size() );
+
+		final SolutionMapping expectSM1 = SolutionMappingUtils.createSolutionMapping(var1, yURI1a, var2, yURI2a);
+		final SolutionMapping expectSM2 = SolutionMappingUtils.createSolutionMapping(var1, yURI1a, var2, yURI2b);
+		final SolutionMapping expectSM3 = SolutionMappingUtils.createSolutionMapping(var1, yURI1b, var2, yURI2a);
+		final SolutionMapping expectSM4 = SolutionMappingUtils.createSolutionMapping(var1, yURI1b, var2, yURI2b);
+
+		boolean sm1Found = false;
+		boolean sm2Found = false;
+		boolean sm3Found = false;
+		boolean sm4Found = false;
+		for ( final SolutionMapping s : r ) {
+			if ( s.equals(expectSM1) ) sm1Found = true;
+			if ( s.equals(expectSM2) ) sm2Found = true;
+			if ( s.equals(expectSM3) ) sm3Found = true;
+			if ( s.equals(expectSM4) ) sm4Found = true;
+		}
+
+		assertTrue(sm1Found);
+		assertTrue(sm2Found);
+		assertTrue(sm3Found);
+		assertTrue(sm4Found);
+	}
+
+
+	// ------------ helpers --------------
+
+	protected Map<Node, Set<Node>> createG2LMap( final Node g, final Node... localsForG ) {
+		final Set<Node> lSet = new HashSet<>( Arrays.asList(localsForG) );
+
+		final Map<Node, Set<Node>> m = new HashMap<>();
+		m.put(g, lSet);
+
+		return m;
+	}
+
 }
