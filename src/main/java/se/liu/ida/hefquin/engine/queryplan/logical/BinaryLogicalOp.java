65bf2ffb5f6acea50144beada327ac89569866ae
Olaf Hartig <olaf.hartig@liu.se>
2024-07-08 22:30:55 +0200

    adds Javadoc comments for the interfaces related to logical operators

--- src/main/java/se/liu/ida/hefquin/engine/queryplan/logical/BinaryLogicalOp.java
+++ src/main/java/se/liu/ida/hefquin/engine/queryplan/logical/BinaryLogicalOp.java
@@ -1,5 +1,10 @@
 package se.liu.ida.hefquin.engine.queryplan.logical;
 
+/**
+ * An interface for any type of {@link LogicalOperator} that has
+ * an arity of two; i.e., it is defined of as a function over two
+ * multisets of solution mappings.
+ */
 public interface BinaryLogicalOp extends LogicalOperator
 {
 
--- src/main/java/se/liu/ida/hefquin/engine/queryplan/logical/NaryLogicalOp.java
+++ src/main/java/se/liu/ida/hefquin/engine/queryplan/logical/NaryLogicalOp.java
@@ -1,5 +1,10 @@
 package se.liu.ida.hefquin.engine.queryplan.logical;
 
+/**
+ * An interface for any type of {@link LogicalOperator} that has an
+ * arbitrary arity ; i.e., it is defined of as a function over an
+ * arbitrary number of multisets of solution mappings.
+ */
 public interface NaryLogicalOp extends LogicalOperator
 {
 
