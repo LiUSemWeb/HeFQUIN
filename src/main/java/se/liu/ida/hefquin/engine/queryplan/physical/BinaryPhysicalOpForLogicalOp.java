178d82324cc1e5baa6bef1efefbb97dee75d0567
Olaf Hartig <olaf.hartig@liu.se>
2024-07-08 14:08:43 +0200

    adds Javadoc comments for the interfaces related to physical operators

--- src/main/java/se/liu/ida/hefquin/engine/queryplan/physical/BinaryPhysicalOpForLogicalOp.java
+++ src/main/java/se/liu/ida/hefquin/engine/queryplan/physical/BinaryPhysicalOpForLogicalOp.java
@@ -2,6 +2,14 @@ package se.liu.ida.hefquin.engine.queryplan.physical;
 
 import se.liu.ida.hefquin.engine.queryplan.logical.BinaryLogicalOp;
 
+/**
+ * An interface for any type of {@link PhysicalOperator}
+ * that directly implements a particular logical operator
+ * that has an arity of two.
+ *
+ * That logical operator can be accessed via the
+ * {@link #getLogicalOperator()} function.
+ */
 public interface BinaryPhysicalOpForLogicalOp extends PhysicalOperatorForLogicalOperator, BinaryPhysicalOp
 {
 	@Override
--- src/main/java/se/liu/ida/hefquin/engine/queryplan/physical/NaryPhysicalOp.java
+++ src/main/java/se/liu/ida/hefquin/engine/queryplan/physical/NaryPhysicalOp.java
@@ -3,6 +3,10 @@ package se.liu.ida.hefquin.engine.queryplan.physical;
 import se.liu.ida.hefquin.engine.queryplan.ExpectedVariables;
 import se.liu.ida.hefquin.engine.queryplan.executable.NaryExecutableOp;
 
+/**
+ * An interface for any type of {@link PhysicalOperator} whose algorithm
+ * consumes an arbitrary number of sequences of solution mappings as input.
+ */
 public interface NaryPhysicalOp extends PhysicalOperator
 {
 	@Override
