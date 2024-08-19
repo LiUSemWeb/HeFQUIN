c2ee049cc81ec994b910fd74c363df9cf5ca290b
Huanyu Li <huanyu.li@liu.se>
2024-08-19 10:34:12 +0200

    To implement a new plan walker instead of using visitors

--- src/main/java/se/liu/ida/hefquin/engine/queryplan/utils/TextBasedLogicalPlanPrinterImpl2.java
+++ src/main/java/se/liu/ida/hefquin/engine/queryplan/utils/TextBasedLogicalPlanPrinterImpl2.java
@@ -49,266 +49,35 @@ public class TextBasedLogicalPlanPrinterImpl2 implements LogicalPlanPrinter
 		return indentLevelString;
 	}
 	
+	/*
+	public String testprint(int level) {
+		String indentLevelString = "";
+		for ( int i = 0; i < level; i++ ) {
+			indentLevelString += levelIndentBase;
+		}
+		return indentLevelString;
+	}
+	*/
+	
 	public void planWalk( final LogicalPlan plan, int planNumber, int planLevel, int numberOfSiblings, IndentingPrintStream out) {
 		//System.out.println(getIndentLevelString2(planLevel, planNumber, numberOfSiblings) + plan.getRootOperator().toString() +" "+planLevel + " " + planNumber + " "+ numberOfSiblings);
 		//System.out.println(getIndentLevelString2(planLevel, planNumber, numberOfSiblings) + plan.getRootOperator().toString() );
 		//System.out.println(plan.getRootOperator().printString(getIndentLevelString2(planLevel, planNumber, numberOfSiblings)));
-		out.append( plan.getRootOperator().printString(getIndentLevelString(planNumber, planLevel, numberOfSiblings)) );
+		String indentLevelString = getIndentLevelString(planNumber, planLevel, numberOfSiblings);
+		if (planNumber < numberOfSiblings-1) {
+			//out.append( plan.getRootOperator().printString(indentLevelString,   levelIndentBase) );
+			out.append( plan.getRootOperator().toPrintString(indentLevelString) );
+		}
+		else {
+			//out.append( plan.getRootOperator().printString(indentLevelString,   levelIndentBase) );
+			out.append( plan.getRootOperator().toPrintString(indentLevelString) );
+		}
 		out.append(System.lineSeparator());
 		planLevel += 1;
 		for ( int i = 0; i < plan.numberOfSubPlans(); ++i ) {
 			planWalk( plan.getSubPlan(i), i, planLevel, plan.numberOfSubPlans(), out );
 		}
 		
-	}
-
-
-	protected  class MyBeforeVisitor implements LogicalPlanVisitor {
-
-		protected final IndentingPrintStream out;
-		
-		protected boolean flagOne = false; 
-		
-		protected boolean flagTwo = false;
-
-		public MyBeforeVisitor( final IndentingPrintStream out ) {
-			this.out = out;
-		}
-
-		@Override
-		public void visit(final LogicalOpRequest<?, ?> op) {
-			//out.appendIndentation();
-			out.appendLevelIndentation(flagOne, flagTwo);
-			out.append( op.toString() );
-			out.append(System.lineSeparator());
-			flagOne = false;
-		}
-
-		@Override
-		public void visit(final LogicalOpTPAdd op) {
-			out.appendIndentation();
-			out.append( op.toString() );
-			out.append(System.lineSeparator());
-			out.increaseIndentationLevel();
-		}
-
-		@Override
-		public void visit(final LogicalOpBGPAdd op) {
-			out.appendIndentation();
-			out.append( op.toString() );
-			out.append(System.lineSeparator());
-			out.increaseIndentationLevel();
-		}
-
-		@Override
-		public void visit(final LogicalOpGPAdd op) {
-			out.appendIndentation();
-			out.append( op.toString() );
-			out.append(System.lineSeparator());
-			out.increaseIndentationLevel();
-		}
-
-		@Override
-		public void visit(final LogicalOpTPOptAdd op) {
-			out.appendIndentation();
-			out.append( op.toString() );
-			out.append(System.lineSeparator());
-			out.increaseIndentationLevel();
-		}
-
-		@Override
-		public void visit(final LogicalOpBGPOptAdd op) {
-			out.appendIndentation();
-			out.append( op.toString() );
-			out.append(System.lineSeparator());
-			out.increaseIndentationLevel();
-		}
-
-		@Override
-		public void visit(final LogicalOpGPOptAdd op) {
-			out.appendIndentation();
-			out.append( op.toString() );
-			out.append(System.lineSeparator());
-			out.increaseIndentationLevel();
-		}
-
-		@Override
-		public void visit(final LogicalOpJoin op) {
-			out.appendIndentation();
-			out.append( op.toString() );
-			out.append(System.lineSeparator());
-			out.increaseIndentationLevel();
-		}
-
-		@Override
-		public void visit(final LogicalOpRightJoin op) {
-			out.appendIndentation();
-			out.append( op.toString() );
-			out.append(System.lineSeparator());
-			out.increaseIndentationLevel();
-		}
-
-		@Override
-		public void visit(final LogicalOpUnion op) {
-			out.appendIndentation();
-			out.append( op.toString() );
-			out.append(System.lineSeparator());
-			out.increaseIndentationLevel();
-		}
-
-		@Override
-		public void visit(final LogicalOpMultiwayJoin op) {
-			//out.appendIndentation();
-			out.appendLevelIndentation(flagOne, flagTwo);
-			out.append( op.toString() );
-			out.append(System.lineSeparator());
-			out.increaseIndentationLevel();
-			flagOne = true;
-		}
-
-		@Override
-		public void visit(final LogicalOpMultiwayLeftJoin op) {
-			out.appendIndentation();
-			out.append( op.toString() );
-			out.append( System.lineSeparator() );
-			out.increaseIndentationLevel();
-		}
-
-		@Override
-		public void visit(final LogicalOpMultiwayUnion op) {
-			//out.appendIndentation();
-			out.appendLevelIndentation(flagOne, flagTwo);
-			out.append( op.toString() );
-			out.append(System.lineSeparator());
-			out.increaseIndentationLevel();
-			flagOne = true;
-		}
-		
-		@Override
-		public void visit(final LogicalOpFilter op) {
-			out.appendIndentation();
-			out.append( op.toString() );
-			out.append(System.lineSeparator());
-			out.increaseIndentationLevel();
-		}
-
-		@Override
-		public void visit(final LogicalOpBind op) {
-			out.appendIndentation();
-			out.append( op.toString() );
-			out.append(System.lineSeparator());
-			out.increaseIndentationLevel();
-		}
-		
-		@Override
-		public void visit(final LogicalOpLocalToGlobal op) {
-			out.appendIndentation();
-			out.append( op.toString() );
-			out.append(System.lineSeparator());
-			out.increaseIndentationLevel();
-		}
-		
-		@Override
-		public void visit(final LogicalOpGlobalToLocal op) {
-			out.appendIndentation();
-			out.append( op.toString() );
-			out.append(System.lineSeparator());
-			out.increaseIndentationLevel();
-		}
-	}
-
-	protected class MyAfterVisitor implements LogicalPlanVisitor {
-
-		protected final IndentingPrintStream out;
-
-		public MyAfterVisitor( final IndentingPrintStream out ) {
-			this.out = out;
-		}
-
-		@Override
-		public void visit(final LogicalOpRequest<?, ?> op) {
-			//nothing to do here
-		}
-
-		@Override
-		public void visit(final LogicalOpTPAdd op) {
-			out.decreaseIndentationLevel();
-		}
-
-		@Override
-		public void visit(final LogicalOpBGPAdd op) {
-			out.decreaseIndentationLevel();
-		}
-
-		@Override
-		public void visit(final LogicalOpGPAdd op) {
-			out.decreaseIndentationLevel();
-		}
-
-		@Override
-		public void visit(final LogicalOpTPOptAdd op) {
-			out.decreaseIndentationLevel();
-		}
-
-		@Override
-		public void visit(final LogicalOpBGPOptAdd op) {
-			out.decreaseIndentationLevel();
-		}
-
-		@Override
-		public void visit(final LogicalOpGPOptAdd op) {
-			out.decreaseIndentationLevel();
-		}
-
-		@Override
-		public void visit(final LogicalOpJoin op) {
-			out.decreaseIndentationLevel();
-		}
-
-		@Override
-		public void visit(final LogicalOpRightJoin op) {
-			out.decreaseIndentationLevel();
-		}
-
-		@Override
-		public void visit(final LogicalOpUnion op) {
-			out.decreaseIndentationLevel();
-		}
-
-		@Override
-		public void visit(final LogicalOpMultiwayJoin op) {
-			out.decreaseIndentationLevel();
-		}
-
-		@Override
-		public void visit(final LogicalOpMultiwayLeftJoin op) {
-			out.decreaseIndentationLevel();
-		}
-
-		@Override
-		public void visit(final LogicalOpMultiwayUnion op) {
-			out.decreaseIndentationLevel();
-		}
-
-		@Override
-		public void visit(final LogicalOpFilter op) {
-			out.decreaseIndentationLevel();
-		}
-
-		@Override
-		public void visit(final LogicalOpBind op) {
-			out.decreaseIndentationLevel();
-		}
-
-		@Override
-		public void visit(final LogicalOpLocalToGlobal op) {
-			out.decreaseIndentationLevel();
-		}
-
-		@Override
-		public void visit(final LogicalOpGlobalToLocal op) {
-			out.decreaseIndentationLevel();
-		}
 	}
 
 }
