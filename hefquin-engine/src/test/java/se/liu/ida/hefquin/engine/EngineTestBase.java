b81721f5a01257e7c8d294c506d8d41c251690ba
Olaf Hartig <olaf.hartig@liu.se>
2024-08-29 13:48:59 +0200

    separates the code between EngineTestBase and EngineTestBaseCopy

--- hefquin-engine/src/test/java/se/liu/ida/hefquin/engine/EngineTestBase.java
+++ hefquin-engine/src/test/java/se/liu/ida/hefquin/engine/EngineTestBase.java
@@ -59,34 +59,6 @@ public abstract class EngineTestBase
 	public static boolean skipLocalGraphQLTests = true;
 
 
-	protected void assertHasNext( final Iterator<SolutionMapping> it,
-	                              final String expectedURIforV1, final Var v1,
-	                              final String expectedURIforV2, final Var v2 )
-	{
-		assertTrue( it.hasNext() );
-
-        final Binding b = it.next().asJenaBinding();
-        assertEquals( 2, b.size() );
-
-        assertEquals( expectedURIforV1, b.get(v1).getURI() );
-        assertEquals( expectedURIforV2, b.get(v2).getURI() );
-	}
-
-	protected void assertHasNext( final Iterator<SolutionMapping> it,
-	                              final String expectedURIforV1, final Var v1,
-	                              final String expectedURIforV2, final Var v2,
-	                              final String expectedURIforV3, final Var v3 )
-	{
-		assertTrue( it.hasNext() );
-
-        final Binding b = it.next().asJenaBinding();
-        assertEquals( 3, b.size() );
-
-        assertEquals( expectedURIforV1, b.get(v1).getURI() );
-        assertEquals( expectedURIforV2, b.get(v2).getURI() );
-        assertEquals( expectedURIforV3, b.get(v3).getURI() );
-	}
-
 	protected TPFServer getDBpediaTPFServer() {
 		final String       tpfServerBaseURL = "http://fragments.dbpedia.org/2016-04/en";
 		final TPFInterface tpfServerIface   = new TPFInterfaceImpl(tpfServerBaseURL, "subject", "predicate", "object");
