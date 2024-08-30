d59244b3098ae56d53a56e4ed66b2e75a4a6ce27
Olaf Hartig <olaf.hartig@liu.se>
2024-08-30 13:02:08 +0200

    adds some documentation to the build.sh script for the vocabularies

--- hefquin-vocabs/build.sh
+++ hefquin-vocabs/build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+### This scripts creates the relevant Java classes for the vocabularies. To use
+### this script you need to set the JENA_HOME environment variable (pointing to
+### the root directory of an unzipped Jena binary package) and then uncomment
+### the relevant line at the very end of this script. 
+
 # Check that JENA_HOME is not empty
 if [ -z "$JENA_HOME" ]; then
   echo "JENA_HOME is not set"
@@ -38,8 +43,27 @@ function proc
     mv "$CLASS".java ${DIR}/
 }
 
-# syntaxCheck  LPGtoRDFConfiguration.ttl
-# proc LPGtoRDFConfiguration.ttl  LPGtoRDF  "http://www.example.org/se/liu/ida/hefquin/lpg2rdf#" "se.liu.ida.hefquin.engine.wrappers.lpgwrapper.vocabulary" ../hefquin-pgconnector/src/main/java/se/liu/ida/hefquin/engine/wrappers/lpgwrapper/vocabulary/
+function procLPGtoRDF
+{
+    syntaxCheck  LPGtoRDFConfiguration.ttl
+    proc LPGtoRDFConfiguration.ttl \
+         LPGtoRDF \
+         "http://www.example.org/se/liu/ida/hefquin/lpg2rdf#" \
+         "se.liu.ida.hefquin.engine.wrappers.lpgwrapper.vocabulary" \
+         ../hefquin-pgconnector/src/main/java/se/liu/ida/hefquin/engine/wrappers/lpgwrapper/vocabulary/
+}
 
-syntaxCheck  EngineConfiguration.ttl
-proc EngineConfiguration.ttl  ECVocab  "http://www.example.org/se/liu/ida/hefquin/engineconf#" "se.liu.ida.hefquin.engine.vocabulary" ../hefquin-engine/src/main/java/se/liu/ida/hefquin/engine/vocabulary/
+function procECVocab
+{
+    syntaxCheck  EngineConfiguration.ttl
+    proc EngineConfiguration.ttl \
+         ECVocab \
+         "http://www.example.org/se/liu/ida/hefquin/engineconf#" \
+         "se.liu.ida.hefquin.engine.vocabulary" \
+         ../hefquin-engine/src/main/java/se/liu/ida/hefquin/engine/vocabulary/
+}
+
+### Below, uncomment the line for which you want to run the script.
+
+#procLPGtoRDF
+procECVocab
