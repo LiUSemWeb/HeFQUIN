46bbdf0d794c6fc98a167df54e49e3601d59cc67
Robin Keskisärkkä <robin.keski@gmail.com>
2024-09-06 10:31:19 +0200

    configure service using command line arguments

--- hefquin-service/src/main/java/se/liu/ida/hefquin/service/HeFQUINServer.java
+++ hefquin-service/src/main/java/se/liu/ida/hefquin/service/HeFQUINServer.java
@@ -2,11 +2,30 @@ package se.liu.ida.hefquin.service;
 
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.webapp.WebAppContext;
+import org.apache.commons.cli.*;
 
 public class HeFQUINServer {
 
 	public static void main( String[] args ) throws Exception {
-		final Server server = run( 8080 );
+		final Options options = new Options();
+		final Option arg1 = new Option( "f", "federationDescription", true, "" );
+		options.addOption( arg1 );
+		final Option arg2 = new Option( "c", "confDescr", true, "" );
+		options.addOption( arg2 );
+		final Option arg3 = new Option( "p", "port", true, "" );
+		options.addOption( arg3 );
+
+		final CommandLineParser parser = new DefaultParser();
+		final CommandLine cmd;
+		cmd = parser.parse( options, args );
+
+		final String ENGINE_CONF_FILE = cmd.getOptionValue( "confDescr", "DefaultEngineConf.ttl" );
+		final String FED_CONF_FILE = cmd.getOptionValue( "federationDescription", "DefaultEngineConf.ttl" );
+		final int port = Integer.parseInt( cmd.getOptionValue( "port", "8080" ) );
+		System.setProperty( "hefquin.configuration", ENGINE_CONF_FILE );
+		System.setProperty( "hefquin.federation", FED_CONF_FILE );
+
+		final Server server = run( port );
 		server.start();
 		server.join();
 	}
@@ -14,8 +33,8 @@ public class HeFQUINServer {
 	public static Server run( final int port ) {
 		final Server server = new Server( port );
 		final WebAppContext webAppContext = new WebAppContext();
-		webAppContext.setResourceBase("hefquin-service/src/main/webapp");
-		server.setHandler(webAppContext);
+		webAppContext.setResourceBase( "hefquin-service/src/main/webapp" );
+		server.setHandler( webAppContext );
 
 		return server;
 	}
