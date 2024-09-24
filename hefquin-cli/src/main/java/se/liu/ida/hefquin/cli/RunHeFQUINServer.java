f180c92ba8a864cc5907afd62753aca2bcfdda97
Robin Keskisärkkä <robin.keski@gmail.com>
2024-09-24 08:58:11 +0200

    add CLI to start hefquin-service in an embedded jetty server

---
+++ hefquin-cli/src/main/java/se/liu/ida/hefquin/cli/RunHeFQUINServer.java
@@ -0,0 +1,87 @@
+package se.liu.ida.hefquin.cli;
+
+import org.apache.jena.cmd.CmdGeneral;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.WebAppContext;
+
+import se.liu.ida.hefquin.cli.modules.ModServer;
+
+public class RunHeFQUINServer extends CmdGeneral
+{
+	protected final ModServer modServer = new ModServer();
+
+	/**
+	 * Main entry point of the tool, accepting command-line arguments to specify the
+	 * system configuration.
+	 *
+	 * @param argv Command-line arguments.
+	 */
+	public static void main( final String[] args ) {
+		new RunHeFQUINServer( args ).mainRun();
+	}
+
+	/**
+	 * Constructor that initializes the command-line tool with necessary argument
+	 * modules for specifying, e.g., federation configuration and engine
+	 * configuration.
+	 * 
+	 * @param argv Command-line arguments.
+	 */
+	public RunHeFQUINServer( final String[] argv ) {
+		super( argv );
+		addModule( modServer );
+	}
+
+	/**
+	 * Returns the usage summary string of the command.
+	 *
+	 * @return A string that describes the usage of the command.
+	 */
+	@Override
+	protected String getSummary() {
+		return getCommandName()
+				+ " [--port=<port>] [--federationDescription=<federation-description>] [--confDescr=<engine-configuration>]";
+	}
+
+	/**
+	 * Returns the command name used to invoke the tool.
+	 *
+	 * @return The name of the command.
+	 */
+	@Override
+	protected String getCommandName() {
+		return "hefquin-server";
+	}
+
+	/**
+	 * Starts up the HeFQUIN servlet using the embedded Jetty server.
+	 */
+	@Override
+	protected void exec() {
+		System.setProperty( "hefquin.configuration", modServer.getConfDescr() );
+		System.setProperty( "hefquin.federation", modServer.getFederationDescription() );
+
+		final Server server = run( modServer.getPort() );
+		try {
+			server.start();
+		} catch ( Exception e ) {
+			e.printStackTrace();
+		}
+		try {
+			server.join();
+		} catch ( InterruptedException e ) {
+			e.printStackTrace();
+		}
+	}
+
+	public static Server run( final int port ) {
+		final Server server = new Server( port );
+		System.out.println("Running on: http://localhost:" + port);
+		final WebAppContext webAppContext = new WebAppContext();
+		webAppContext.setResourceBase( "hefquin-service/src/main/webapp" );
+		server.setHandler( webAppContext );
+
+		return server;
+	}
+
+}
---
+++ hefquin-cli/src/main/java/se/liu/ida/hefquin/cli/modules/ModServer.java
@@ -0,0 +1,63 @@
+package se.liu.ida.hefquin.cli.modules;
+
+import org.apache.jena.cmd.ArgDecl;
+import org.apache.jena.cmd.CmdArgModule;
+import org.apache.jena.cmd.CmdGeneral;
+import org.apache.jena.cmd.ModBase;
+
+/**
+ * Command-line argument module for specifying endpoint and authentication.
+ */
+public class ModServer extends ModBase
+{
+	protected final ArgDecl argPort = new ArgDecl( ArgDecl.HasValue, "port" );
+	protected final ArgDecl argConfDescr = new ArgDecl( ArgDecl.HasValue, "configurationDescription", "confDescr" );
+	protected final ArgDecl argFedDescr = new ArgDecl( ArgDecl.HasValue, "federationDescription", "fd" );
+
+	protected int port;
+	protected String fedDescr;
+	protected String confDescr;
+
+	@Override
+	public void registerWith( final CmdGeneral cmdLine ) {
+		cmdLine.getUsage().startCategory( "Settings" );
+
+		cmdLine.add( argPort, "--port", "Server port (default: 8080)" );
+		cmdLine.add( argConfDescr, "--confDescr",
+				"File with an RDF description of the configuration (default: DefaultEngineConf.ttl)" );
+		cmdLine.add( argFedDescr, "--federationDescription",
+				"File with an RDF description of the federation (default: DefaultFederation.ttl)" );
+	}
+
+	@Override
+	public void processArgs( final CmdArgModule cmdLine ) {
+		if ( cmdLine.contains( argPort ) ) {
+			port = Integer.parseInt(cmdLine.getValue( argPort ));
+		} else {
+			port = 8080;
+		}
+		if ( cmdLine.contains( argConfDescr ) ) {
+			confDescr = cmdLine.getValue( argConfDescr );
+		} else {
+			confDescr = "DefaultEngineConf.ttl";
+		}
+		if ( cmdLine.contains( argFedDescr ) ) {
+			fedDescr = cmdLine.getValue( argFedDescr );
+		} else {
+			fedDescr = "DefaultFedConf.ttl";
+		}
+	}
+
+	public int getPort() {
+		return port;
+	}
+
+	public String getConfDescr() {
+		return confDescr;
+
+	}
+
+	public String getFederationDescription() {
+		return fedDescr;
+	}
+}
