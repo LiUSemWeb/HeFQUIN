96f4f2f6e9b0c18550e5c319129406ce543cce99
Robin Keskisärkkä <robin.keski@gmail.com>
2024-09-27 15:26:47 +0200

    use mustache templates

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,4 +1,49 @@
+// Run when DOM is fully loaded
+window.onload = () => {
+    relPath = getRelPath();
+    renderTemplates(relPath);
+    trimCode();
+};
+
+// Function to get relative path (used to find assets)
+function getRelPath(){
+    const basePath = window.location.pathname;
+    const depth = window.location.pathname.split('/').length - 2;
+    return './' + '../'.repeat(depth);
+}
+
+// Function to toggle menu (mobile view)
 function toggleMenu() {
     const navLinks = document.querySelector('.nav-links');
     navLinks.classList.toggle('active');
 }
+
+// Function to load templates using fetch
+async function loadTemplate(templateUrl) {
+    const response = await fetch(templateUrl);
+    return response.text();
+}
+
+// Function to render the templates with Mustache
+async function renderTemplates() {
+    // Load and render header + nav
+    const headerTemplate = await loadTemplate(`${relPath}templates/header.mustache`);
+    const headerData = { relPath };
+    const renderedHeader = Mustache.render(headerTemplate, headerData);
+    document.getElementById('header').innerHTML = renderedHeader;
+
+    // Load and render footer
+    const footerTemplate = await loadTemplate(`${relPath}/templates/footer.mustache`);
+    const renderedFooter = Mustache.render(footerTemplate, {});
+    document.getElementById('footer').innerHTML = renderedFooter;
+}
+
+// Trim whitespaces around code blocks.
+function trimCode(){
+    // Trim around text in <code> blocks
+    Array.from(document.getElementsByTagName('code')).forEach(el => {
+        el.textContent = el.textContent.trim();
+    });
+}
+
+
