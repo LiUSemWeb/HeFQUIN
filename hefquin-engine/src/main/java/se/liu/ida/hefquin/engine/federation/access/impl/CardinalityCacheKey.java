2009046f3b290560de76afd6ea559a78e35fcc11
Robin Keskisärkkä <robin.keski@gmail.com>
2025-02-02 22:41:49 +0100

    extend cache key to support BRTPF with bindings

--- hefquin-engine/src/main/java/se/liu/ida/hefquin/engine/federation/access/impl/CardinalityCacheKey.java
+++ hefquin-engine/src/main/java/se/liu/ida/hefquin/engine/federation/access/impl/CardinalityCacheKey.java
@@ -2,27 +2,46 @@ package se.liu.ida.hefquin.engine.federation.access.impl;
 
 import java.io.Serializable;
 import java.util.Objects;
-import se.liu.ida.hefquin.base.query.SPARQLQuery;
+import se.liu.ida.hefquin.engine.federation.BRTPFServer;
+import se.liu.ida.hefquin.engine.federation.FederationMember;
+import se.liu.ida.hefquin.engine.federation.SPARQLEndpoint;
+import se.liu.ida.hefquin.engine.federation.TPFServer;
+import se.liu.ida.hefquin.engine.federation.access.BRTPFRequest;
+import se.liu.ida.hefquin.engine.federation.access.DataRetrievalRequest;
+import se.liu.ida.hefquin.engine.federation.access.SPARQLRequest;
+import se.liu.ida.hefquin.engine.federation.access.TPFRequest;
 
 /**
- * A key for caching cardinality requests, uniquely identified by a SPARQL
- * query and a target URL.
+ * A key for caching cardinality requests, uniquely identified by a {@link DataRetrievalRequest} and a {@link FederationMember}.
  */
 public class CardinalityCacheKey implements Serializable {
 	private static final long serialVersionUID = 1L;
 
-	protected final String query;
-	protected final String url;
+	protected String query;
+	protected String url;
+	protected String bindings;
 
-	/**
-	 * Constructs a new CardinalityCacheKey using a SPARQL query and a URL.
-	 *
-	 * @param query the SPARQL query
-	 * @param url   the endpoint URL
-	 */
-	public CardinalityCacheKey( final SPARQLQuery query, final String url ) {
-		this.query = query.toString();
-		this.url = url;
+	public CardinalityCacheKey( final DataRetrievalRequest req, final FederationMember fm ) {
+		// Default field values to ensure they are always initialized
+		this.query = req.toString();
+		this.url = fm.toString();
+		this.bindings = "";
+
+		if ( req instanceof SPARQLRequest sparqlRequest && fm instanceof SPARQLEndpoint sparqlEndpoint ) {
+			query = sparqlRequest.toString();
+			url = sparqlEndpoint.getInterface().getURL();
+		} else if ( req instanceof TPFRequest tpfRequest ) {
+			query = tpfRequest.toString();
+			if ( fm instanceof TPFServer tpfServer ) {
+				url = tpfServer.getInterface().createRequestURL( tpfRequest );
+			} else if ( fm instanceof BRTPFServer brtpfServer ) {
+				url = brtpfServer.getInterface().createRequestURL( tpfRequest );
+			}
+		} else if ( req instanceof BRTPFRequest brtpfRequest && fm instanceof BRTPFServer brtpfServer ) {
+			query = brtpfRequest.getTriplePattern().toString();
+			url = brtpfServer.getInterface().createRequestURL( brtpfRequest );
+			bindings = brtpfRequest.getSolutionMappings().toString();
+		}
 	}
 
 	@Override
@@ -42,6 +61,6 @@ public class CardinalityCacheKey implements Serializable {
 
 	@Override
 	public String toString() {
-		return "CardinalityCacheKey{query='" + query + "', url='" + url + "'}";
+		return "CardinalityCacheKey{query='" + query + "', url='" + url + "', bindings='" + bindings + "'}";
 	}
 }
