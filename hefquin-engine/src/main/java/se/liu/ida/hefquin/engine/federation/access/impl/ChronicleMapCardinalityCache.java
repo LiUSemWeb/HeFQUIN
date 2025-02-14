5e967c7aa895b2ac16a91d7eb01a6818b7c5ed82
Robin Keskisärkkä <robin.keski@gmail.com>
2025-02-14 11:11:57 +0100

    add PersistableCache based on ChronicleMap

---
+++ hefquin-engine/src/main/java/se/liu/ida/hefquin/engine/federation/access/impl/ChronicleMapCardinalityCache.java
@@ -0,0 +1,151 @@
+package se.liu.ida.hefquin.engine.federation.access.impl;
+
+import java.io.File;
+import java.io.IOException;
+import net.openhft.chronicle.map.ChronicleMap;
+import se.liu.ida.hefquin.base.datastructures.PersistableCache;
+
+/**
+ * A thread-safe persistent cache implementation for storing cardinality entries. This
+ * cache uses a {@link ChroncicleMap}.
+ *
+ * @param <K> The key type for caching cardinality responses.
+ */
+public class ChronicleMapCardinalityCache implements PersistableCache<CardinalityCacheKey, CardinalityCacheEntry> {
+	final ChronicleMap<CardinalityCacheKey, CardinalityCacheEntry> map;
+	protected final String filename;
+	protected final static String defaultFilename = "cache/chronicle-map.dat";
+
+	/**
+	 * Constructs a new {@link PersistableCardinalityCacheImpl} with the default
+	 * cache file.
+	 * 
+	 * @throws IOException
+	 */
+	public ChronicleMapCardinalityCache() throws IOException {
+		this( defaultFilename );
+	}
+	
+	/**
+	 * Constructs a new {@link ChronicleMapCardinalityCache} with a custom file
+	 * path.
+	 *
+	 * @param filename Path to the cache file.
+	 * @throws IOException
+	 */
+	public ChronicleMapCardinalityCache( final String filename ) throws IOException {
+		this.filename = filename;
+		ensureFileExists();
+
+		// ChronicleMap for persistent storage
+		map = ChronicleMap.of( CardinalityCacheKey.class, CardinalityCacheEntry.class )
+				.name( "cardinality-map" ).entries( 10_000 )
+				.averageKeySize( 512 )
+				.averageValueSize( 64 )
+				.createPersistedTo( new File( this.filename ) );
+	}
+	
+	/**
+	 * Ensures that the cache file exists before initialization. If the file
+	 * does not exist, it is created along with necessary directories.
+	 */
+	private void ensureFileExists() {
+		final File file = new File( filename );
+		try {
+			if ( ! file.exists() ) {
+				file.getParentFile().mkdirs();
+				file.createNewFile();
+			}
+		} catch ( IOException e ) {
+			throw new RuntimeException( "Failed to create file: " + file.getAbsolutePath(), e );
+		}
+	}
+
+	/**
+	 * Adds a new cache entry to the cache, associated with the given key.
+	 * If an entry already exists for this key, it is replaced.
+	 *
+	 * @param key   The key identifying the response.
+	 * @param value The entry to store.
+	 */
+	@Override
+	public void put( CardinalityCacheKey key, CardinalityCacheEntry entry ) {
+		map.put( key, entry );
+	}
+
+	/**
+	 * Retrieves the cache entry associated with the given key.
+	 *
+	 * @param key The key to look up.
+	 * @return The entry, or {@link null} if not found.
+	 */
+	@Override
+	public CardinalityCacheEntry get( CardinalityCacheKey key ) {
+		return map.get( key );
+	}
+
+
+	/**
+	 * Removes the cahce entry associated with the given key from the cache.
+	 *
+	 * @param key The key to remove.
+	 * @return {@link true} if an entry was removed, {@link false} otherwise.
+	 */
+	@Override
+	public boolean evict( CardinalityCacheKey key ) {
+		return map.remove( key ) != null;
+	}
+
+	/**
+	 * Removes the specified entry from the cache only if the current value matches.
+	 *
+	 * @param key   The key to remove.
+	 * @param value The expected value to match before removal.
+	 * @return {@link true} if the entry was removed, {@link false} otherwise.
+	 */
+	@Override
+	public boolean evict( CardinalityCacheKey key, CardinalityCacheEntry entry ) {
+		return map.remove( key, entry );
+	}
+
+	/**
+	 * Checks whether the cache is currently empty.
+	 *
+	 * @return {@link true} if the cache is empty, {@link false} otherwise.
+	 */
+	@Override
+	public boolean isEmpty() {
+		return map.isEmpty();
+	}
+
+	/**
+	 * Clears all entries from the cache.
+	 */
+	@Override
+	public void clear() {
+		map.clear();
+	}
+
+	/**
+	 * No-op implementation.
+	 * 
+	 * This method is required by the interface but is not used because ChronicleMap
+	 * persists changes automatically. There is no need for an explicit save operation.
+	 */
+	@Override
+	public void save() {
+		// Do nothing
+	}
+
+	/**
+	 * No-op implementation.
+	 * 
+	 * This method is required by the interface but is not used because ChronicleMap
+	 * automatically loads data from the mapped file upon creation.
+	 */
+	@Override
+	public void load() {
+		// Do nothing
+	}
+
+}
