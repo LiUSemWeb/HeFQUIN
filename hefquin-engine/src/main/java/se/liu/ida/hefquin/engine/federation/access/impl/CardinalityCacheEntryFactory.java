cf6c9c849c3b21d5a5036993a4bf689be30385e3
Robin Keskisärkkä <robin.keski@gmail.com>
2025-02-17 16:16:06 +0100

    add created at, remove expiration time

--- hefquin-engine/src/main/java/se/liu/ida/hefquin/engine/federation/access/impl/CardinalityCacheEntryFactory.java
+++ hefquin-engine/src/main/java/se/liu/ida/hefquin/engine/federation/access/impl/CardinalityCacheEntryFactory.java
@@ -8,20 +8,9 @@ import se.liu.ida.hefquin.base.datastructures.impl.cache.CacheEntryFactory;
  * An entry used when caching cardinality requests.
  */
 public class CardinalityCacheEntryFactory implements CacheEntryFactory<CardinalityCacheEntry, Integer> {
-	private final long defaultTimeToLive = 5 * 60 * 1000; // ms
-	private final long timeToLive;
-
-	public CardinalityCacheEntryFactory(){
-		timeToLive = defaultTimeToLive;
-	}
-
-	public CardinalityCacheEntryFactory( final long timeToLive ){
-		this.timeToLive = timeToLive;
-	}
 
 	@Override
 	public CardinalityCacheEntry createCacheEntry( final Integer cardinality ) {
-		final long expirationTime = Instant.now().toEpochMilli() + timeToLive;
-		return new CardinalityCacheEntry( cardinality, expirationTime );
+		return new CardinalityCacheEntry( cardinality, Instant.now().toEpochMilli() );
 	}
 }
