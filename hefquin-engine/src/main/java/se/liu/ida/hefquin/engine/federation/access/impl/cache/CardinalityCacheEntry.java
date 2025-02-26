a634ff607e1f5712d5d3ef19c1a94077ba3a2736
Robin Keskisärkkä <robin.keski@gmail.com>
2025-02-26 11:25:59 +0100

    remove methods moved to CacheEntryBase

--- hefquin-engine/src/main/java/se/liu/ida/hefquin/engine/federation/access/impl/cache/CardinalityCacheEntry.java
+++ hefquin-engine/src/main/java/se/liu/ida/hefquin/engine/federation/access/impl/cache/CardinalityCacheEntry.java
@@ -7,33 +7,13 @@ import se.liu.ida.hefquin.base.datastructures.impl.cache.CacheEntryBase;
  */
 public class CardinalityCacheEntry extends CacheEntryBase<Integer> {
 	private static final long serialVersionUID = 1L;
-	private final long entryCreatedAt;
 	
 	public CardinalityCacheEntry( final Integer cardinality, final long entryCreatedAt ){
-		super( cardinality );
-        this.entryCreatedAt = entryCreatedAt;
-	}
-
-	@Override
-	public boolean equals( Object obj ) {
-		if ( this == obj )
-			return true;
-		if ( obj == null || getClass() != obj.getClass() )
-			return false;
-		return this.getObject() == ((CardinalityCacheEntry) obj).getObject();
-	}
-
-	@Override
-	public int hashCode() {
-		return getObject();
+		super( cardinality, entryCreatedAt );
 	}
 
 	@Override
 	public String toString() {
 		return "CardinalityCacheEntry{cardinality='" + getObject() + "'}";
-	}
-
-	public long getCacheEntryCreatedAt(){
-		return entryCreatedAt;
 	}
 }
