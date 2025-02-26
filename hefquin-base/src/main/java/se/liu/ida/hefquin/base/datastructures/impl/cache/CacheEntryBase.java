8b016cba6c7ead0cc2dc3fe4030e83702fa56a69
Robin Keskisärkkä <robin.keski@gmail.com>
2025-02-26 10:29:10 +0100

    updated CacheEntryBase to align with changes in CacheEntry

--- hefquin-base/src/main/java/se/liu/ida/hefquin/base/datastructures/impl/cache/CacheEntryBase.java
+++ hefquin-base/src/main/java/se/liu/ida/hefquin/base/datastructures/impl/cache/CacheEntryBase.java
@@ -1,20 +1,32 @@
 package se.liu.ida.hefquin.base.datastructures.impl.cache;
 
 import java.io.Serializable;
+import java.time.Instant;
 
 public class CacheEntryBase<ObjectType> implements CacheEntry<ObjectType>, Serializable
 {
 	private static final long serialVersionUID = 1L;
 	protected final ObjectType obj;
+	protected final long creationTime;
 
 	public CacheEntryBase( final ObjectType obj ) {
+		this( obj, Instant.now().toEpochMilli() );
+	}
+
+	public CacheEntryBase( final ObjectType obj, final long creationTime ) {
 		assert obj != null;
 		this.obj = obj;
+		this.creationTime = creationTime;
 	}
 
 	@Override
 	public ObjectType getObject() {
 		return obj;
+	}
+
+	@Override
+	public long createdAt() {
+		return creationTime;
 	}
 
 }
