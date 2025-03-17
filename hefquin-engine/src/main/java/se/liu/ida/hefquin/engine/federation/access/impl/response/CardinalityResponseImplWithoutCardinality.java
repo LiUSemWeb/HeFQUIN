59d6840c10d2621a6cd60d56793e63fd75fb7293
Robin Keskisärkkä <robin.keski@gmail.com>
2025-03-17 17:02:57 +0100

    extend CardinalityResponseImpl, make use of fallback value explicit

--- hefquin-engine/src/main/java/se/liu/ida/hefquin/engine/federation/access/impl/response/CardinalityResponseImplWithoutCardinality.java
+++ hefquin-engine/src/main/java/se/liu/ida/hefquin/engine/federation/access/impl/response/CardinalityResponseImplWithoutCardinality.java
@@ -1,53 +1,17 @@
 package se.liu.ida.hefquin.engine.federation.access.impl.response;
 
-import java.util.Date;
-
-import se.liu.ida.hefquin.engine.federation.FederationMember;
-import se.liu.ida.hefquin.engine.federation.access.CardinalityResponse;
 import se.liu.ida.hefquin.engine.federation.access.DataRetrievalRequest;
 import se.liu.ida.hefquin.engine.federation.access.DataRetrievalResponse;
 
-public class CardinalityResponseImplWithoutCardinality implements CardinalityResponse
+public class CardinalityResponseImplWithoutCardinality extends CardinalityResponseImpl
 {
-	protected final DataRetrievalResponse wrappedResponse;
-	protected final DataRetrievalRequest request;
-
 	public CardinalityResponseImplWithoutCardinality( final DataRetrievalResponse wrappedResponse,
 	                                                  final DataRetrievalRequest request ) {
-		assert wrappedResponse != null;
-		assert request != null;
-
-		this.wrappedResponse = wrappedResponse;
-		this.request = request;
-	}
-
-	public DataRetrievalResponse getWrappedResponse() {
-		return wrappedResponse;
+		super( wrappedResponse, request, Integer.MAX_VALUE );
 	}
 
 	@Override
-	public FederationMember getFederationMember() {
-		return wrappedResponse.getFederationMember();
+	public boolean isFallbackResponse() {
+		return true;
 	}
-
-	@Override
-	public DataRetrievalRequest getRequest() {
-		return request;
-	}
-
-	@Override
-	public Date getRequestStartTime() {
-		return wrappedResponse.getRequestStartTime();
-	}
-
-	@Override
-	public Date getRetrievalEndTime() {
-		return wrappedResponse.getRetrievalEndTime();
-	}
-
-	@Override
-	public int getCardinality() {
-		return Integer.MAX_VALUE;
-	}
-
 }
