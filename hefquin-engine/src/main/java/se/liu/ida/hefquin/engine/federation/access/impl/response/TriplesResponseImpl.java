8e926c3a365ebe893c167f29b6a95cf0c362b146
Robin Keskisärkkä <robin.keski@gmail.com>
2025-03-17 17:07:17 +0100

    add additional constructors to support error details

--- hefquin-engine/src/main/java/se/liu/ida/hefquin/engine/federation/access/impl/response/TriplesResponseImpl.java
+++ hefquin-engine/src/main/java/se/liu/ida/hefquin/engine/federation/access/impl/response/TriplesResponseImpl.java
@@ -8,31 +8,94 @@ import se.liu.ida.hefquin.engine.federation.FederationMember;
 import se.liu.ida.hefquin.engine.federation.access.DataRetrievalRequest;
 import se.liu.ida.hefquin.engine.federation.access.TriplesResponse;
 
-public class TriplesResponseImpl
-                            extends DataRetrievalResponseBase
-                            implements TriplesResponse
+public class TriplesResponseImpl extends DataRetrievalResponseBase implements TriplesResponse
 {
 	protected final List<Triple> triples;
 
 	/**
-	 * Initializes the retrievalEndTime to the time when this object is created.
+	 * Constructs a response with the given triples, federation member, request, and request start time. The retrieval
+	 * end time is automatically set to the current time at the moment of construction. This constructor assumes no
+	 * error occurred.
+	 *
+	 * @param triples          the list of triples contained in this response (must not be {@code null})
+	 * @param fm               the federation member from which this response originates (must not be {@code null})
+	 * @param request          the data retrieval request associated with this response (must not be {@code null})
+	 * @param requestStartTime the time at which the request was initiated (must not be {@code null})
 	 */
 	public TriplesResponseImpl( final List<Triple> triples,
 	                            final FederationMember fm,
 	                            final DataRetrievalRequest request,
 	                            final Date requestStartTime ) {
-		super(fm, request, requestStartTime);
+		super( fm, request, requestStartTime );
 
 		assert triples != null;
 		this.triples = triples;
 	}
 
+	/**
+	 * Constructs a response with the given triples. federation member, request, request start time, and the retrieval
+	 * end time. This constructor assumes no error occurred.
+	 *
+	 * @param triples          the list of triples contained in this response (must not be {@code null})
+	 * @param fm               the federation member from which this response originates (must not be {@code null})
+	 * @param request          the request associated with this response (must not be {@code null})
+	 * @param requestStartTime the time at which the request was initiated (must not be {@code null})
+	 * @param retrievalEndTime the time at which the retrieval of this response was completed (must not be {@code null})
+	 */
 	public TriplesResponseImpl( final List<Triple> triples,
 	                            final FederationMember fm,
 	                            final DataRetrievalRequest request,
 	                            final Date requestStartTime,
 	                            final Date retrievalEndTime ) {
-		super(fm, request, requestStartTime, retrievalEndTime);
+		super( fm, request, requestStartTime, retrievalEndTime );
+
+		assert triples != null;
+		this.triples = triples;
+	}
+
+	/**
+	 * Constructs a response with the given triples, federation member, request, request start time, and error details.
+	 * The retrieval end time is automatically set to the current time at the moment of construction.
+	 *
+	 * @param triples          the list of triples contained in this response (must not be {@code null})
+	 * @param fm               the federation member from which this response originates (must not be {@code null})
+	 * @param request          the request associated with this response (must not be {@code null})
+	 * @param requestStartTime the time at which the request was initiated (must not be {@code null})
+	 * @param errorStatusCode  the HTTP status code representing an error, or {@code null} if no error occurred
+	 * @param errorDescription a short description of the error, or {@code null} if no error occurred
+	 */
+	public TriplesResponseImpl( final List<Triple> triples,
+	                            final FederationMember fm,
+	                            final DataRetrievalRequest request,
+	                            final Date requestStartTime,
+								final Integer errorStatusCode,
+								final String errorDescription ) {
+		super( fm, request, requestStartTime, errorStatusCode, errorDescription );
+
+		assert triples != null;
+		this.triples = triples;
+	}
+
+	/**
+	 * Constructs a response with the given triples, federation member, request, request start time, retrieval end time,
+	 * and error details.
+	 *
+	 * @param triples          the list of triples contained in this response (must not be {@code null})
+	 * @param fm               the federation member from which this response originates (must not be {@code null})
+	 * @param request          the request associated with this response (must not be {@code null})
+	 * @param requestStartTime the time at which the request was initiated (must not be {@code null})
+	 * @param retrievalEndTime the time at which the retrieval of this response was completed (must not be {@code null})
+	 * @param errorStatusCode  the HTTP status code representing an error, or {@code null} if no error occurred
+	 * @param errorDescription a short description of the error, or {@code null} if no error occurred
+	 */
+	public TriplesResponseImpl( final List<Triple> triples,
+	                            final FederationMember fm,
+	                            final DataRetrievalRequest request,
+	                            final Date requestStartTime,
+	                            final Date retrievalEndTime,
+								final Integer errorStatusCode,
+								final String errorDescription ) {
+		super( fm, request, requestStartTime, retrievalEndTime, errorStatusCode, errorDescription );
 
 		assert triples != null;
 		this.triples = triples;
