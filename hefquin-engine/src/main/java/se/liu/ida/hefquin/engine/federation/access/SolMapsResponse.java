2ecdc0d8307f15ef21ada8d75ed89990838fc648
Robin Keskisärkkä <robin.keski@gmail.com>
2025-03-25 13:48:24 +0100

    add default implementation of getSize() that supports iterable

--- hefquin-engine/src/main/java/se/liu/ida/hefquin/engine/federation/access/SolMapsResponse.java
+++ hefquin-engine/src/main/java/se/liu/ida/hefquin/engine/federation/access/SolMapsResponse.java
@@ -1,5 +1,8 @@
 package se.liu.ida.hefquin.engine.federation.access;
 
+import java.util.Collection;
+import java.util.Iterator;
+
 import se.liu.ida.hefquin.base.data.SolutionMapping;
 
 public interface SolMapsResponse extends DataRetrievalResponse<Iterable<SolutionMapping>>
@@ -9,5 +12,16 @@ public interface SolMapsResponse extends DataRetrievalResponse<Iterable<Solution
 	/**
 	 * Returns the number of triples that are returned by {@link #getSolutionMappings()}. 
 	 */
-	int getSize();
+	default int getSize(){
+		final Iterable<SolutionMapping> mappings = getSolutionMappings();
+		if (mappings instanceof Collection) {
+			return ((Collection<SolutionMapping>) mappings).size();
+		}
+		// Fallback to manual count
+		int count = 0;
+		for ( Iterator<SolutionMapping> it = mappings.iterator(); it.hasNext(); it.next() ){
+			count++;
+		}
+		return count;
+	}
 }
