b174d2a1489c78b555276e488ad1991979d3303a
Robin Keskisärkkä <robin.keski@gmail.com>
2025-03-25 15:32:14 +0100

    update JSONResponseImpl to comply with generic DataRetrievalResponse<T>

--- hefquin-engine/src/main/java/se/liu/ida/hefquin/engine/federation/access/impl/response/JSONResponseImpl.java
+++ hefquin-engine/src/main/java/se/liu/ida/hefquin/engine/federation/access/impl/response/JSONResponseImpl.java
@@ -8,92 +8,87 @@ import se.liu.ida.hefquin.engine.federation.FederationMember;
 import se.liu.ida.hefquin.engine.federation.access.DataRetrievalRequest;
 import se.liu.ida.hefquin.engine.federation.access.JSONResponse;
 
-public class JSONResponseImpl extends DataRetrievalResponseBase implements JSONResponse
+public class JSONResponseImpl extends DataRetrievalResponseBase<JsonObject> implements JSONResponse
 {
-	protected final JsonObject jsonObj;
-
 	/**
 	 * Constructs a response with the given JSON object, federation member, request, and request start time. The
-	 * retireval end time is automatically set to the current time at the moment of construction. This constructor
+	 * retrieval end time is automatically set to the current time at the moment of construction. This constructor
 	 * assumes no error occurred.
 	 *
-	 * @param obj              the JSON object contained in this response (must not be {@code null})
+	 * @param jsonObject       the JSON object contained in this response (must not be {@code null})
 	 * @param fm               the federation member from which this response originates (must not be {@code null})
 	 * @param request          the data retrieval request associated with this response (must not be {@code null})
 	 * @param requestStartTime the time at which the request was initiated (must not be {@code null})
 	 */
-	public JSONResponseImpl( final JsonObject obj,
+	public JSONResponseImpl( final JsonObject jsonObject,
 	                         final FederationMember fm,
 	                         final DataRetrievalRequest request,
 	                         final Date requestStartTime ) {
-		super( fm, request, requestStartTime );
-		this.jsonObj = obj;
+		super( jsonObject, fm, request, requestStartTime );
 	}
 
 	/**
 	 * Constructs a response with the given JSON object, federation member, request, request start time, and retrieval
 	 * end time. This constructor assumes no error occurred.
 	 *
-	 * @param obj              the JSON object contained in this response (must not be {@code null})
+	 * @param jsonObject       the JSON object contained in this response (must not be {@code null})
 	 * @param fm               the federation member from which this response originates (must not be {@code null})
 	 * @param request          the data retrieval request associated with this response (must not be {@code null})
 	 * @param requestStartTime the time at which the request was initiated (must not be {@code null})
 	 * @param retrievalEndTime the time at which the retrieval of this response was completed (must not be {@code null})
 	 */
-	public JSONResponseImpl( final JsonObject obj,
+	public JSONResponseImpl( final JsonObject jsonObject,
 	                         final FederationMember fm,
 	                         final DataRetrievalRequest request,
 	                         final Date requestStartTime,
 	                         final Date retrievalEndTime ) {
-		super( fm, request, requestStartTime, retrievalEndTime );
-		this.jsonObj = obj;
+		super( jsonObject, fm, request, requestStartTime, retrievalEndTime );
 	}
 
 	/**
 	 * Constructs a response with the given JSON object, federation member, request, request start time, and error
 	 * details. The retrieval end time is automatically set to the current time at the moment of construction.
 	 *
-	 * @param obj              the JSON object contained in this response (must not be {@code null})
+	 * @param jsonObject       the JSON object contained in this response (must not be {@code null})
 	 * @param fm               the federation member from which this response originates (must not be {@code null})
-	 * @param request          the request associated with this response (must not be {@code null})
+	 * @param request          the data retrieval request associated with this response (must not be {@code null})
 	 * @param requestStartTime the time at which the request was initiated (must not be {@code null})
 	 * @param errorStatusCode  the HTTP status code representing an error, or {@code null} if no error occurred
 	 * @param errorDescription a short description of the error, or {@code null} if no error occurred
 	 */
-	public JSONResponseImpl( final JsonObject obj,
+	public JSONResponseImpl( final JsonObject jsonObject,
 	                         final FederationMember fm,
 	                         final DataRetrievalRequest request,
 	                         final Date requestStartTime,
 	                         final Integer errorStatusCode,
 	                         final String errorDescription ) {
-		super( fm, request, requestStartTime, errorStatusCode, errorDescription );
-		this.jsonObj = obj;
+		super( jsonObject, fm, request, requestStartTime, errorStatusCode, errorDescription );
 	}
 
 	/**
-	 * Constructs a response with the given JSON object, federation member, request, request start time, retrieval end
+	 * Constructs a response with the given JSON object, federation member, request, request start time, retireval end
 	 * time, and error details.
 	 *
-	 * @param obj              the JSON object contained in this response (must not be {@code null})
+	 * @param jsonObject       the JSON object contained in this response (must not be {@code null})
 	 * @param fm               the federation member from which this response originates (must not be {@code null})
-	 * @param request          the request associated with this response (must not be {@code null})
+	 * @param request          the data retrieval request associated with this response (must not be {@code null})
 	 * @param requestStartTime the time at which the request was initiated (must not be {@code null})
-	 * @param requestEndTime   the time at which the request was completed (must not be {@code null})
+	 * @param retrievalEndTime the time at which the retrieval of this response was completed (must not be {@code null})
 	 * @param errorStatusCode  the HTTP status code representing an error, or {@code null} if no error occurred
 	 * @param errorDescription a short description of the error, or {@code null} if no error occurred
 	 */
-	public JSONResponseImpl( final JsonObject obj,
+	public JSONResponseImpl( final JsonObject jsonObject,
 	                         final FederationMember fm,
 	                         final DataRetrievalRequest request,
 	                         final Date requestStartTime,
-	                         final Date requestEndTime,
+	                         final Date retrievalEndTime,
 	                         final Integer errorStatusCode,
 	                         final String errorDescription ) {
-		super( fm, request, requestStartTime, requestEndTime, errorStatusCode, errorDescription );
-		this.jsonObj = obj;
+		super( jsonObject, fm, request, requestStartTime, retrievalEndTime, errorStatusCode, errorDescription );
 	}
 
+	@Override
 	public JsonObject getJsonObject() {
-		return jsonObj;
+		return data;
 	}
 }
