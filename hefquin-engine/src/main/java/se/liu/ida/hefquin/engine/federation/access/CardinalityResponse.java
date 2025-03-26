48bb8c2dccd5d9c6de3123e424d01ff02c5749a5
Robin Keskisärkkä <robin.keski@gmail.com>
2025-03-26 11:49:25 +0100

    update to comply with generic DataRetrievalResponse<T>, add additional constructors, leverage default method implementations where possible

--- hefquin-engine/src/main/java/se/liu/ida/hefquin/engine/federation/access/CardinalityResponse.java
+++ hefquin-engine/src/main/java/se/liu/ida/hefquin/engine/federation/access/CardinalityResponse.java
@@ -1,6 +1,6 @@
 package se.liu.ida.hefquin.engine.federation.access;
 
-public interface CardinalityResponse extends DataRetrievalResponse
+public interface CardinalityResponse extends DataRetrievalResponse<Integer>
 {
 	int getCardinality();
 }
