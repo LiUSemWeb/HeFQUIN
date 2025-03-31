3fdb6dcc947072103983ec190f1d8914b6f3ce10
Robin Keskisärkkä <robin.keski@gmail.com>
2025-03-31 09:18:41 +0200

    make UnsupportedOperationDueToRetrievalError extend RuntimeException

--- hefquin-engine/src/main/java/se/liu/ida/hefquin/engine/federation/access/UnsupportedOperationDueToRetrievalError.java
+++ hefquin-engine/src/main/java/se/liu/ida/hefquin/engine/federation/access/UnsupportedOperationDueToRetrievalError.java
@@ -2,31 +2,40 @@ package se.liu.ida.hefquin.engine.federation.access;
 
 import se.liu.ida.hefquin.engine.federation.FederationMember;
 
-public class UnsupportedOperationDueToRetrievalError extends FederationAccessException
+public class UnsupportedOperationDueToRetrievalError extends RuntimeException
 {
 	private static final long serialVersionUID = 1L;
+	private final DataRetrievalRequest req;
+	private final FederationMember fm;
 
 	public UnsupportedOperationDueToRetrievalError( final String message,
 	                                                final Throwable cause,
 	                                                final DataRetrievalRequest req,
 	                                                final FederationMember fm ) {
-		super( message, cause, req, fm );
+		super( message, cause );
+		this.req = req;
+		this.fm = fm;
 	}
 
 	public UnsupportedOperationDueToRetrievalError( final String message,
 	                                                final DataRetrievalRequest req,
 	                                                final FederationMember fm ) {
-		super( message, req,  fm );
+		super( message );
+		this.req = req;
+		this.fm = fm;
 	}
 
 	public UnsupportedOperationDueToRetrievalError( final Throwable cause,
 	                                                final DataRetrievalRequest req,
 	                                                final FederationMember fm ) {
-		super( cause, req, fm );
+		super( cause );
+		this.req = req;
+		this.fm = fm;
 	}
 
 	public UnsupportedOperationDueToRetrievalError( final DataRetrievalRequest req,
 	                                                final FederationMember fm ) {
-		super( req, fm );
+		this.req = req;
+		this.fm = fm;
 	}
 }
