f330dd8c8eabf2d8b52b3b5c680753287ca78e37
Robin Keskisärkkä <robin.keski@gmail.com>
2025-04-02 09:37:13 +0200

    UnsupportedOperationDueToRetrievalError causes getSize() to return a value that should be treated as meaningless

--- hefquin-engine/src/main/java/se/liu/ida/hefquin/engine/federation/access/TriplesResponse.java
+++ hefquin-engine/src/main/java/se/liu/ida/hefquin/engine/federation/access/TriplesResponse.java
@@ -10,16 +10,21 @@ public interface TriplesResponse extends DataRetrievalResponse<Iterable<Triple>>
 	/**
 	 * Returns the number of triples that are returned by the {@link #getResponseData()}.
 	 */
-	default int getSize(){
-		final Iterable<Triple> triples = getResponseData();
-		if (triples instanceof Collection) {
-			return ((Collection<Triple>) triples).size();
+	default int getSize() {
+		try {
+			final Iterable<Triple> triples = getResponseData();
+			if ( triples instanceof Collection ) {
+				return ((Collection<Triple>) triples).size();
+			}
+			// Fallback to manual count
+			int count = 0;
+			for ( Iterator<Triple> it = triples.iterator(); it.hasNext(); it.next() ) {
+				count++;
+			}
+			return count;
+		} catch ( UnsupportedOperationDueToRetrievalError e ) {
+			// intentionally do nothing
 		}
-		// Fallback to manual count
-		int count = 0;
-		for ( Iterator<Triple> it = triples.iterator(); it.hasNext(); it.next() ){
-			count++;
-		}
-		return count;
+		return -1;
 	}
 }
