a5f046bf557d39bc969b13ea8f44ecd9aaa18d46
Robin Keskisärkkä <robin.keski@gmail.com>
2025-04-09 14:37:07 +0200

    add throws UnsupportedOperationDueToRetrievalError

--- hefquin-engine/src/main/java/se/liu/ida/hefquin/engine/federation/access/TriplesResponse.java
+++ hefquin-engine/src/main/java/se/liu/ida/hefquin/engine/federation/access/TriplesResponse.java
@@ -8,23 +8,18 @@ import se.liu.ida.hefquin.base.data.Triple;
 public interface TriplesResponse extends DataRetrievalResponse<Iterable<Triple>>
 {
 	/**
-	 * Returns the number of triples that are returned by {@link #getResponseData()}.
+	 * Returns the number of triples contained in this response.
 	 */
-	default int getSize() {
-		try {
-			final Iterable<Triple> triples = getResponseData();
-			if ( triples instanceof Collection c ) {
-				return c.size();
-			}
-			// Fallback to manual count
-			int count = 0;
-			for ( Iterator<Triple> it = triples.iterator(); it.hasNext(); it.next() ) {
-				count++;
-			}
-			return count;
-		} catch ( UnsupportedOperationDueToRetrievalError e ) {
-			// intentionally do nothing
+	default int getSize() throws UnsupportedOperationDueToRetrievalError {
+		final Iterable<Triple> triples = getResponseData();
+		if ( triples instanceof Collection c ) {
+			return c.size();
 		}
-		return -1;
+		// Fallback to manual count
+		int count = 0;
+		for ( Iterator<Triple> it = triples.iterator(); it.hasNext(); it.next() ) {
+			count++;
+		}
+		return count;
 	}
 }
