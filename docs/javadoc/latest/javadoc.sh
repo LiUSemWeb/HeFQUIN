faa8ec7a0457f455ac5c17266566606b5560f847
GitHub Action <action@github.com>
2025-04-17 09:30:06 +0000

    update docs

--- docs/javadoc/latest/javadoc.sh
+++ docs/javadoc/latest/javadoc.sh
@@ -1,1 +1,1 @@
-/opt/hostedtoolcache/Java_Adopt_jdk/17.0.15-6/x64/bin/javadoc -J-Duser.language= -J-Duser.country= @options @packages
+/opt/hostedtoolcache/Java_Temurin-Hotspot_jdk/17.0.4-101/x64/bin/javadoc -J-Duser.language= -J-Duser.country= @options @packages
