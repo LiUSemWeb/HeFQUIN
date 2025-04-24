469f1df2cb8c01c5aef5d11d6fbd41a2f6234f08
Robin Keskisärkkä <robin.keski@gmail.com>
2025-04-24 16:39:44 +0200

    default query formatting

--- hefquin-service/src/main/webapp/js/script.js
+++ hefquin-service/src/main/webapp/js/script.js
@@ -1,4 +1,4 @@
-const defaultQuery = 'PREFIX rdf:    <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\nPREFIX rdfs:   <http://www.w3.org/2000/01/rdf-schema#>\nPREFIX owl:    <http://www.w3.org/2002/07/owl#>\nPREFIX dbo:    <http://dbpedia.org/ontology/>\n\nSELECT * WHERE {\n	SERVICE <http://dbpedia.org/sparql> {\n		<http://dbpedia.org/resource/Berlin> dbo:country ?c .\n		?c owl:sameAs ?cc\n	}\n	SERVICE <https://query.wikidata.org/sparql> {\n		?cc rdfs:label ?o\n	}\n}';
+const defaultQuery = 'PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\nPREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\nPREFIX owl: <http://www.w3.org/2002/07/owl#>\nPREFIX dbo: <http://dbpedia.org/ontology/>\n\nSELECT *\nWHERE {\n  SERVICE <http://dbpedia.org/sparql> {\n    <http://dbpedia.org/resource/Berlin> dbo:country ?c .\n    ?c owl:sameAs ?cc\n  }\n  SERVICE <https://query.wikidata.org/sparql> {\n    ?cc rdfs:label ?o\n  }\n}';
 
 async function initQueryEditor(){
     const origin = window.location.origin;
