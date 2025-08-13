960838179948ab3050c011c002b13c9894e12008
Robin Keskisärkkä <robin.keski@gmail.com>
2025-08-13 14:15:45 +0200

    add physical op for bound join

---
+++ hefquin-engine/src/main/java/se/liu/ida/hefquin/engine/queryplan/physical/impl/PhysicalOpBindJoinWithBoundJoin.java
@@ -0,0 +1,71 @@
+package se.liu.ida.hefquin.engine.queryplan.physical.impl;
+
+import se.liu.ida.hefquin.base.query.ExpectedVariables;
+import se.liu.ida.hefquin.base.query.SPARQLGraphPattern;
+import se.liu.ida.hefquin.engine.queryplan.executable.UnaryExecutableOp;
+import se.liu.ida.hefquin.engine.queryplan.executable.impl.ops.ExecOpBindJoinSPARQLwithBoundJoin;
+import se.liu.ida.hefquin.engine.queryplan.info.QueryPlanningInfo;
+import se.liu.ida.hefquin.engine.queryplan.logical.impl.LogicalOpGPAdd;
+import se.liu.ida.hefquin.engine.queryplan.logical.impl.LogicalOpGPOptAdd;
+import se.liu.ida.hefquin.engine.queryplan.physical.PhysicalPlanVisitor;
+import se.liu.ida.hefquin.federation.SPARQLEndpoint;
+
+/**
+ * A physical operator that implements (a batching version of) the bound-join
+ * algorithm that uses UNION clauses with variable renaming (as proposed in
+ * the FedX paper by Schwarte et al. 2011). The variable that is renamed can
+ * be any non-join variable.
+ *
+ * <p>
+ * <b>Semantics:</b> TODO
+ * </p>
+ *
+ * <p>
+ * <b>Algorithm description:</b> For a detailed description of the
+ * actual algorithm associated with this physical operator, refer
+ * to {@link ExecOpBindJoinWithBoundJoin}, which provides the
+ * implementation of this algorithm.
+ * </p>
+ */
+public class PhysicalOpBindJoinWithBoundJoin extends BaseForPhysicalOpSingleInputJoinAtSPARQLEndpoint
+{
+	public PhysicalOpBindJoinWithBoundJoin( final LogicalOpGPAdd lop ) {
+		super(lop);
+	}
+
+	public PhysicalOpBindJoinWithBoundJoin( final LogicalOpGPOptAdd lop ) {
+		super(lop);
+	}
+
+	@Override
+	public UnaryExecutableOp createExecOp( final SPARQLGraphPattern pattern,
+	                                       final SPARQLEndpoint sparqlEndpoint,
+	                                       final boolean useOuterJoinSemantics,
+	                                       final boolean collectExceptions,
+	                                       final QueryPlanningInfo qpInfo,
+	                                       final ExpectedVariables... inputVars ) {
+		return new ExecOpBindJoinSPARQLwithBoundJoin( pattern,
+		                                              sparqlEndpoint,
+		                                              inputVars[0],
+		                                              useOuterJoinSemantics,
+		                                              ExecOpBindJoinSPARQLwithBoundJoin.DEFAULT_BATCH_SIZE,
+		                                              collectExceptions,
+		                                              qpInfo );
+	}
+
+	@Override
+	public void visit( final PhysicalPlanVisitor visitor ) {
+		visitor.visit(this);
+	}
+
+	@Override
+	public boolean equals( final Object o ) {
+		return o instanceof PhysicalOpBindJoinWithBoundJoin
+				&& ((PhysicalOpBindJoinWithBoundJoin) o).lop.equals(lop);
+	}
+
+	@Override
+	public String toString() {
+		return "> BoundJoinBindJoin" + lop.toString();
+	}
+}
