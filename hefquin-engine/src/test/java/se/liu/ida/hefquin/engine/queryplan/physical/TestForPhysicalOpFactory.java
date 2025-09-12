da731cb9301e28f11a70d3e0d1f59c5c5fb37c65
Robin Keskisärkkä <robin.keski@gmail.com>
2025-09-12 16:37:18 +0200

    refactor test

--- hefquin-engine/src/test/java/se/liu/ida/hefquin/engine/queryplan/physical/TestForPhysicalOpFactory.java
+++ hefquin-engine/src/test/java/se/liu/ida/hefquin/engine/queryplan/physical/TestForPhysicalOpFactory.java
@@ -12,24 +12,40 @@ import org.junit.Test;
 import se.liu.ida.hefquin.base.query.ExpectedVariables;
 import se.liu.ida.hefquin.base.query.SPARQLGraphPattern;
 import se.liu.ida.hefquin.base.query.impl.GenericSPARQLGraphPatternImpl1;
+import se.liu.ida.hefquin.engine.queryplan.logical.LogicalOperator;
 import se.liu.ida.hefquin.engine.queryplan.logical.impl.LogicalOpGPAdd;
+import se.liu.ida.hefquin.engine.queryplan.physical.TestsForPhysicalOpProviders.LogicalOpConstructor;
 import se.liu.ida.hefquin.engine.queryplan.physical.impl.PhysicalOpBindJoinWithBoundJoin;
 
-public class TestForPhysicalOpFactory {
-	protected static PhysicalOpFactory factory = new PhysicalOpFactory()
-		.register( new PhysicalOpBindJoinWithBoundJoin.Provider() );
+public class TestForPhysicalOpFactory
+{
+	@Test
+	public void testOpBindJoinWithBoundJoin_gpAdd(){
+		final PhysicalOpFactory factory = new PhysicalOpFactory()
+			.register( new PhysicalOpBindJoinWithBoundJoin.Provider() );
+		assertSupportFor(LogicalOpGPAdd::new, factory);
+	}
 
 	@Test
-	public void testOpBindJoinWithBoundJoin(){
+	public void testOpBindJoinWithBoundJoin_gpOptAdd(){
+		final PhysicalOpFactory factory = new PhysicalOpFactory()
+			.register( new PhysicalOpBindJoinWithBoundJoin.Provider() );
+		assertSupportFor(LogicalOpGPAdd::new, factory);
+	}
+
+
+	public void assertSupportFor( final LogicalOpConstructor logicalOpConstructor, final PhysicalOpFactory factory ){
 		final String queryString = "SELECT * WHERE { ?s ?p ?o }";
 		Element el = QueryFactory.create(queryString).getQueryPattern();
 		final SPARQLGraphPattern p = new GenericSPARQLGraphPatternImpl1(el);
-		final LogicalOpGPAdd lop = new LogicalOpGPAdd( new TestUtils.SPARQLEndpointForTest(), p );
+		final LogicalOperator lop = logicalOpConstructor.apply( new TestUtils.SPARQLEndpointForTest(), p );
 
 		final ExpectedVariables sp = TestUtils.getExpectedVariables( List.of("s", "p"), List.of() );
 		final ExpectedVariables spo = TestUtils.getExpectedVariables( List.of("s", "p", "o"), List.of() );
 
+		// wrong type returned
 		assertEquals( PhysicalOpBindJoinWithBoundJoin.class, factory.create(lop, sp).getClass());
-		assertThrows( UnsupportedOperationException.class, () -> factory.create(lop, spo));
+		// non-joinable var missing
+		assertThrows( UnsupportedOperationException.class, () -> factory.create(lop, spo) );
 	}
 }
