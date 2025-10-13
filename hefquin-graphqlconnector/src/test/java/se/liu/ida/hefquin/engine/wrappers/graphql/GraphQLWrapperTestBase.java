49d390b8f3f7fbd9861abfbe284c9ad508efc5fd
Olaf Hartig <olaf.hartig@liu.se>
2025-10-13 21:46:50 +0200

    resolves issue in 'hefquin-graphqlconnector' that caused the unit tests to fail after upgrade to Jena v5.5.0

--- hefquin-graphqlconnector/src/test/java/se/liu/ida/hefquin/engine/wrappers/graphql/GraphQLWrapperTestBase.java
+++ hefquin-graphqlconnector/src/test/java/se/liu/ida/hefquin/engine/wrappers/graphql/GraphQLWrapperTestBase.java
@@ -9,6 +9,7 @@ import org.apache.jena.datatypes.xsd.impl.XSDBaseNumericType;
 import org.apache.jena.datatypes.xsd.impl.XSDBaseStringType;
 import org.apache.jena.graph.Node;
 import org.apache.jena.graph.NodeFactory;
+import org.apache.jena.sys.JenaSystem;
 
 import se.liu.ida.hefquin.engine.wrappers.graphql.data.GraphQLEntrypoint;
 import se.liu.ida.hefquin.engine.wrappers.graphql.data.GraphQLField;
@@ -28,89 +29,92 @@ import se.liu.ida.hefquin.engine.wrappers.graphql.impl.SPARQL2GraphQLTranslatorI
  * modules.
  */
 public class GraphQLWrapperTestBase {
+
+    static { JenaSystem.init(); }
+
     // Author test fields
-    protected static final GraphQLField a1 = new GraphQLFieldImpl("id", "ID!", GraphQLFieldType.SCALAR);
-    protected static final GraphQLField a2 = new GraphQLFieldImpl("name", "String!", GraphQLFieldType.SCALAR);
-    protected static final GraphQLField a3 = new GraphQLFieldImpl("age", "Int!", GraphQLFieldType.SCALAR);
-    protected static final GraphQLField a4 = new GraphQLFieldImpl("books", "Book", GraphQLFieldType.OBJECT);
+    protected final GraphQLField a1 = new GraphQLFieldImpl("id", "ID!", GraphQLFieldType.SCALAR);
+    protected final GraphQLField a2 = new GraphQLFieldImpl("name", "String!", GraphQLFieldType.SCALAR);
+    protected final GraphQLField a3 = new GraphQLFieldImpl("age", "Int!", GraphQLFieldType.SCALAR);
+    protected final GraphQLField a4 = new GraphQLFieldImpl("books", "Book", GraphQLFieldType.OBJECT);
 
     // Book test fields
-    protected static final GraphQLField b1 = new GraphQLFieldImpl("id", "ID!", GraphQLFieldType.SCALAR);
-    protected static final GraphQLField b2 = new GraphQLFieldImpl("title", "String!", GraphQLFieldType.SCALAR);
-    protected static final GraphQLField b3 = new GraphQLFieldImpl("nr_pages", "Int!", GraphQLFieldType.SCALAR);
-    protected static final GraphQLField b4 = new GraphQLFieldImpl("genre", "Genre", GraphQLFieldType.SCALAR);
-    protected static final GraphQLField b5 = new GraphQLFieldImpl("authors", "Author", GraphQLFieldType.OBJECT);
+    protected final GraphQLField b1 = new GraphQLFieldImpl("id", "ID!", GraphQLFieldType.SCALAR);
+    protected final GraphQLField b2 = new GraphQLFieldImpl("title", "String!", GraphQLFieldType.SCALAR);
+    protected final GraphQLField b3 = new GraphQLFieldImpl("nr_pages", "Int!", GraphQLFieldType.SCALAR);
+    protected final GraphQLField b4 = new GraphQLFieldImpl("genre", "Genre", GraphQLFieldType.SCALAR);
+    protected final GraphQLField b5 = new GraphQLFieldImpl("authors", "Author", GraphQLFieldType.OBJECT);
 
     // Argument definitions for entrypoints
-    protected static final Map<String, String> argDefs1 = Map.ofEntries(entry("id", "ID!"));
+    protected final Map<String, String> argDefs1 = Map.ofEntries(entry("id", "ID!"));
 
-    protected static final Map<String, String> argDefs2 = Map.ofEntries(entry("name", "String"), entry("age", "Int"));
+    protected final Map<String, String> argDefs2 = Map.ofEntries(entry("name", "String"), entry("age", "Int"));
 
-    protected static final Map<String, String> argDefs3 = Map.ofEntries(entry("title", "String"),
+    protected final Map<String, String> argDefs3 = Map.ofEntries(entry("title", "String"),
             entry("nr_pages", "Int"), entry("genre", "Genre"));
 
     // Query entrypoints (query type fields)
-    protected static final GraphQLEntrypoint e1 = new GraphQLEntrypointImpl("author", argDefs1, "Author",
+    protected final GraphQLEntrypoint e1 = new GraphQLEntrypointImpl("author", argDefs1, "Author",
             GraphQLEntrypointType.SINGLE);
-    protected static final GraphQLEntrypoint e2 = new GraphQLEntrypointImpl("authors", argDefs2, "Author",
+    protected final GraphQLEntrypoint e2 = new GraphQLEntrypointImpl("authors", argDefs2, "Author",
             GraphQLEntrypointType.FILTERED);
-    protected static final GraphQLEntrypoint e3 = new GraphQLEntrypointImpl("allAuthors", new HashMap<>(), "Author",
+    protected final GraphQLEntrypoint e3 = new GraphQLEntrypointImpl("allAuthors", new HashMap<>(), "Author",
             GraphQLEntrypointType.FULL);
-    protected static final GraphQLEntrypoint e4 = new GraphQLEntrypointImpl("book", argDefs1, "Book",
+    protected final GraphQLEntrypoint e4 = new GraphQLEntrypointImpl("book", argDefs1, "Book",
             GraphQLEntrypointType.SINGLE);
-    protected static final GraphQLEntrypoint e5 = new GraphQLEntrypointImpl("books", argDefs3, "Book",
+    protected final GraphQLEntrypoint e5 = new GraphQLEntrypointImpl("books", argDefs3, "Book",
             GraphQLEntrypointType.FILTERED);
-    protected static final GraphQLEntrypoint e6 = new GraphQLEntrypointImpl("allBooks", new HashMap<>(), "Book",
+    protected final GraphQLEntrypoint e6 = new GraphQLEntrypointImpl("allBooks", new HashMap<>(), "Book",
             GraphQLEntrypointType.FULL);
 
     // Translator, config and schema
-    protected static final String classPrefix = "http://example.org/c/";
-    protected static final String propertyPrefix = "http://example.org/p/";
-    protected static final SPARQL2GraphQLTranslator translator = new SPARQL2GraphQLTranslatorImpl();
-    protected static final GraphQL2RDFConfiguration config = new DefaultGraphQL2RDFConfiguration(classPrefix,
+    protected final String classPrefix = "http://example.org/c/";
+    protected final String propertyPrefix = "http://example.org/p/";
+    protected final SPARQL2GraphQLTranslator translator = new SPARQL2GraphQLTranslatorImpl();
+    protected final GraphQL2RDFConfiguration config = new DefaultGraphQL2RDFConfiguration(classPrefix,
             propertyPrefix);
-    protected static final GraphQLSchema schema = initializeGraphQLTestSchema();
-    protected static final JSON2SolutionGraphConverter jsonTranslator = new JSON2SolutionGraphConverterImpl(config, schema);
-    protected static final GraphQLSolutionGraphSolver solutionGraphTranslator = new GraphQLSolutionGraphSolverImpl();
+    protected final GraphQLSchema schema = initializeGraphQLTestSchema();
+    protected final JSON2SolutionGraphConverter jsonTranslator = new JSON2SolutionGraphConverterImpl(config, schema);
+    protected final GraphQLSolutionGraphSolver solutionGraphTranslator = new GraphQLSolutionGraphSolverImpl();
 
     // Variables nodes
-    protected static final Node var1 = NodeFactory.createVariable("author");
-    protected static final Node var2 = NodeFactory.createVariable("book");
-    protected static final Node var3 = NodeFactory.createVariable("name");
-    protected static final Node var4 = NodeFactory.createVariable("title");
-    protected static final Node var5 = NodeFactory.createVariable("id");
-    protected static final Node var6 = NodeFactory.createVariable("s");
-    protected static final Node var7 = NodeFactory.createVariable("p");
-    protected static final Node var8 = NodeFactory.createVariable("o");
-    protected static final Node var9 = NodeFactory.createVariable("author2");
+    protected final Node var1 = NodeFactory.createVariable("author");
+    protected final Node var2 = NodeFactory.createVariable("book");
+    protected final Node var3 = NodeFactory.createVariable("name");
+    protected final Node var4 = NodeFactory.createVariable("title");
+    protected final Node var5 = NodeFactory.createVariable("id");
+    protected final Node var6 = NodeFactory.createVariable("s");
+    protected final Node var7 = NodeFactory.createVariable("p");
+    protected final Node var8 = NodeFactory.createVariable("o");
+    protected final Node var9 = NodeFactory.createVariable("author2");
 
     // URI nodes
-    protected static final Node uri1 = NodeFactory.createURI(config.mapFieldToProperty("Author", "id"));
-    protected static final Node uri2 = NodeFactory.createURI(config.mapFieldToProperty("Author", "name"));
-    protected static final Node uri3 = NodeFactory.createURI(config.mapFieldToProperty("Author", "age"));
-    protected static final Node uri4 = NodeFactory.createURI(config.mapFieldToProperty("Author", "books"));
-    protected static final Node uri5 = NodeFactory.createURI(config.mapFieldToProperty("Book", "id"));
-    protected static final Node uri6 = NodeFactory.createURI(config.mapFieldToProperty("Book", "title"));
-    protected static final Node uri7 = NodeFactory.createURI(config.mapFieldToProperty("Book", "nr_pages"));
-    protected static final Node uri8 = NodeFactory.createURI(config.mapFieldToProperty("Book", "genre"));
-    protected static final Node uri9 = NodeFactory.createURI(config.mapFieldToProperty("Book", "authors"));
-    protected static final Node uri10 = NodeFactory.createURI(config.getClassMembershipURI());
+    protected final Node uri1 = NodeFactory.createURI(config.mapFieldToProperty("Author", "id"));
+    protected final Node uri2 = NodeFactory.createURI(config.mapFieldToProperty("Author", "name"));
+    protected final Node uri3 = NodeFactory.createURI(config.mapFieldToProperty("Author", "age"));
+    protected final Node uri4 = NodeFactory.createURI(config.mapFieldToProperty("Author", "books"));
+    protected final Node uri5 = NodeFactory.createURI(config.mapFieldToProperty("Book", "id"));
+    protected final Node uri6 = NodeFactory.createURI(config.mapFieldToProperty("Book", "title"));
+    protected final Node uri7 = NodeFactory.createURI(config.mapFieldToProperty("Book", "nr_pages"));
+    protected final Node uri8 = NodeFactory.createURI(config.mapFieldToProperty("Book", "genre"));
+    protected final Node uri9 = NodeFactory.createURI(config.mapFieldToProperty("Book", "authors"));
+    protected final Node uri10 = NodeFactory.createURI(config.getClassMembershipURI());
 
-    protected static final Node uri11 = NodeFactory.createURI(config.mapTypeToClass("Author"));
-    protected static final Node uri12 = NodeFactory.createURI(config.mapTypeToClass("Book"));
+    protected final Node uri11 = NodeFactory.createURI(config.mapTypeToClass("Author"));
+    protected final Node uri12 = NodeFactory.createURI(config.mapTypeToClass("Book"));
 
     // Literal nodes
-    protected static final Node lit1 = NodeFactory.createLiteral("auth3", XSDBaseStringType.XSDstring);
-    protected static final Node lit2 = NodeFactory.createLiteral("book5", XSDBaseStringType.XSDstring);
-    protected static final Node lit3 = NodeFactory.createLiteral("39", XSDBaseNumericType.XSDunsignedInt);
-    protected static final Node lit4 = NodeFactory.createLiteral("William Shakespeare", XSDBaseStringType.XSDstring);
-    protected static final Node lit5 = NodeFactory.createLiteral("MYSTERY", XSDBaseStringType.XSDstring);
-    protected static final Node lit6 = NodeFactory.createLiteral("auth4", XSDBaseStringType.XSDstring);
+    protected final Node lit1 = NodeFactory.createLiteral("auth3", XSDBaseStringType.XSDstring);
+    protected final Node lit2 = NodeFactory.createLiteral("book5", XSDBaseStringType.XSDstring);
+    protected final Node lit3 = NodeFactory.createLiteral("39", XSDBaseNumericType.XSDunsignedInt);
+    protected final Node lit4 = NodeFactory.createLiteral("William Shakespeare", XSDBaseStringType.XSDstring);
+    protected final Node lit5 = NodeFactory.createLiteral("MYSTERY", XSDBaseStringType.XSDstring);
+    protected final Node lit6 = NodeFactory.createLiteral("auth4", XSDBaseStringType.XSDstring);
 
     /**
      * Initializes a GraphQL endpoint for the tests
      */
-    protected static GraphQLSchema initializeGraphQLTestSchema(){
+    protected GraphQLSchema initializeGraphQLTestSchema(){
         Map<String,GraphQLField> authorFields = new HashMap<>();
         Map<String,GraphQLField> bookFields = new HashMap<>();
         Map<String,Map<String,GraphQLField>> objectTypeToFields = new HashMap<>();
