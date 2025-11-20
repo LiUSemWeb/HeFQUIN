4c31c5b46cb1839416306b9f01ca84445c3b6923
Olaf Hartig <olaf.hartig@liu.se>
2025-11-20 19:14:26 +0100

    completes the implementation of MappingOpExtend, including unit tests for it

--- hefquin-rml/src/main/java/se/liu/ida/hefquin/mappings/algebra/ops/MappingOpExtend.java
+++ hefquin-rml/src/main/java/se/liu/ida/hefquin/mappings/algebra/ops/MappingOpExtend.java
@@ -1,13 +1,18 @@
 package se.liu.ida.hefquin.mappings.algebra.ops;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import org.apache.jena.graph.Node;
+
 import se.liu.ida.hefquin.mappings.algebra.MappingOperator;
 import se.liu.ida.hefquin.mappings.algebra.MappingRelation;
+import se.liu.ida.hefquin.mappings.algebra.MappingRelationCursor;
 import se.liu.ida.hefquin.mappings.algebra.exprs.ExtendExpression;
-import se.liu.ida.hefquin.mappings.algebra.exprs.ExtendExpressionUtils;
 import se.liu.ida.hefquin.mappings.algebra.sources.DataObject;
 import se.liu.ida.hefquin.mappings.algebra.sources.SourceReference;
 
@@ -43,7 +48,7 @@ public class MappingOpExtend extends BaseForMappingOperator
 			valid = false;
 		}
 		else {
-			final Set<String> varsInExpr = ExtendExpressionUtils.getAllMentionedAttributes(expr);
+			final Set<String> varsInExpr = expr.getAllMentionedAttributes();
 			valid = schemaOfSubOp.containsAll(varsInExpr);
 		}
 	}
@@ -65,8 +70,103 @@ public class MappingOpExtend extends BaseForMappingOperator
 
 	@Override
 	public MappingRelation evaluate( final Map<SourceReference, DataObject> srMap ) {
-		// TODO
-		throw new UnsupportedOperationException();
+		final MappingRelation input = subOp.evaluate(srMap);
+		return new MyMappingRelation(input);
+	}
+
+	protected class MyMappingRelation implements MappingRelation {
+		protected final MappingRelation input;
+		protected final List<String> schema;
+
+		protected final String[] schemaMappingAttrs;
+		protected final int[] schemaMappingIdxs;
+
+		public MyMappingRelation( final MappingRelation input ) {
+			this.input = input;
+
+			final List<String> inputSchema = input.getSchema();
+			this.schema = new ArrayList<>( inputSchema.size() + 1 );
+			this.schema.addAll(inputSchema);
+			this.schema.add(attribute);
+
+			final Set<String> attrsInExpr = expr.getAllMentionedAttributes();
+			schemaMappingAttrs = new String[ attrsInExpr.size() ];
+			schemaMappingIdxs = new int[ attrsInExpr.size() ];
+			int idxOut = 0;
+			for ( int idxIn = 0; idxIn < inputSchema.size(); idxIn++ ) {
+				final String attr = inputSchema.get(idxIn);
+				if ( attrsInExpr.contains(attr) ) {
+					schemaMappingAttrs[idxOut] = attr;
+					schemaMappingIdxs[idxOut] = idxIn;
+					idxOut++;
+				}
+			}
+		}
+
+		@Override
+		public List<String> getSchema() { return schema; }
+
+		@Override
+		public MappingRelationCursor getCursor() {
+			return new MyCursor( this, input.getCursor(),
+			                     schema.size() - 1,
+			                     schemaMappingAttrs, schemaMappingIdxs );
+		}
+	}
+
+	protected class MyCursor implements MappingRelationCursor {
+		protected final MappingRelation myRelation;
+		protected final MappingRelationCursor input;
+		protected final int idxOfNewAttribute;
+		protected final String[] schemaMappingAttrs;
+		protected final int[] schemaMappingIdxs;
+
+		protected final Map<String,Node> currentAssignment = new HashMap<>();
+		protected Node currentValue = null;
+
+		public MyCursor( final MappingRelation myRelation,
+		                 final MappingRelationCursor input,
+		                 final int idxOfNewAttribute,
+		                 final String[] schemaMappingAttrs,
+		                 final int[] schemaMappingIdxs ) {
+			this.myRelation = myRelation;
+			this.input = input;
+			this.idxOfNewAttribute = idxOfNewAttribute;
+			this.schemaMappingAttrs = schemaMappingAttrs;
+			this.schemaMappingIdxs = schemaMappingIdxs;
+		}
+
+		@Override
+		public MappingRelation getMappingRelation() { return myRelation; }
+
+		@Override
+		public boolean hasNext() {
+			return input.hasNext();
+		}
+
+		@Override
+		public void advance() {
+			input.advance();
+			currentValue = null;
+		}
+
+		@Override
+		public Node getValueOfCurrentTuple( final int idxOfAttribute ) {
+			if ( idxOfAttribute != idxOfNewAttribute )
+				return input.getValueOfCurrentTuple(idxOfAttribute);
+
+			if ( currentValue == null ) {
+				for ( int i = 0; i < schemaMappingAttrs.length; i++ ) {
+					final String attr = schemaMappingAttrs[i];
+					final Node value = input.getValueOfCurrentTuple( schemaMappingIdxs[i] );
+					currentAssignment.put(attr, value);
+				}
+
+				currentValue = expr.evaluate( currentAssignment );
+			}
+
+			return currentValue;
+		}
 	}
 
 }
---
+++ hefquin-rml/src/test/java/se/liu/ida/hefquin/mappings/algebra/ops/MappingOpExtendTest.java
@@ -0,0 +1,169 @@
+package se.liu.ida.hefquin.mappings.algebra.ops;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.apache.jena.graph.Node;
+import org.apache.jena.graph.NodeFactory;
+import org.junit.Test;
+
+import se.liu.ida.hefquin.mappings.algebra.MappingOperator;
+import se.liu.ida.hefquin.mappings.algebra.MappingRelation;
+import se.liu.ida.hefquin.mappings.algebra.MappingRelationCursor;
+import se.liu.ida.hefquin.mappings.algebra.exprs.ExtendExpression;
+import se.liu.ida.hefquin.mappings.algebra.impl.MappingRelationImplWithTupleLayout;
+import se.liu.ida.hefquin.mappings.algebra.impl.MappingRelationImplWithoutTuples;
+
+public class MappingOpExtendTest extends BaseForMappingOperatorTests
+{
+	@Test
+	public void getSchema_Valid() {
+		final MappingRelation input = new MappingRelationImplWithoutTuples("attr1", "attr2");
+		final MappingOperator subOp = new ConstantMappingOperatorForTests(input);
+
+		final ExtendExpression expr = new ExtendExprForTests("attr1");
+
+		final MappingOpExtend op = new MappingOpExtend(subOp, expr, "attr3");
+
+		assertTrue( op.isValid() );
+		assertEquals( Set.of("attr1", "attr2", "attr3"), op.getSchema() );
+	}
+
+	@Test
+	public void getSchema_Invalid1() {
+		final MappingRelation input = new MappingRelationImplWithoutTuples("attr1", "attr2");
+		final MappingOperator subOp = new ConstantMappingOperatorForTests(input);
+
+		// the attribute in the expression is not provided by the input relation
+		final ExtendExpression expr = new ExtendExprForTests("attrX");
+
+		final MappingOpExtend op = new MappingOpExtend(subOp, expr, "attr3");
+
+		assertFalse( op.isValid() );
+		assertEquals( Set.of("attr1", "attr2", "attr3"), op.getSchema() );
+	}
+
+	@Test
+	public void getSchema_Invalid2() {
+		final MappingRelation input = new MappingRelationImplWithoutTuples("attr1", "attr2");
+		final MappingOperator subOp = new ConstantMappingOperatorForTests(input);
+
+		final ExtendExpression expr = new ExtendExprForTests("attr1");
+
+		// the attribute is already in the input relation
+		final MappingOpExtend op = new MappingOpExtend(subOp, expr, "attr2");
+
+		assertFalse( op.isValid() );
+		assertEquals( Set.of("attr1", "attr2"), op.getSchema() );
+	}
+
+	@Test
+	public void evaluate1() {
+		// input is a two-column relation with two tuples, and
+		// the expression is applied to only one of these columns
+		final Node[] t1 = { NodeFactory.createLiteralByValue(1),
+		                    NodeFactory.createLiteralByValue(2) };
+		final Node[] t2 = { NodeFactory.createLiteralByValue(3),
+		                    NodeFactory.createLiteralByValue(4) };
+
+		final List<String> s = List.of("attr1", "attr2");
+		final MappingRelation r = new MappingRelationImplWithTupleLayout(s, t1, t2);
+		final MappingOperator subOp = new ConstantMappingOperatorForTests(r);
+
+		final ExtendExpression expr = new ExtendExprForTests("attr1");
+
+		final MappingOpExtend op = new MappingOpExtend(subOp, expr, "attr3");
+
+		final MappingRelationCursor c = op.evaluate(null).getCursor();
+
+		assertTrue( c.hasNext() );
+		c.advance();
+
+		assertTrue( c.getValueOfCurrentTuple(0) == t1[0] );
+		assertTrue( c.getValueOfCurrentTuple(1) == t1[1] );
+		assertEquals( Integer.valueOf(1), c.getValueOfCurrentTuple(2).getLiteralValue() );
+
+		assertTrue( c.hasNext() );
+		c.advance();
+
+		assertTrue( c.getValueOfCurrentTuple(0) == t2[0] );
+		assertTrue( c.getValueOfCurrentTuple(1) == t2[1] );
+		assertEquals( Integer.valueOf(3), c.getValueOfCurrentTuple(2).getLiteralValue() );
+
+		assertFalse( c.hasNext() );
+	}
+
+	@Test
+	public void evaluate2() {
+		// input is a two-column relation with two tuples, and
+		// the expression is applied to both of these columns
+		final Node[] t1 = { NodeFactory.createLiteralByValue(1),
+		                    NodeFactory.createLiteralByValue(2) };
+		final Node[] t2 = { NodeFactory.createLiteralByValue(3),
+		                    NodeFactory.createLiteralByValue(4) };
+
+		final List<String> s = List.of("attr1", "attr2");
+		final MappingRelation r = new MappingRelationImplWithTupleLayout(s, t1, t2);
+		final MappingOperator subOp = new ConstantMappingOperatorForTests(r);
+
+		final ExtendExpression expr = new ExtendExprForTests("attr1", "attr2");
+
+		final MappingOpExtend op = new MappingOpExtend(subOp, expr, "attr3");
+
+		final MappingRelationCursor c = op.evaluate(null).getCursor();
+
+		assertTrue( c.hasNext() );
+		c.advance();
+
+		assertTrue( c.getValueOfCurrentTuple(0) == t1[0] );
+		assertTrue( c.getValueOfCurrentTuple(1) == t1[1] );
+		assertEquals( Integer.valueOf(1+2), c.getValueOfCurrentTuple(2).getLiteralValue() );
+
+		assertTrue( c.hasNext() );
+		c.advance();
+
+		assertTrue( c.getValueOfCurrentTuple(0) == t2[0] );
+		assertTrue( c.getValueOfCurrentTuple(1) == t2[1] );
+		assertEquals( Integer.valueOf(3+4), c.getValueOfCurrentTuple(2).getLiteralValue() );
+
+		assertFalse( c.hasNext() );
+	}
+
+
+	// ----------- helper ----------
+
+	public static class ExtendExprForTests implements ExtendExpression {
+		final Set<String> A;
+
+		public ExtendExprForTests( final String ... A ) { this( Arrays.asList(A) ); }
+
+		public ExtendExprForTests( final List<String> A ) { this( new HashSet<>(A) ); }
+
+		public ExtendExprForTests( final Set<String> A ) { this.A = A; }
+
+		@Override
+		public Set<String> getAllMentionedAttributes() { return A; }
+
+		@Override
+		public Node evaluate( final Map<String, Node> assignment ) {
+			int sum = 0;
+			for ( final Map.Entry<String, Node> e : assignment.entrySet() ) {
+				final Object v = e.getValue().getLiteralValue();
+				if ( v instanceof Integer i ) {
+					sum += i;
+				}
+			}
+
+			final Node n = NodeFactory.createLiteralByValue(sum);
+			return n;
+		}
+	}
+
+}
