5c9aa5a3d8d301c85400bc97d5838a3a8f0398f6
Olaf Hartig <olaf.hartig@liu.se>
2026-02-17 10:51:35 +0100

    changes UnaryExecutableOpBaseWithBatching such that the given batch size is considered only as a minimum (this way, bigger number of input solution mappings that arrive at the same time may be processed all together rather than artificially split into smaller batches that are processed separately)

--- hefquin-engine/src/main/java/se/liu/ida/hefquin/engine/queryplan/executable/impl/ops/UnaryExecutableOpBaseWithBatching.java
+++ hefquin-engine/src/main/java/se/liu/ida/hefquin/engine/queryplan/executable/impl/ops/UnaryExecutableOpBaseWithBatching.java
@@ -12,11 +12,22 @@ import se.liu.ida.hefquin.engine.queryplan.info.QueryPlanningInfo;
 import se.liu.ida.hefquin.engine.queryproc.ExecutionContext;
 
 /**
- * Base class for all implementations of {@link UnaryExecutableOp} that process
- * the input solution mappings in batches (bind joins are typical examples of
- * such operators).
- *
- * This base class handles collecting the input solution mappings into batches.
+ * Base class for all implementations of {@link UnaryExecutableOp} that work
+ * more effectively if at least a minimum number of input solution mappings
+ * are available to process. To this end, the functionality of this base class
+ * is to collect the input solution mappings until there are at least as many
+ * as specified by the {@code minimumBatchSize} argument of the constructor
+ * and, then, pass the resulting batch of collected input solution mappings
+ * to the {@code _processBatch} function implemented by the operator that is
+ * implemented as an extension of this base class. If further input solution
+ * mappings arrive after that, then these are collected into another batch,
+ * and so on.
+ * <p>
+ * Notice that the specified {@code minimumBatchSize} is only a lower bound;
+ * if multiple input solution mapping arrive at the same time, they are all
+ * added into the current batch, which may result in batches that contain
+ * (many) more than the minimum number of solution mappings.
+ * <p>
  * Classes that extend this base class need to implement two functions:
  * <ul>
  * <li>{@link #_processBatch(List, IntermediateResultElementSink, ExecutionContext)} and</li>
@@ -27,18 +38,18 @@ public abstract class UnaryExecutableOpBaseWithBatching extends UnaryExecutableO
 {
 	private int numberOfBatchesProcessed = 0;
 
-	protected final int batchSize;
+	protected final int minimumBatchSize;
 	protected final List<SolutionMapping> collectedInputSolMaps;
 
-	public UnaryExecutableOpBaseWithBatching( final int batchSize,
+	public UnaryExecutableOpBaseWithBatching( final int minimumBatchSize,
 	                                          final boolean collectExceptions,
 	                                          final QueryPlanningInfo qpInfo ) {
 		super(collectExceptions, qpInfo);
 
-		assert batchSize > 0;
+		assert minimumBatchSize > 0;
 
-		this.batchSize = batchSize;
-		collectedInputSolMaps = new ArrayList<>(batchSize);
+		this.minimumBatchSize = minimumBatchSize;
+		collectedInputSolMaps = new ArrayList<>(minimumBatchSize);
 	}
 
 	@Override
@@ -53,7 +64,38 @@ public abstract class UnaryExecutableOpBaseWithBatching extends UnaryExecutableO
 		// If enough solution mappings have been collected to form the next
 		// batch, process this batch and, afterwards, remove the solution
 		// mappings of that batch from the collection.
-		if ( collectedInputSolMaps.size() == batchSize ) {
+		if ( collectedInputSolMaps.size() == minimumBatchSize ) {
+			_processBatch(collectedInputSolMaps, sink, execCxt);
+			collectedInputSolMaps.clear();
+			numberOfBatchesProcessed++;
+		}
+	}
+
+	@Override
+	protected final void _process( final List<SolutionMapping> inputSolMaps,
+	                               final IntermediateResultElementSink sink,
+	                               final ExecutionContext execCxt )
+			 throws ExecOpExecutionException
+	{
+		// If the number of the given solution mappings exceeds the minimum
+		// batch size and no other solution mappings have been collected so
+		// far (at least not, since the previous batch was processed), then
+		// we can pass on the given solution mappings directly as the next
+		// batch to be processed.
+		if (    inputSolMaps.size() >= minimumBatchSize
+		     && collectedInputSolMaps.isEmpty()  ) {
+			_processBatch(inputSolMaps, sink, execCxt);
+			numberOfBatchesProcessed++;
+			return;
+		}
+
+		// Otherwise, we append the given solution mappings to the collection.
+		collectedInputSolMaps.addAll(inputSolMaps);
+
+		// If enough solution mappings have been collected to form the next
+		// batch, process this batch and, afterwards, remove the solution
+		// mappings of that batch from the collection.
+		if ( collectedInputSolMaps.size() >= minimumBatchSize ) {
 			_processBatch(collectedInputSolMaps, sink, execCxt);
 			collectedInputSolMaps.clear();
 			numberOfBatchesProcessed++;
@@ -90,8 +132,9 @@ public abstract class UnaryExecutableOpBaseWithBatching extends UnaryExecutableO
 	 * Implementations of this function need to process the given batch
 	 * of solution mappings as <strong>last</strong> input, conclude the
 	 * execution of this operator, and send the remaining result elements
-	 * (if any) to the given sink. Notice that the given batch of solution
-	 * mappings may be empty!
+	 * (if any) to the given sink. Notice that the batch given here may
+	 * contain fewer solution mappings than the minimum batch size, and
+	 * it may even be empty!
 	 *
 	 * If an exception occurs during this process, then this exception needs
 	 * to be thrown.
