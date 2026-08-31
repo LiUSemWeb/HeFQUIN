fed568bd77c2005a1459197dd9d1b4da1affd62b
Robin Keskisärkkä <robin.keski@gmail.com>
2026-08-31 09:49:51 +0200

    add default cache-aware request methods to FederationAccessManager

--- hefquin-access/src/main/java/se/liu/ida/hefquin/federation/access/FederationAccessManager.java
+++ hefquin-access/src/main/java/se/liu/ida/hefquin/federation/access/FederationAccessManager.java
@@ -17,6 +17,26 @@ public interface FederationAccessManager extends StatsProvider
 	  MemberType extends FederationMember >
 	CompletableFuture<CardinalityResponse> issueCardinalityRequest( ReqType req, MemberType fm );
 
+	default < ReqType extends DataRetrievalRequest,
+	          RespType extends DataRetrievalResponse<?>,
+	          MemberType extends FederationMember >
+	CompletableFuture<RespType> issueRequest( ReqType req, MemberType fm, boolean ignoreCache )
+			throws FederationAccessException
+	{
+		return issueRequest(req, fm);
+	};
+
+	default < ReqType extends DataRetrievalRequest,
+	          RespType extends DataRetrievalResponse<?>,
+	          MemberType extends FederationMember >
+	CompletableFuture<CardinalityResponse> issueCardinalityRequest( ReqType req,
+	                                                                MemberType fm,
+	                                                                boolean ignoreCardinalityCache )
+			throws FederationAccessException
+	{
+		return issueCardinalityRequest(req, fm);
+	}
+
 	@Override
 	FederationAccessStats getStats();
 
