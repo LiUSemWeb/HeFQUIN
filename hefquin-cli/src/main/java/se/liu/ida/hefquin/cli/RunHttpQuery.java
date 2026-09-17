b0f5c44ae1eaf80eb6788d3f5f7eeaf863ded132
kentthang010 <thangkent010@gmail.com>
2026-09-17 13:15:00 +0200

    create ModStatsPrinting and rearrange order of args

--- hefquin-cli/src/main/java/se/liu/ida/hefquin/cli/RunHttpQuery.java
+++ hefquin-cli/src/main/java/se/liu/ida/hefquin/cli/RunHttpQuery.java
@@ -45,6 +45,7 @@ import se.liu.ida.hefquin.base.net.http.HttpConstants;
 import se.liu.ida.hefquin.cli.modules.ModPlanPrinting;
 import se.liu.ida.hefquin.cli.modules.ModQuery;
 import se.liu.ida.hefquin.cli.modules.ModResultsOutExt;
+import se.liu.ida.hefquin.cli.modules.ModStatsPrinting;
 import se.liu.ida.hefquin.jenaext.query.SyntaxForHeFQUIN;
 import se.liu.ida.hefquin.jenaext.sparql.lang.sparql_12_hefquin.ParserSPARQL12HeFQUIN;
 
@@ -71,6 +72,7 @@ public class RunHttpQuery extends CmdGeneral
 	protected final ModPlanPrinting  modPlanPrinting =  new ModPlanPrinting();
 	protected final ModQuery         modQuery =         new ModQuery();
 	protected final ModResultsOutExt modResultsExt =    new ModResultsOutExt();
+	protected final ModStatsPrinting modStatsPrinting = new ModStatsPrinting();
 
 	protected final ArgDecl argServerAddress = new ArgDecl( ArgDecl.HasValue, "server" );
 
@@ -88,6 +90,7 @@ public class RunHttpQuery extends CmdGeneral
 		addModule( modTime );
 		addModule( modPlanPrinting );
 		addModule( modResultsExt );
+		addModule( modStatsPrinting );
 		addModule( modQuery );
 
 		getUsage().startCategory("Server");
@@ -219,10 +222,10 @@ public class RunHttpQuery extends CmdGeneral
 		if ( modPlanPrinting.getExecutablePlanPrinter() != null )
 			builder.header( HttpConstants.X_HEADER_PRINT_EXECUTABLE_PLAN, "true" );
 
-		if ( modResultsExt.needsQueryProcStats() )
+		if ( modStatsPrinting.needsQueryProcStats() )
 			builder.header( HttpConstants.X_HEADER_RETURN_QUERY_PROC_STATS, "true" );
 
-		if ( modResultsExt.needsFedAccessStats() )
+		if ( modStatsPrinting.needsFedAccessStats() )
 			builder.header( HttpConstants.X_HEADER_RETURN_FED_ACCESS_STATS, "true" );
 
 		if ( isDebug() ) {
@@ -283,14 +286,14 @@ public class RunHttpQuery extends CmdGeneral
 
 		final JsonValue queryProcStatsValue = obj.get(HttpConstants.JSON_QUERY_PROC_STATS);
 		if ( queryProcStatsValue != null ) {
-			modResultsExt.handleQueryProcStats( queryProcStatsValue, msg -> cmdError( msg, false ) );
+			modStatsPrinting.handleQueryProcStats( queryProcStatsValue, msg -> cmdError( msg, false ) );
 
-			modResultsExt.handleOnelineTimeStats( extractOnelineTimeStats( queryProcStatsValue ), msg -> cmdError( msg, false ) );
+			modStatsPrinting.handleOnelineTimeStats( extractOnelineTimeStats( queryProcStatsValue ), msg -> cmdError( msg, false ) );
 		}
 
 		final JsonValue fedAccessStatsValue = obj.get(HttpConstants.JSON_FED_ACCESS_STATS);
 		if ( fedAccessStatsValue != null ) {
-			modResultsExt.handleFedAccessStats( fedAccessStatsValue, msg -> cmdError( msg, false ) );
+			modStatsPrinting.handleFedAccessStats( fedAccessStatsValue, msg -> cmdError( msg, false ) );
 		}
 	}
 
